4f1859e22723e4a236ac5e231b12cf31b7643edb
Justin Marney <justin@mustwin.com>
2016-08-05 22:50:44 +0000

    Fix incorrect method call. This would've been caught by tests. This is a WIP :D

---
+++ resource_identity_policy_2.go
@@ -0,0 +1,49 @@
+package main
+
+import "github.com/hashicorp/terraform/helper/schema"
+
+func ResourceIdentityPolicy() *schema.Resource {
+	policySchema := make(map[string]*schema.Schema)
+
+	for key, value := range resourceSchema {
+		policySchema[key] = value
+	}
+
+	policySchema["statements"] = &schema.Schema{
+		Type:     schema.TypeList,
+		Required: true,
+		Elem:     &schema.Schema{Type: schema.TypeString},
+	}
+
+	return &schema.Resource{
+		Create: createPolicy,
+		Read:   readPolicy,
+		Update: updatePolicy,
+		Delete: deletePolicy,
+		Schema: policySchema,
+	}
+}
+
+func createPolicy(d *schema.ResourceData, m interface{}) (e error) {
+	client := m.(BareMetalClient)
+	sync := &PolicySync{d, client}
+	return createResource(d, sync)
+}
+
+func readPolicy(d *schema.ResourceData, m interface{}) (e error) {
+	client := m.(BareMetalClient)
+	sync := &PolicySync{d, client}
+	return readResource(sync)
+}
+
+func updatePolicy(d *schema.ResourceData, m interface{}) (e error) {
+	client := m.(BareMetalClient)
+	sync := &PolicySync{d, client}
+	return updateResource(d, sync)
+}
+
+func deletePolicy(d *schema.ResourceData, m interface{}) (e error) {
+	client := m.(BareMetalClient)
+	sync := &PolicySync{d, client}
+	return sync.Delete()
+}
