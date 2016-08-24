f56f45e21e52860508adf3dd682abd53e67b134a
Justin Marney <gotascii@gmail.com>
2016-08-24 20:51:19 +0000

    Update baremetal to 9b2c7afab7d307ac707bf1c84cc52ba2d489ef7e.

---
+++ vendor/github.com/MustWin/baremetal-sdk-go/core_drg_attachment.go
@@ -0,0 +1,112 @@
+package baremetal
+
+import "net/http"
+
+// DrgAttachment describes a Drg attachment to a Vcn
+//
+// See https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#DrgAttachment
+type DrgAttachment struct {
+	ETaggedResource
+	CompartmentID string `json:"compartmentId"`
+	DisplayName   string `json:"displayName"`
+	DrgID         string `json:"drgId"`
+	ID            string `json:"id"`
+	State         string `json:"state"`
+	TimeCreated   Time   `json:"timeCreated"`
+	VcnID         string `json:"vcnId"`
+}
+
+// DrgAttachmentList contains a list of volume attachments
+//
+type DrgAttachmentList struct {
+	ResourceContainer
+	DrgAttachments []DrgAttachment
+}
+
+func (l *DrgAttachmentList) GetList() interface{} {
+	return &l.DrgAttachments
+}
+
+// CreateDrgAttachment attaches a drg to a vcn.
+//
+// See https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#createDrgAttachment
+func (c *Client) CreateDrgAttachment(drgID, vcnID string, opts ...Options) (res *DrgAttachment, e error) {
+	body := struct {
+		DisplayName string `json:"displayName,omitempty"`
+		DrgID       string `json:"drgId"`
+		VcnID       string `json:"vcnId"`
+	}{
+		DrgID: drgID,
+		VcnID: vcnID,
+	}
+
+	if len(opts) > 0 {
+		body.DisplayName = opts[0].DisplayName
+	}
+
+	reqOpts := &sdkRequestOptions{
+		body:    body,
+		name:    resourceDrgAttachments,
+		options: opts,
+	}
+
+	var response *requestResponse
+	if response, e = c.coreApi.request(http.MethodPost, reqOpts); e != nil {
+		return
+	}
+
+	res = &DrgAttachment{}
+	e = response.unmarshal(res)
+	return
+}
+
+// GetDrgAttachment gets information about the specified drg attachment
+//
+// See https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#getDrgAttachment
+func (c *Client) GetDrgAttachment(id string, opts ...Options) (res *DrgAttachment, e error) {
+	reqOpts := &sdkRequestOptions{
+		name:    resourceDrgAttachments,
+		options: opts,
+		ids:     urlParts{id},
+	}
+	var resp *requestResponse
+	if resp, e = c.coreApi.getRequest(reqOpts); e != nil {
+		return
+	}
+
+	res = &DrgAttachment{}
+	e = resp.unmarshal(res)
+	return
+}
+
+// DeleteDrgAttachment detaches a drg from its vcn
+//
+// See https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#deleteDrgAttachment
+func (c *Client) DeleteDrgAttachment(id string, opts ...Options) (e error) {
+	reqOpts := &sdkRequestOptions{
+		name:    resourceDrgAttachments,
+		options: opts,
+		ids:     urlParts{id},
+	}
+	return c.coreApi.deleteRequest(reqOpts)
+}
+
+// ListDrgAttachments gets a list of the drgs in the specified compartment
+//
+// See https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#listDrgAttachments
+func (c *Client) ListDrgAttachments(compartmentID string, opts ...Options) (res *DrgAttachmentList, e error) {
+	reqOpts := &sdkRequestOptions{
+		name:    resourceDrgAttachments,
+		ocid:    compartmentID,
+		options: opts,
+	}
+
+	var resp *requestResponse
+	if resp, e = c.coreApi.getRequest(reqOpts); e != nil {
+		return
+	}
+
+	res = &DrgAttachmentList{}
+	e = resp.unmarshal(res)
+	return
+}
---
+++ vendor/github.com/MustWin/baremetal-sdk-go/core_instance_console_histories.go
@@ -0,0 +1,126 @@
+package baremetal
+
+import (
+	"net/http"
+	"time"
+)
+
+// ConsoleHistoryMetadata describes console history metadata
+//
+// See: https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#ConsoleHistoryMetadata
+type ConsoleHistoryMetadata struct {
+	ETaggedResource
+	AvailabilityDomain string    `json:"availabilityDomain"`
+	CompartmentID      string    `json:"compartmentId"`
+	DisplayName        string    `json:"displayName"`
+	ID                 string    `json:"id"`
+	InstanceID         string    `json:"instanceId"`
+	State              string    `json:"state"`
+	TimeCreated        time.Time `json:"TimeCreated"`
+}
+
+// InstanceConsoleHistoriesMetadataList contains a list of Console History Metadata
+type InstanceConsoleHistoriesMetadataList struct {
+	ResourceContainer
+	InstanceConsoleHistoriesMetadatas []ConsoleHistoryMetadata
+}
+
+func (l *InstanceConsoleHistoriesMetadataList) GetList() interface{} {
+	return &l.InstanceConsoleHistoriesMetadatas
+}
+
+// ListConsoleHistories shows the metadata for the specified compartment or instance
+//
+// See https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#listConsoleHistories
+func (c *Client) ListConsoleHistories(compartmentID string, opts ...Options) (icHistories *InstanceConsoleHistoriesMetadataList, e error) {
+	reqOpts := &sdkRequestOptions{
+		name:    resourceInstanceConsoleHistories,
+		ocid:    compartmentID,
+		options: opts,
+	}
+
+	var resp *requestResponse
+	if resp, e = c.coreApi.getRequest(reqOpts); e != nil {
+		return
+	}
+
+	icHistories = &InstanceConsoleHistoriesMetadataList{}
+	e = resp.unmarshal(icHistories)
+	return
+}
+
+// CaptureConsoleHistory captures the most recent serial console data (up to a megabyte) for the specified instance.
+//
+// See https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#captureConsoleHistory
+func (c *Client) CaptureConsoleHistory(instanceID string, opts ...Options) (icHistory *ConsoleHistoryMetadata, e error) {
+	createRequest := struct {
+		InstanceID string `json:"instanceId"`
+	}{
+		InstanceID: instanceID,
+	}
+
+	reqOpts := &sdkRequestOptions{
+		body:    createRequest,
+		name:    resourceInstanceConsoleHistories,
+		options: opts,
+	}
+
+	var resp *requestResponse
+	if resp, e = c.coreApi.request(http.MethodPost, reqOpts); e != nil {
+		return
+	}
+
+	icHistory = &ConsoleHistoryMetadata{}
+	e = resp.unmarshal(icHistory)
+	return
+}
+
+// GetConsoleHistory shows the metadata for the specified console history
+//
+// See https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#getConsoleHistory
+func (c *Client) GetConsoleHistory(instanceID string, opts ...Options) (consoleHistoryMetadata *ConsoleHistoryMetadata, e error) {
+	reqOpts := &sdkRequestOptions{
+		name:    resourceInstanceConsoleHistories,
+		options: opts,
+		ids:     urlParts{instanceID},
+	}
+	var resp *requestResponse
+	if resp, e = c.coreApi.getRequest(reqOpts); e != nil {
+		return
+	}
+
+	consoleHistoryMetadata = &ConsoleHistoryMetadata{}
+	e = resp.unmarshal(consoleHistoryMetadata)
+	return
+}
+
+// DeleteConsoleHistory deletes the specified console history metadata and the console history data
+//
+// See https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#deleteConsoleHistory
+func (c *Client) DeleteConsoleHistory(id string, opts ...Options) (e error) {
+	reqOpts := &sdkRequestOptions{
+		name:    resourceInstanceConsoleHistories,
+		options: opts,
+		ids:     urlParts{id},
+	}
+	return c.coreApi.deleteRequest(reqOpts)
+}
+
+// ShowConsoleHistoryData gets the actual console history data (not the metadata).
+//
+// See https://docs.us-az-phoenix-1.oracleiaas.com/api/core.html#showConsoleHistoryData
+func (c *Client) ShowConsoleHistoryData(instanceConsoleHistoryID string, opts ...Options) (consoleHistoryData string, e error) {
+	reqOpts := &sdkRequestOptions{
+		name:    resourceInstanceConsoleHistories,
+		options: opts,
+		ids:     urlParts{instanceConsoleHistoryID, dataURLPart},
+	}
+	var resp *requestResponse
+	if resp, e = c.coreApi.getRequest(reqOpts); e != nil {
+		return
+	}
+
+	consoleHistoryData = string(resp.body[:])
+
+	return
+}
