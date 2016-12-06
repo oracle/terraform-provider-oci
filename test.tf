a2619d94708ce8fad358fadc5336ff88baac675c
Gonzalo Maldonado <elg0nz@gmail.com>
2016-12-05 18:20:04 -0800

    Update test terraform plan

--- test.tf
+++ test.tf
@@ -1,5 +1,14 @@
-resource "baremetal_identity_user" "users" {
-  compartment_id = "TBD.TBD.TBD"
-  name = "test_user"
-  description = "A test user"
+provider "baremetal" {
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  private_key_password = "${var.private_key_password}"
+  private_key = ""
+}
+
+resource "baremetal_identity_compartment" "test-dec5" {
+  compartment_id = "${var.tenancy_ocid}-dec5"
+  name = "test_compartment_dec5"
+  description = "A special test compartment."
 }
