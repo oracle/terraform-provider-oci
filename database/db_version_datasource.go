3f599e92c8582843df38d93dfd8f531b0e4a2727
Justin Marney <gotascii@gmail.com>
2016-12-14 23:14:26 +0000

    DBVersion datasource.

---
+++ database/db_version_datasource.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"github.com/MustWin/terraform-Oracle-BareMetal-Provider/client"
+	"github.com/MustWin/terraform-Oracle-BareMetal-Provider/crud"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func DBVersionDatasource() *schema.Resource {
+	return &schema.Resource{
+		Read: readDBVersions,
+		Schema: map[string]*schema.Schema{
+			"compartment_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"db_versions": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"version": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"limit": {
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"page": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func readDBVersions(d *schema.ResourceData, m interface{}) (e error) {
+	client := m.(client.BareMetalClient)
+	reader := &DBVersionDatasourceCrud{D: d, Client: client}
+	return crud.ReadResource(reader)
+}
