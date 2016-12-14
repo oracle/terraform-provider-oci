53f40426467e9a0183b58620712ef564ff8e1441
Gonzalo Maldonado <elg0nz@gmail.com>
2016-12-14 15:31:52 -0800

    Implement BucketsummaryDatasource

--- objectstorage/bucketsummary_datasource.go
+++ objectstorage/bucketsummary_datasource.go
@@ -1,1 +1,49 @@
 package objectstorage
+import (
+	"github.com/MustWin/terraform-Oracle-BareMetal-Provider/client"
+	"github.com/MustWin/terraform-Oracle-BareMetal-Provider/crud"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+
+func BucketsummaryDatasource() *schema.Resource {
+	return &schema.Resource{
+		Read: readBucketsummaries(),
+		Schema: map[string]*schema.Schema{
+			"compartment_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"namespace": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"bucketsummaries": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"version": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"limit": {
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"page": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func readBucketsummaries(d *schema.ResourceData, m interface{}) (e error) {
+	client := m.(client.BareMetalClient)
+	reader := &BucketsummaryDatasourceCrud{D: d, Client: client}
+	return crud.ReadResource(reader)
+}
