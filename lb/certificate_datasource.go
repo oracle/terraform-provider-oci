f1df25658e918c32d328a3220876710d0a5675bc
Mike Ihbe <mikejihbe@gmail.com>
2017-04-03 11:37:34 -0700

    add a certificate datasource

--- lb/certificate_datasource.go
+++ lb/certificate_datasource.go
@@ -2,4 +2,34 @@
 
 package lb
 
-// TODO:
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"github.com/oracle/terraform-provider-baremetal/client"
+	"github.com/oracle/terraform-provider-baremetal/crud"
+)
+
+func CertificateDatasource() *schema.Resource {
+	return &schema.Resource{
+		Read: readCertificate,
+		Schema: map[string]*schema.Schema{
+			"load_balancer_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"certificates": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     LoadBalancerCertificateResource(),
+			},
+		},
+	}
+}
+
+func readCertificate(d *schema.ResourceData, m interface{}) (e error) {
+	client := m.(client.BareMetalClient)
+	sync := &BackendSetDatasourceCrud{}
+	sync.D = d
+	sync.Client = client
+	return crud.ReadResource(sync)
+}
