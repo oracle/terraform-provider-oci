d159a7849706b1b4fe9a688e7060b45c6fd5beeb
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2017-04-28 12:44:39 -0700

    lb: data.load_balancer_backendsets: update & test

--- lb/backendset_datasource_crud.go
+++ lb/backendset_datasource_crud.go
@@ -3,6 +3,7 @@
 package lb
 
 import (
+	"log"
 	"time"
 
 	"github.com/MustWin/baremetal-sdk-go"
@@ -22,48 +23,59 @@ func (s *BackendSetDatasourceCrud) Get() (e error) {
 }
 
 func (s *BackendSetDatasourceCrud) SetData() {
-	if s.Res != nil {
-		s.D.SetId(time.Now().UTC().String())
-		resources := []map[string]interface{}{}
-		for _, v := range s.Res.BackendSets {
-			backends := []map[string]interface{}{}
-			for _, backend := range v.Backends {
-				res := map[string]interface{}{
-					"name":       backend.Name,
-					"ip_address": backend.IPAddress,
-					"port":       backend.Port,
-					"backup":     backend.Backup,
-					"drain":      backend.Drain,
-					"offline":    backend.Offline,
-					"weight":     backend.Weight,
-				}
-				backends = append(backends, res)
+	if s.Res == nil {
+		panic("LoadBalancer Backend Resource is nil, cannot SetData")
+	}
+	s.D.SetId(time.Now().UTC().String())
+	resources := []map[string]interface{}{}
+	for _, v := range s.Res.BackendSets {
+		var healthChecker []map[string]interface{}
+		if hc := v.HealthChecker; hc != nil {
+			healthChecker = []map[string]interface{}{
+				{
+					"interval_ms":         hc.IntervalInMS,
+					"port":                hc.Port,
+					"protocol":            hc.Protocol,
+					"response_body_regex": hc.ResponseBodyRegex,
+				},
 			}
-
-			hcState := s.D.Get("health_checker").(map[string]interface{})
-			healthChecker := baremetal.HealthChecker{
-				IntervalInMS:      hcState["interval_ms"].(int),
-				Port:              hcState["port"].(int),
-				Protocol:          hcState["protocol"].(string),
-				ResponseBodyRegex: hcState["response_body_regex"].(string),
-			}
-
-			sslState := s.D.Get("ssl_configuration").(map[string]interface{})
-			sslConfig := baremetal.SSLConfiguration{
-				CertificateName:       sslState["certificate_name"].(string),
-				VerifyDepth:           sslState["verify_depth"].(int),
-				VerifyPeerCertificate: sslState["verify_peer_certificate"].(bool),
-			}
-			res := map[string]interface{}{
-				"name":              v.Name,
-				"policy":            v.Policy,
-				"health_checker":    healthChecker,
-				"ssl_configuration": sslConfig,
-				"backends":          backends,
-			}
-			resources = append(resources, res)
 		}
-		s.D.Set("backendsets", resources)
+
+		var sslConfig []map[string]interface{}
+		if ssl := v.SSLConfig; ssl != nil {
+			sslConfig = []map[string]interface{}{
+				{
+					"certificate_name":        ssl.CertificateName,
+					"verify_depth":            ssl.VerifyDepth,
+					"verify_peer_certificate": ssl.VerifyPeerCertificate,
+				},
+			}
+		}
+		backends := []map[string]interface{}{}
+		for _, backend := range v.Backends {
+			res := map[string]interface{}{
+				"name":       backend.Name,
+				"ip_address": backend.IPAddress,
+				"port":       backend.Port,
+				"backup":     backend.Backup,
+				"drain":      backend.Drain,
+				"offline":    backend.Offline,
+				"weight":     backend.Weight,
+			}
+			backends = append(backends, res)
+		}
+		res := map[string]interface{}{
+			"name":              v.Name,
+			"policy":            v.Policy,
+			"health_checker":    healthChecker,
+			"ssl_configuration": sslConfig,
+			"backend":           backends,
+		}
+		resources = append(resources, res)
+	}
+	err := s.D.Set("backendsets", resources)
+	if err != nil {
+		log.Printf("[ERROR] Failed to set load_balancers: %v", err)
 	}
 	return
 }
