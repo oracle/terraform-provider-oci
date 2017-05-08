28d0bfee8620b915d21186174cf83ce834717276
Craig Carl <craig.carl@oracle.com>
2017-05-08 15:03:25 +0000

    updated NFS server example

---
+++ docs/examples/storage/nfs/compute.tf
@@ -0,0 +1,17 @@
+resource "baremetal_core_instance" "TFInstance" {
+  availability_domain = "${lookup(data.baremetal_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}" 
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "NFS server"
+  hostname_label = "NFSserver"
+  image = "${lookup(data.baremetal_core_images.OLImageOCID.images[0], "id")}"
+  shape = "${var.InstanceShape}"
+  subnet_id = "${var.SubnetOCID}"
+  metadata {
+    ssh_authorized_keys = "${var.ssh_public_key}"
+    user_data = "${base64encode(file(var.BootStrapFile))}"
+  }
+
+  timeouts {
+    create = "60m"
+  }
+}
