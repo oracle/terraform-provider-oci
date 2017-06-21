dd0cd99f2cb99613b58ef405ae374c61ecc3dfb8
Esteban G <esteban.ginez@oracle.com>
2017-06-21 10:06:29 -0700

    Adding documentation to preauntreq

---
+++ docs/examples/storage/preauthrequests/provider.tf
@@ -0,0 +1,6 @@
+provider "baremetal" {
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+}
