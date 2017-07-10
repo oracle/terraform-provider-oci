0dc8b9c65a0698342669590f1f76ef8de7494b8e
ccushing <cody.cushing@oracle.com>
2017-07-10 12:02:20 -0700

    Update iscsiattach.sh example

--- docs/examples/storage/nfs/userdata/iscsiattach.sh
+++ docs/examples/storage/nfs/userdata/iscsiattach.sh
@@ -7,61 +7,21 @@
 # 20 April 2017
 # Copyright Oracle, Inc.  All rights reserved.
 
-# Make FIFO pipes for the two loops below
-mkfifo discpipe
-mkfifo sesspipe
-
-# Set the address ranges based on the Block Storage version
-V1ADDR="169.254.0.2"
-V2ADDR="169.254.2.0"
-
-# Set the block storage version
-BSV="v1"
-
-# If the BSV is v2, we need to scan all 254 addresses, otherwise,
-# we scan 1. :-(
-
-if [ ${BSV} = "v2" ]
-then
-	numAddrs=254
-	BASEADDR=${V2ADDR}
-else
-	numAddrs=3
-	BASEADDR=${V1ADDR}
-fi
+BASEADDR="169.254.2.2"
 
 # Set a base address incrementor so we can loop through all the
 # addresses.
 addrCount=0
 
-echo "Scanning "${numAddrs}" for new targets.  Stand by."
-while [ ${addrCount} -le ${numAddrs} ]
+while [ ${addrCount} -le 32 ]
 do
-	# Set the current address to attempt to attach.
-	if [ ${BSV} = "v2" ]
-	then
-		CURRADDR=`echo ${BASEADDR} | awk -F\. '{
-last=$4+'${addrCount}'
-print $1"."$2"."$3"."last
-}'`
-	else
-		CURRADDR=`echo ${BASEADDR} | awk -F\. '{
-last=$3+'${addrCount}'
-print $1"."$2"."last"."$4
-}'`
-	fi
 
-	# We use ping to see if the target is even there.
-	# Skip to the next address if we cant ping it.
-	ping -q -c 1 -W 1 ${CURRADDR} > /dev/null 2>&1
-	result=$?
-	if [ ${result} -ne 0 ]
-	then
-		(( addrCount = addrCount + 1 ))
-		continue
-	fi
+	CURRADDR=`echo ${BASEADDR} | awk -F\. '{last=$4+'${addrCount}';print $1"."$2"."$3"."last}'`
 
-	echo "Connecting to "${CURRADDR}
+	clear
+	echo "Attempting connection to ${CURRADDR}"
+
+	mkfifo discpipe
 	# Find all the iSCSI Block Storage volumes attached to the instance but
 	# not configured for use on the instance.  Basically, get a list of the
 	# volumes that the instance can see, the loop through the ones it has,
@@ -80,13 +40,14 @@ print $1"."$2"."last"."$4
 		(( addrCount = addrCount + 1 ))
 		continue
 	fi
-
+	 
 	# Loop through the list (via the named FIFO pipe below)
 	while read target
 	do
+	    mkfifo sesspipe
 	    # Get the list of the currently attached Block Storage volumes
 	    iscsiadm -m session -P 0 | grep -v uefi | awk '{print $4}' > sesspipe 2> /dev/null &
-
+	     
 	    # Set a flag, and loop through the sessions (attached, but not configured)
 	    # and see if the volumes match.  If so, skip to the next until we get
 	    # through the list.  Session list is via the pipe.
@@ -99,7 +60,7 @@ print $1"."$2"."last"."$4
 		    break
 		fi
 	    done < sesspipe
-
+	     
 	    # If the volume is not found, configure it.  Get the resulting device file.
 	    if [ ${found} = "false" ]
 	    then
@@ -109,9 +70,8 @@ print $1"."$2"."last"."$4
 		sleep 10
 	    fi
 	done < discpipe
+	
 	(( addrCount = addrCount + 1 ))
+	find . -maxdepth 1 -type p -exec rm {} \;
 done
 echo "Scan Complete."
-
-# Remove the FIFOs
-find . -maxdepth 1 -type p -exec rm {} \;
