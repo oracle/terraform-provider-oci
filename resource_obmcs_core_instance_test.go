f5cdcc8ad219eb486322518d6b21e33de9002c32
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2017-08-10 22:55:34 -0700

    resource instance: simplify test

--- resource_obmcs_core_instance_test.go
+++ resource_obmcs_core_instance_test.go
@@ -4,88 +4,37 @@ package main
 
 import (
 	"testing"
-	"time"
 
 	"github.com/MustWin/baremetal-sdk-go"
 	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/terraform"
-
-	"github.com/stretchr/testify/suite"
-
-	"github.com/oracle/terraform-provider-baremetal/crud"
 )
 
-type ResourceCoreInstanceTestSuite struct {
-	suite.Suite
-	Client       mockableClient
-	Provider     terraform.ResourceProvider
-	Providers    map[string]terraform.ResourceProvider
-	TimeCreated  baremetal.Time
-	Config       string
-	ResourceName string
-	Res          *baremetal.Instance
-	DeletedRes   *baremetal.Instance
-}
-
-func (s *ResourceCoreInstanceTestSuite) SetupTest() {
-	s.Client = GetTestProvider()
-
-	s.Provider = Provider(
-		func(d *schema.ResourceData) (interface{}, error) {
-			return s.Client, nil
-		},
-	)
-
-	s.Providers = map[string]terraform.ResourceProvider{
-		"baremetal": s.Provider,
-	}
-
-	s.TimeCreated = baremetal.Time{Time: time.Now()}
-
-	s.Config = instanceConfig + `
+func TestAccResourceCoreInstance(t *testing.T) {
+	config := instanceConfig + `
 	data "baremetal_core_instances" "s" {
       		compartment_id = "${var.compartment_id}"
       		availability_domain = "${data.baremetal_identity_availability_domains.ADs.availability_domains.0.name}"
     	}`
 
-	s.Config += testProviderConfig()
-
-	s.ResourceName = "baremetal_core_instance.t"
-}
-
-func (s *ResourceCoreInstanceTestSuite) TestCreateResourceCoreInstance() {
-
-	resource.UnitTest(s.T(), resource.TestCase{
-		Providers: s.Providers,
+	config += testProviderConfig()
+	resource.UnitTest(t, resource.TestCase{
+		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
 				ImportState:       true,
 				ImportStateVerify: true,
-				Config:            s.Config,
+				Config:            config,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(s.ResourceName, "availability_domain"),
-					resource.TestCheckResourceAttr(s.ResourceName, "display_name", "instance_name"),
-					resource.TestCheckResourceAttrSet(s.ResourceName, "id"),
-					resource.TestCheckResourceAttr(s.ResourceName, "state", baremetal.ResourceRunning),
-					resource.TestCheckResourceAttrSet(s.ResourceName, "time_created"),
-					resource.TestCheckResourceAttrSet(s.ResourceName, "public_ip"),
-					resource.TestCheckResourceAttrSet(s.ResourceName, "private_ip"),
+					resource.TestCheckResourceAttrSet("baremetal_core_instance.t", "availability_domain"),
+					resource.TestCheckResourceAttr("baremetal_core_instance.t", "display_name", "instance_name"),
+					resource.TestCheckResourceAttrSet("baremetal_core_instance.t", "id"),
+					resource.TestCheckResourceAttr("baremetal_core_instance.t", "state", baremetal.ResourceRunning),
+					resource.TestCheckResourceAttrSet("baremetal_core_instance.t", "time_created"),
+					resource.TestCheckResourceAttrSet("baremetal_core_instance.t", "public_ip"),
+					resource.TestCheckResourceAttrSet("baremetal_core_instance.t", "private_ip"),
 					resource.TestCheckResourceAttrSet("data.baremetal_core_instances.s", "instances.#"),
 				),
 			},
 		},
 	})
-}
-
-func TestIsStatefulResource(t *testing.T) {
-	var sr crud.StatefulResource
-	sr = &InstanceResourceCrud{}
-	if sr == nil {
-		t.Fail()
-	}
-}
-
-func TestResourceCoreInstanceTestSuite(t *testing.T) {
-	suite.Run(t, new(ResourceCoreInstanceTestSuite))
 }
