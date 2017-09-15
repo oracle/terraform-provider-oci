2f55ff67da28f8972ac2d9c5a35fe37b91befa28
ccushing <cody.cushing@oracle.com>
2017-09-15 09:48:34 -0700

    fix/breakout load balancer tests

--- data_source_obmcs_loadbalancer_loadbalancer_policies_test.go
+++ data_source_obmcs_loadbalancer_loadbalancer_policies_test.go
@@ -6,30 +6,24 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/terraform"
 )
 
-func TestLoadBalancerPoliciesDatasource(t *testing.T) {
-	client := GetTestProvider()
-	providers := map[string]terraform.ResourceProvider{
-		"oci": Provider(func(d *schema.ResourceData) (interface{}, error) {
-			return client, nil
-		}),
-	}
+func TestAccDatasourceLoadBalancerPolicies_basic(t *testing.T) {
+	providers := testAccProviders
+	config := testProviderConfig() + `
+	data "oci_load_balancer_policies" "t" {
+		compartment_id = "${var.compartment_id}"
+	}`
+
 	resourceName := "data.oci_load_balancer_policies.t"
-	config := `
-data "oci_load_balancer_policies" "t" {
-  compartment_id = "${var.compartment_id}"
-}
-`
-	config += testProviderConfig()
 
 	resource.UnitTest(t, resource.TestCase{
 		PreventPostDestroyRefresh: true,
 		Providers:                 providers,
 		Steps: []resource.TestStep{
 			{
+				ImportState:       true,
+				ImportStateVerify: true,
 				Config: config,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet(resourceName, "compartment_id"),
