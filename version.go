9b1602fc26360d3f540e24e1f376f475a7e49afc
ccushing <cody.cushing@oracle.com>
2017-09-16 01:23:35 -0700

    version as binary suffix * simpler make commands

--- version.go
+++ version.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"log"
-	"strconv"
-	"strings"
 )
 
-// Current public release available on github, overwritten as build step, do not alter, see Makefile
-const version = "v0.0.0"
+// Version injected during Makefile "release" build step, do not commit change
+const version = "1.2.3"
 
 func printVersion() {
-	// increment build number programatically since this will be the next public release
-	strs := strings.Split(version, ".")
-	build, _ := strconv.ParseInt(strs[2], 10, 64)
-	strs[2] = strconv.FormatInt(build+1, 10)
-	log.Printf("[INFO] terraform-provider-oci %s\n", strings.Join(strs, "."))
+	log.Printf("[INFO] terraform-provider-oci %s\n", version)
 }
