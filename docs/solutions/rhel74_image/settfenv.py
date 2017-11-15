67da474e69ce7259dbe7fd244967e8d728eae0ec
Steve Nelson <steve.b.nelson@oracle.com>
2017-11-15 16:16:04 -0600

    Fixed induced bug on naming...

--- docs/solutions/rhel74_image/settfenv.py
+++ docs/solutions/rhel74_image/settfenv.py
@@ -1,10 +1,10 @@
-#!/usr/local/bin/python2.7
+#!/usr/local/bin/python3.6
 
 import os
-import ConfigParser
+import configparser
 import argparse
 
-envList = {
+env_list = {
     "tenancy": "tenancy_ocid",
     "fingerprint": "fingerprint",
     "key_file": "private_key_path",
@@ -17,7 +17,7 @@ envList = {
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--config_path', required=False,
-                       default='~/.oraclebmc/config', dest="config_path")
+                       default='~/.oci/config', dest="config_path")
 argparser.add_argument('--profile', required=False,
                        default='DEFAULT', dest="profile")
 argparser.add_argument('--dest_file', required=False,
@@ -26,45 +26,44 @@ argparser.add_argument('--public_key', required=False,
                        default="~/.ssh/id_rsa.pub", dest='public_key')
 args = vars(argparser.parse_args())
 
-outputEnv = {}
-outputFile = os.open(args["dest_file"], os.O_RDWR |
-                     os.O_TRUNC | os.O_CREAT, 0755)
+output_env = {}
+output_file = os.open(args["dest_file"], os.O_RDWR |
+                      os.O_TRUNC | os.O_CREAT, 0x0755)
 
 if len(args["config_path"]) <= 0:
-    obmcsConfig = "~/.oraclebmc/config"
+    oci_config_file = "~/.oci/config"
 else:
-    obmcsConfig = args["config_path"]
+    oci_config_file = args["config_path"]
 
-obmcsConfig = os.path.expanduser(obmcsConfig)
+oci_config_file = os.path.expanduser(oci_config_file)
 
-parser = ConfigParser.ConfigParser()
-parser.read(obmcsConfig)
+parser = configparser.ConfigParser()
+parser.read(oci_config_file)
 
 if (args["profile"] in parser.sections()):
     for options in parser.options(args["profile"]):
-        outputEnv[envList[options]] = parser.get(
+        output_env[env_list[options]] = parser.get(
             args["profile"], options)
 elif args["profile"] == "DEFAULT":
     for options in parser.defaults().keys():
-        outputEnv[envList[options]] = parser.defaults()[options]
+        output_env[env_list[options]] = parser.defaults()[options]
 
-if "~" in outputEnv["private_key_path"]:
-    outputEnv["private_key_path"] = \
-        os.path.expanduser(outputEnv["private_key_path"])
+if "~" in output_env["private_key_path"]:
+    output_env["private_key_path"] = \
+        os.path.expanduser(output_env["private_key_path"])
 
-publicKeyFile = os.path.expanduser(args['public_key'])
-if (os.path.exists(publicKeyFile)):
-    pkFile = os.open(publicKeyFile, os.O_RDONLY)
-    outputEnv["ssh_public_key"] = os.read(pkFile, 512).rstrip()
-    os.close(pkFile)
+public_key_file = os.path.expanduser(args['public_key'])
+if (os.path.exists(public_key_file)):
+    pk_file = os.open(public_key_file, os.O_RDONLY)
+    output_env["ssh_public_key"] = os.read(pk_file, 512).rstrip()
+    os.close(pk_file)
 
-outputEnv["profile"] = args["profile"]
+output_env["profile"] = args["profile"]
 
-os.write(outputFile, "#!/bin/bash\n")
-for key in envList.keys():
-    if envList[key] not in outputEnv:
-        outputEnv[envList[key]] = ""
-    os.write(outputFile, "export TF_VAR_{0}=\"{1}\"\n".format(
-        envList[key], outputEnv[envList[key]]))
-
-os.close(outputFile)
+os.write(output_file, b"#!/bin/bash\n")
+for key in env_list.keys():
+    if env_list[key] not in output_env:
+        output_env[env_list[key]] = ""
+    os.write(output_file, "export TF_VAR_{0}=\"{1}\"\n".format(
+        env_list[key], output_env[env_list[key]]).encode())
+os.close(output_file)
