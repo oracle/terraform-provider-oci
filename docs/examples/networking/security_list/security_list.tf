a7f8a55e9ef33e02f8c10548faa86870aad50c88
Brian Gustafson <brian.gustafson@oracle.com>
2017-11-20 14:35:55 -0800

    Add source port range to security list rules
    Currently, TCP and UDP options can be specified with a single set of
    max/min values, which correspond to the destingation port range. This change
    adds the ability to specify the source port range as well, issue #338.
    
    Ideally, we should deprecate the existing max/min params and add a new
    destination_port_range option next to the new source_port_range. Unfortunately,
    a number of issue with nested schemas in Terraform are preventing support of both
    of these params simultaneously. Instead, max/min params will remain, and source_port_range
    will exist beside it.
    
    This change also enables security lists with no rules.
    
    Some updates are made to the docs, but the docs for the security list resource and data source
    could really use a bigger update that is not included in this change.

--- docs/examples/networking/security_list/security_list.tf
+++ docs/examples/networking/security_list/security_list.tf
@@ -9,9 +9,6 @@ variable "private_key_path" {}
 variable "compartment_ocid" {}
 variable "region" {}
 
-variable "vcn_ocid" {}
-
-
 provider "oci" {
   tenancy_ocid = "${var.tenancy_ocid}"
   user_ocid = "${var.user_ocid}"
@@ -20,12 +17,25 @@ provider "oci" {
   region = "${var.region}"
 }
 
+resource "oci_core_virtual_network" "vcn1" {
+  cidr_block = "10.0.0.0/16"
+  dns_label = "vnc1"
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "vnc1"
+}
+
 # Protocols are specified as protocol numbers.
 # http://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
 
+resource "oci_core_security_list" "empty_list" {
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id = "${oci_core_virtual_network.vcn1.id}"
+  display_name = "empty_list"
+}
+
 resource "oci_core_security_list" "security_list1" {
   compartment_id = "${var.compartment_ocid}"
-  vcn_id = "${var.vcn_ocid}"
+  vcn_id = "${oci_core_virtual_network.vcn1.id}"
   display_name = "security_list1"
 
   // allow outbound tcp traffic on all ports
@@ -46,13 +56,18 @@ resource "oci_core_security_list" "security_list1" {
     }
   }
 
-  // allow inbound ssh traffic
+  // allow inbound ssh traffic from a specific port
   ingress_security_rules {
     protocol = "6" // tcp
     source = "0.0.0.0/0"
     stateless = false
 
     tcp_options {
+      source_port_range {
+        "min" = 100
+        "max" = 100
+      }
+      // These values correspond to the destination port range.
       "min" = 22
       "max" = 22
     }
