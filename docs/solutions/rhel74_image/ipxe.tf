bb476c92abd9e775fefd32157ae852bedda6bc70
Steven Nelson <nelsonse@gmail.com>
2017-11-28 17:05:09 -0800

    Create RHEL 7.4 bootable image (#339)
    This provides a fully automated process to create a RHEL 7.4 image in OCI. Consult the README for requirements.

---
+++ docs/solutions/rhel74_image/ipxe.tf
@@ -0,0 +1,33 @@
+# DO NOT ALTER THIS FILE
+
+resource "oci_core_instance" "ipxe_node" {
+  availability_domain = "${data.oci_identity_availability_domains.ad.availability_domains.0.name}"
+  compartment_id      = "${data.oci_identity_compartments.compartment.compartments.0.id}"
+  display_name        = "${var.ipxe_instance["name"]}"
+  image               = "${data.oci_core_images.image.images.0.id}" 
+  shape               = "${var.ipxe_instance["shape"]}"
+  subnet_id           = "${data.oci_core_subnets.subnet.subnets.0.id}"
+  hostname_label      = "${var.ipxe_instance["hostname"]}"
+
+  metadata {
+    ssh_authorized_keys = "${var.ssh_public_key}"
+    user_data = "${base64encode(file(data.external.ipxe_gen.result["shell"]))}"
+  }
+}
+
+resource "null_resource" "delete_ipxe" {
+  triggers {
+    ipxe_node_id = "${oci_core_instance.ipxe_node.id}"
+  }
+  provisioner "local-exec" {
+    command = "rm -rf ./ipxe.sh"
+  }
+}
+
+resource "null_resource" "delete_ipxe_destroy" {
+  provisioner "local-exec" {
+    when = "destroy"
+    command = "rm -rf ./ipxe.sh"
+  }
+}
+
---
+++ docs/solutions/rhel74_image/variable.tf
@@ -0,0 +1,37 @@
+# DO NOT ALTER THIS FILE
+
+provider "oci" {
+  tenancy_ocid         = "${var.tenancy_ocid}"
+  user_ocid            = "${var.user_ocid}"
+  fingerprint          = "${var.fingerprint}"
+  private_key_path     = "${var.private_key_path}"
+  private_key_password = "${var.private_key_password}"
+  region               = "${var.region}"
+}
+
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "private_key_password" {}
+variable "ssh_public_key" {}
+variable "region" {}
+
+variable "ipxe_instance" {
+	type = "map"
+	default = {
+		name = "ipxe-rhel74"
+		hostname = "ipxe-rhel74"
+		image = "Oracle-Linux-7.4-2017.09.29-0"
+		shape = "VM.Standard1.1"
+	}
+}
+
+variable "region_all_zeros_ocid" {
+	type = "map"
+	default = {
+		us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaay27pdopotkapf2ahjlsn2wxndui5hn5w37hd2wss4ses4ol5xs6a"
+		us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaaqftkoa5web2r7w4ls3wekgqmqy5f7untloetfiozyqbv2ql6qidq"
+		eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaah4rggbyglst25peqd7vnyjzl6n5lwogiyllb6jaircakom46nswq"
+	}
+}
