d14448926d4ba6cf8f9a92fd17b1064013a0e3a0
Brian Gustafson <bgusto@gmail.com>
2018-01-02 12:10:46 -0800

    Make examples testable (#398)
    Adds test cases TestExamplesPlan and TestExamplesApply, which will plan
    and apply every example under /docs/examples. (There's an exception for
    db_systems, which needs some work to make runnable using the stardard
    set of variables.) The tests ensure that all examples are runnable using
    the same set of variables.
    
    In the future we might consider updating this to enable concurrent runs.

--- docs/examples/networking/subnet/subnet.tf
+++ docs/examples/networking/subnet/subnet.tf
@@ -5,11 +5,6 @@ variable "private_key_path" {}
 variable "compartment_ocid" {}
 variable "region" {}
 
-variable "vcn_ocid" {}
-variable "dhcp_options_ocid" {}
-variable "route_table_ocid" {}
-variable "security_list_ocid" {}
-
 provider "oci" {
   tenancy_ocid = "${var.tenancy_ocid}"
   user_ocid = "${var.user_ocid}"
@@ -18,23 +13,31 @@ provider "oci" {
   region = "${var.region}"
 }
 
+resource "oci_core_virtual_network" "ExampleVCN" {
+  cidr_block = "10.1.0.0/16"
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "TFExampleVCN"
+  dns_label = "tfexamplevcn"
+}
+
 /* 
 Because you can specify multiple security lists/subnet the security_list_ids value must be specified as a list in []'s.
  See https://www.terraform.io/docs/configuration/syntax.html
    
 Generally you wouldn't specify a subnet without first specifying a VCN. Once the VCN has been created you would get the vcn_id, route_table_id, and security_list_id(s) from that resource and use Terraform attributes below to populate those values.
  See https://www.terraform.io/docs/configuration/interpolation.html*/
-
-resource "oci_core_subnet" "a_TF_managed_subnet" {
+resource "oci_core_subnet" "ExampleSubnet" {
   availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[0],"name")}"
-  cidr_block = "10.0.1.0/24"
-  display_name = "subnet1"
-  dns_label = "subnet1"
+  cidr_block = "10.1.1.0/24"
+  display_name = "TFExampleSubnet"
+  dns_label = "tfexamplesubnet"
   compartment_id = "${var.compartment_ocid}"
-  vcn_id = "${var.vcn_ocid}}"
-  security_list_ids = ["${var.security_list_ocid}"]
-  route_table_id = "${var.route_table_ocid}"
-  dhcp_options_id = "${var.dhcp_options_ocid}"
+  vcn_id = "${oci_core_virtual_network.ExampleVCN.id}}"
+  security_list_ids = ["${oci_core_virtual_network.ExampleVCN.default_security_list_id}"]
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id = "${oci_core_virtual_network.ExampleVCN.id}"
+  route_table_id = "${oci_core_virtual_network.ExampleVCN.default_route_table_id}"
+  dhcp_options_id = "${oci_core_virtual_network.ExampleVCN.default_dhcp_options_id}"
 }
 
 data "oci_identity_availability_domains" "ADs" {
--- docs/examples/storage/nfs/compute.tf
+++ docs/examples/storage/nfs/compute.tf
@@ -5,7 +5,7 @@ resource "oci_core_instance" "TFInstance" {
   hostname_label = "NFSserver"
   image = "${lookup(data.oci_core_images.OLImageOCID.images[0], "id")}"
   shape = "${var.InstanceShape}"
-  subnet_id = "${var.SubnetOCID}"
+  subnet_id = "${oci_core_subnet.ExampleSubnet.id}"
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
     user_data = "${base64encode(file(var.BootStrapFile))}"
