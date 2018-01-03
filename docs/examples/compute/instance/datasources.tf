c65996837d2de80c82e324473a2e3b107b91ff2d
Brian Gustafson <bgusto@gmail.com>
2018-01-02 16:43:01 -0800

    Update compute examples (#403)
    My previous PR had left out the internet gateway necessary in the
    instance example to perform the remote exec. Also, switching the default
    shape in multi_vnic to a smaller shape.

--- docs/examples/compute/instance/datasources.tf
+++ docs/examples/compute/instance/datasources.tf
@@ -1,6 +1,6 @@
 # Gets a list of Availability Domains
 data "oci_identity_availability_domains" "ADs" {
-  compartment_id = "${var.tenancy_ocid}"
+    compartment_id = "${var.tenancy_ocid}"
 }
 
 # Gets the OCID of the image. This technique is for example purposes only. The results of oci_core_images may
@@ -12,12 +12,12 @@ data "oci_core_images" "OLImageOCID" {
 
 # Gets a list of vNIC attachments on the instance
 data "oci_core_vnic_attachments" "InstanceVnics" {
-compartment_id = "${var.compartment_ocid}" 
-availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
-instance_id = "${oci_core_instance.TFInstance.id}"
+    compartment_id = "${var.compartment_ocid}"
+    availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
+    instance_id = "${oci_core_instance.TFInstance.id}"
 } 
 
 # Gets the OCID of the first (default) vNIC
 data "oci_core_vnic" "InstanceVnic" {
-vnic_id = "${lookup(data.oci_core_vnic_attachments.InstanceVnics.vnic_attachments[0],"vnic_id")}"
+    vnic_id = "${lookup(data.oci_core_vnic_attachments.InstanceVnics.vnic_attachments[0],"vnic_id")}"
 }
