acb0de6412ce141f5bb67ba2e894bb643b513490
Steve Nelson <steve.b.nelson@oracle.com>
2018-03-09 10:37:01 -0800

    Removed comment from compartment main.tf

--- docs/solutions/sql_server_always_on_availability_groups/modules/iam/compartment/main.tf
+++ docs/solutions/sql_server_always_on_availability_groups/modules/iam/compartment/main.tf
@@ -1,5 +1,4 @@
 resource "oci_identity_compartment" "compartment" {
-#  count       = "${var.compartment_ocid == "" ? 1 : 0}"
   provider    = "oci.home"
   name        = "${var.compartment_name}"
   description = "${var.compartment_description}"
