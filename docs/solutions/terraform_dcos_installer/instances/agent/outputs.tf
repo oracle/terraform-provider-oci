7932fa6206bc59bf4abb8d94069bb7a06026ae49
cgong-github <changbin.gong@oracle.com>
2018-03-29 15:11:24 -0400

    Delete newly added files.

--- docs/solutions/terraform_dcos_installer/instances/agent/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Output the private and public IPs of the instance
-  
-output "private_ips" {
-  value = ["${oci_core_instance.dcos_agent.*.private_ip}"]
-}
-
-output "instance_public_ips" {
-  value = ["${oci_core_instance.dcos_agent.*.public_ip}"]
-}
--- docs/solutions/terraform_dcos_installer/network/vcn/datasources.tf
+++
@@ -1,5 +0,0 @@
-# Gets a list of availability domains
-data "oci_identity_availability_domains" "ADs" {
-  compartment_id = "${var.tenancy_ocid}"
-}
-
