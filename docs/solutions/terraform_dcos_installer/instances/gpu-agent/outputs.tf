b3fd0641a8df0323910bd29dc1d27bd886478d1d
cgong-github <changbin.gong@oracle.com>
2018-03-29 15:17:22 -0400

    Delete newly added files.

--- docs/solutions/terraform_dcos_installer/instances/gpu-agent/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Output the private and public IPs of the instance
-  
-output "private_ips" {
-  value = ["${oci_core_instance.dcos_gpu_agent.*.private_ip}"]
-}
-
-output "instance_public_ips" {
-  value = ["${oci_core_instance.dcos_gpu_agent.*.public_ip}"]
-}
--- docs/solutions/terraform_dcos_installer/instances/master/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Output the private and public IPs of the instance
-  
-output "private_ips" {
-  value = ["${oci_core_instance.dcos_master.*.private_ip}"]
-}
-
-output "public_ips" {
-  value = ["${oci_core_instance.dcos_master.*.public_ip}"]
-}
