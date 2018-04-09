3910af8675a8251eb6fcff559eb8203c339fcc12
Rohan Bhattacharjee <rohan.bhattacharjee@oracle.com>
2018-04-09 11:57:18 -0700

    Using newer images for Oracle Linux 7.4 OS. This address issue https://jira-sd.mc1.oracleiaas.com/projects/TOP/queues/691/TOP-31

--- provider/core_volume_attachment_resource_test.go
+++ provider/core_volume_attachment_resource_test.go
@@ -47,10 +47,11 @@ func (s *ResourceCoreVolumeAttachmentTestSuite) SetupTest() {
 	variable "InstanceImageOCID" {
 	  type = "map"
 	  default = {
-		// Oracle-provided image "Oracle-Linux-7.4-2017.12.18-0"
-		us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaasc56hnpnx7swoyd2fw5gyvbn3kcdmqc2guiiuvnztl2erth62xnq"
-		us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaaxrqeombwty6jyqgk3fraczdd63bv66xgfsqka4ktr7c57awr3p5a"
-		eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaayxmzu6n5hsntq4wlffpb4h6qh6z3uskpbm5v3v4egqlqvwicfbyq"
+		// Oracle-provided image "Oracle-Linux-7.4-2018.02.21-1"
+		us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaaupbfz5f5hdvejulmalhyb6goieolullgkpumorbvxlwkaowglslq"
+		us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaajlw3xfie2t5t52uegyhiq2npx7bqyu4uvi2zyu3w3mqayc2bxmaa"
+		eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt1.aaaaaaaa7d3fsb6272srnftyi4dphdgfjf6gurxqhmv6ileds7ba3m2gltxq"
+		uk-london-1 = "ocid1.image.oc1.uk-london1.aaaaaaaaa6h6gj6v4n56mqrbgnosskq63blyv2752g36zerymy63cfkojiiq"
 	  }
 	}
 
