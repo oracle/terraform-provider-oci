f7a17bd52538854cc6cd340ec6e343ccd70c89c8
Rohan Bhattacharjee <rohan.bhattacharjee@oracle.com>
2018-04-27 16:29:18 -0700

    Fixed image OCIDs for LHR and FRA regions

--- provider/core_instance_resource_test.go
+++ provider/core_instance_resource_test.go
@@ -52,11 +52,12 @@ func (s *ResourceCoreInstanceTestSuite) SetupTest() {
 	variable "InstanceImageOCID" {
 	  type = "map"
 	  default = {
+		// See https://docs.us-phoenix-1.oraclecloud.com/images/
 		// Oracle-provided image "Oracle-Linux-7.4-2018.02.21-1"
 		us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaaupbfz5f5hdvejulmalhyb6goieolullgkpumorbvxlwkaowglslq"
 		us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaajlw3xfie2t5t52uegyhiq2npx7bqyu4uvi2zyu3w3mqayc2bxmaa"
-		eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt1.aaaaaaaa7d3fsb6272srnftyi4dphdgfjf6gurxqhmv6ileds7ba3m2gltxq"
-		uk-london-1 = "ocid1.image.oc1.uk-london1.aaaaaaaaa6h6gj6v4n56mqrbgnosskq63blyv2752g36zerymy63cfkojiiq"
+		eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa7d3fsb6272srnftyi4dphdgfjf6gurxqhmv6ileds7ba3m2gltxq"
+		uk-london-1 = "ocid1.image.oc1.uk-london-1.aaaaaaaaa6h6gj6v4n56mqrbgnosskq63blyv2752g36zerymy63cfkojiiq"
 	  }
 	}`
 
