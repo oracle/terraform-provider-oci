ab9bbca898bcbc366923689b9018976d1fecfeb5
ccushing <cody.cushing@oracle.com>
2018-04-26 17:09:18 -0700

    Factor identity examples into single template * Factor multi-region example into distinct "concept" folder

---
+++ docs/examples/concepts/multi_region/availability_domains.tf
@@ -0,0 +1,34 @@
+/*
+ * This example file demonstrates how to read AD values from multiple regions and employ filters
+ * to isolate specific ADs.
+ */
+
+data "oci_identity_availability_domains" "ad-phx" {
+  compartment_id = "${var.tenancy_ocid}"
+
+  filter {
+    name = "name"
+    values = ["\\w*-AD-1"]
+    regex = true
+  }
+}
+
+data "oci_identity_availability_domains" "ad-iad" {
+  provider = "oci.iad"
+  compartment_id = "${var.tenancy_ocid}"
+
+  filter {
+    name = "name"
+    values = ["\\w*-AD-1"]
+    regex = true
+  }
+}
+
+
+output "ad-phx" {
+  value = "${data.oci_identity_availability_domains.ad-phx.availability_domains}"
+}
+
+output "ad-iad" {
+  value = "${data.oci_identity_availability_domains.ad-iad.availability_domains}"
+}
---
+++ docs/examples/concepts/multi_region/readme.md
@@ -0,0 +1,12 @@
+    #     ___  ____     _    ____ _     _____
+    #    / _ \|  _ \   / \  / ___| |   | ____|
+    #   | | | | |_) | / _ \| |   | |   |  _|
+    #   | |_| |  _ < / ___ | |___| |___| |___
+    #    \___/|_| \_/_/   \_\____|_____|_____|
+***
+
+## Multi-Region Provider Configuration
+
+This example demonstrates how to:
+* Define multiple OCI providers targeting different regions
+* List and filter Availability Domains across regions
