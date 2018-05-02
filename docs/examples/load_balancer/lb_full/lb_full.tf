d4df0d3bcee0900530e7e00aa7923ca0e31c6a96
Ramy Cohen <ramy.cohen.masri@oracle.com>
2018-05-01 17:58:59 -0700

    update lb_full example to include hostnames in the listener

--- docs/examples/load_balancer/lb_full/lb_full.tf
+++ docs/examples/load_balancer/lb_full/lb_full.tf
@@ -234,10 +234,25 @@ resource "oci_load_balancer_path_route_set" "test_path_route_set" {
     }
 }
 
+resource "oci_load_balancer_hostname" "test_hostname1" {
+    #Required
+    hostname = "app.example.com"
+    load_balancer_id = "${oci_load_balancer.lb1.id}"
+    name = "hostname1"
+}
+
+resource "oci_load_balancer_hostname" "test_hostname2" {
+    #Required
+    hostname = "app2.example.com"
+    load_balancer_id = "${oci_load_balancer.lb1.id}"
+    name = "hostname2"
+}
+
 resource "oci_load_balancer_listener" "lb-listener1" {
   load_balancer_id         = "${oci_load_balancer.lb1.id}"
   name                     = "http"
   default_backend_set_name = "${oci_load_balancer_backend_set.lb-bes1.id}"
+  hostname_names           = ["${oci_load_balancer_hostname.test_hostname1.name}", "${oci_load_balancer_hostname.test_hostname2.name}"]
   port                     = 80
   protocol                 = "HTTP"
   connection_configuration {
