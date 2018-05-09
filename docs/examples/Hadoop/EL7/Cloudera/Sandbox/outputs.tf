fb813f78ea953e0804e03bc96f9f029304ee4cec
Zachary Smith <37876896+smithzc@users.noreply.github.com>
2018-05-09 16:34:07 -0700

    Update outputs.tf

--- docs/examples/Hadoop/EL7/Cloudera/Sandbox/outputs.tf
+++ docs/examples/Hadoop/EL7/Cloudera/Sandbox/outputs.tf
@@ -4,6 +4,14 @@ output "Cloudera Manager Login" {
   value = ["http://${data.oci_core_vnic.sandbox_vnic.public_ip_address}:7180/cmf/"]
 }
 
+output "HUE Login" {
+  value = ["http://${data.oci_core_vnic.sandbox_vnic.public_ip_address}:8888/"]
+}
+
+output "Cloudera Guided Demo" {
+  value = ["http://${data.oci_core_vnic.sandbox_vnic.public_ip_address}:80/"]
+}
+
 output "Sandbox SSH" { 
   value = ["ssh -i ~/.ssh/id_rsa opc@${data.oci_core_vnic.sandbox_vnic.public_ip_address}"]
 }
