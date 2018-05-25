db4ca4c53f0929efc4abbc06c9d6027b18e69065
Zachary Smith <37876896+smithzc@users.noreply.github.com>
2018-05-25 07:56:26 -0700

    Update iscsi.sh

--- docs/solutions/Hadoop/EL7/Cloudera/N-Node/scripts/iscsi.sh
+++ docs/solutions/Hadoop/EL7/Cloudera/N-Node/scripts/iscsi.sh
@@ -3,21 +3,24 @@
 ## by Zachary Smith (Zachary.Smith@oracle.com)
 ## Last Update - March 2018
 
-#Look for all ISCSI devices in parallel
+#Look for all ISCSI devices in sequence, finish on first failure
 touch /tmp/iscsi.lock
-for i in `seq 2 32`; do 
-        wait="0"
-                while [ $wait = "0" ]; do
-                        iscsi_ct=`ps -ef | grep iscsiadm | grep -iv grep | wc -l`
-                        if [ $iscsi_ct -lt 5 ]; then
-                                sudo iscsiadm -m discoverydb -D -t sendtargets -p 169.254.2.$i:3260 &
-                                wait="1"
-                        else
-                                sleep 1
-                        fi
-                done
+done="0"
+echo -e "Mapping Block Volumes...."
+for i in `seq 2 32`; do
+        if [ $done = "0" ]; then
+                sudo iscsiadm -m discoverydb -D -t sendtargets -p 169.254.2.$i:3260
+                iscsi_chk=`echo -e $?`
+                if [ $iscsi_chk = "0" ]; then
+                        echo -e "Success for volume $((i-1))."
+                        continue
+                else
+                        echo -e "Completed - $((i-2)) volumes found."
+                        done="1"
+                fi
+        fi
 done;
-sleep 10 
+echo -e "Setting auto-startup for volumes."
 sudo iscsiadm -m node -l
 sudo iscsiadm -m node -n node.startup -v automatic
 rm -f /tmp/iscsi.lock
