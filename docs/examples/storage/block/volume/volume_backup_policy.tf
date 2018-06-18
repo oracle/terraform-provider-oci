8984d039d071b32835e87a620192b9b61cad7096
Ravi Tandon <ravi.tandon@oracle.com>
2018-06-18 14:24:38 -0700

    Add support for VolumeBackupPolicy and VolumeBackupPolicyAssignment

---
+++ docs/examples/storage/block/volume/volume_backup_policy.tf
@@ -0,0 +1,63 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+
+provider "oci" {
+    tenancy_ocid = "${var.tenancy_ocid}"
+    user_ocid = "${var.user_ocid}"
+    fingerprint = "${var.fingerprint}"
+    private_key_path = "${var.private_key_path}"
+    region = "${var.region}"
+}
+
+variable "DBSize" {
+  default = "50" // size in GBs, min: 50, max 16384
+}
+
+data "oci_identity_availability_domains" "ADs" {
+  compartment_id = "${var.tenancy_ocid}"
+}
+
+resource "oci_core_volume" "t" {
+  availability_domain = "${data.oci_identity_availability_domains.ADs.availability_domains.0.name}"
+  compartment_id = "${var.tenancy_ocid}"
+  display_name = "-tf-volume"
+  size_in_gbs = "${var.DBSize}"
+}
+
+data "oci_core_volume_backup_policies" "test_volume_backup_policies" {
+}
+
+output "policies" {
+  value = "${data.oci_core_volume_backup_policies.test_volume_backup_policies.volume_backup_policies}"
+}
+
+data "oci_core_volume_backup_policies" "test_volume_backup_policies_silver" {
+  filter {
+    name = "display_name"
+    values = [ "silver" ]
+  }
+}
+
+output "silver_policy_id" {
+  value = "${data.oci_core_volume_backup_policies.test_volume_backup_policies_silver.volume_backup_policies.0.id}"
+}
+
+resource "oci_core_volume_backup_policy_assignment" "test_backup_policy_assignment" {
+  asset_id = "${oci_core_volume.t.id}"
+  policy_id = "${data.oci_core_volume_backup_policies.test_volume_backup_policies_silver.volume_backup_policies.0.id}"
+}
+
+data "oci_core_volume_backup_policy_assignments" "test_backup_policy_assignments" {
+  asset_id = "${oci_core_volume.t.id}"
+  filter {
+    name = "id"
+    values = ["${oci_core_volume_backup_policy_assignment.test_backup_policy_assignment.id}"]
+  }
+}
+
+output "test_backup_policy_assignments" {
+  value = "${data.oci_core_volume_backup_policy_assignments.test_backup_policy_assignments.volume_backup_policy_assignments}"
+}
