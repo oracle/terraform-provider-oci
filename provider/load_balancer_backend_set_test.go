4d918c9207df64e3074e5a583a89070c816390a9
Mukarram Baig <mukarram.baig@oracle.com>
2018-07-13 11:25:51 -0700

    Enable generated tests for lb backend sets

--- provider/load_balancer_backend_set_test.go
+++ provider/load_balancer_backend_set_test.go
@@ -55,7 +55,7 @@ resource "oci_load_balancer_backend_set" "test_backend_set" {
 	}
 	ssl_configuration {
 		#Required
-		certificate_name = "${var.backend_set_ssl_configuration_certificate_name}"
+		certificate_name = "${oci_load_balancer_certificate.t.certificate_name}"
 
 		#Optional
 		verify_depth = "${var.backend_set_ssl_configuration_verify_depth}"
@@ -77,15 +77,23 @@ variable "backend_set_policy" { default = "LEAST_CONNECTIONS" }
 variable "backend_set_session_persistence_configuration_cookie_name" { default = "example_cookie" }
 variable "backend_set_session_persistence_configuration_disable_fallback" { default = false }
 variable "backend_set_ssl_configuration_certificate_name" { default = "example_certificate_bundle" }
-variable "backend_set_ssl_configuration_verify_depth" { default = "3" }
+variable "backend_set_ssl_configuration_verify_depth" { default = 6 }
 variable "backend_set_ssl_configuration_verify_peer_certificate" { default = false }
 
 `
-	BackendSetResourceDependencies = LoadBalancerPropertyVariables + LoadBalancerRequiredOnlyResource
+	BackendSetResourceDependencies = `
+	resource "oci_load_balancer_certificate" "t" {
+		load_balancer_id = "${oci_load_balancer_load_balancer.test_load_balancer.id}"
+		ca_certificate = "-----BEGIN CERTIFICATE-----\nMIIBNzCB4gIJAKtwJkxUgNpzMA0GCSqGSIb3DQEBCwUAMCMxITAfBgNVBAoTGElu\ndGVybmV0IFdpZGdpdHMgUHR5IEx0ZDAeFw0xNzA0MTIyMTU3NTZaFw0xODA0MTIy\nMTU3NTZaMCMxITAfBgNVBAoTGEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDBcMA0G\nCSqGSIb3DQEBAQUAA0sAMEgCQQDlM8lz3BFJA6zBlsF63k9ajPVq3Q1WQoHQ3j35\n08DRKIfwqfV+CxL63W3dZrwL4TrjqorP5CQ36+I6OWALH2zVAgMBAAEwDQYJKoZI\nhvcNAQELBQADQQCEjHVQJoiiVpIIvDWF+4YDRReVuwzrvq2xduWw7CIsDWlYuGZT\nQKVY6tnTy2XpoUk0fqUvMB/M2HGQ1WqZGHs6\n-----END CERTIFICATE-----"
+		certificate_name = "example_certificate_bundle"
+		private_key = "-----BEGIN RSA PRIVATE KEY-----\nMIIBOgIBAAJBAOUzyXPcEUkDrMGWwXreT1qM9WrdDVZCgdDePfnTwNEoh/Cp9X4L\nEvrdbd1mvAvhOuOqis/kJDfr4jo5YAsfbNUCAwEAAQJAJz8k4bfvJceBT2zXGIj0\noZa9d1z+qaSdwfwsNJkzzRyGkj/j8yv5FV7KNdSfsBbStlcuxUm4i9o5LXhIA+iQ\ngQIhAPzStAN8+Rz3dWKTjRWuCfy+Pwcmyjl3pkMPSiXzgSJlAiEA6BUZWHP0b542\nu8AizBT3b3xKr1AH2nkIx9OHq7F/QbECIHzqqpDypa8/QVuUZegpVrvvT/r7mn1s\nddS6cDtyJgLVAiEA1Z5OFQeuL2sekBRbMyP9WOW7zMBKakLL3TqL/3JCYxECIAkG\nl96uo1MjK/66X5zQXBG7F2DN2CbcYEz0r3c3vvfq\n-----END RSA PRIVATE KEY-----"
+		public_certificate = "-----BEGIN CERTIFICATE-----\nMIIBNzCB4gIJAKtwJkxUgNpzMA0GCSqGSIb3DQEBCwUAMCMxITAfBgNVBAoTGElu\ndGVybmV0IFdpZGdpdHMgUHR5IEx0ZDAeFw0xNzA0MTIyMTU3NTZaFw0xODA0MTIy\nMTU3NTZaMCMxITAfBgNVBAoTGEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDBcMA0G\nCSqGSIb3DQEBAQUAA0sAMEgCQQDlM8lz3BFJA6zBlsF63k9ajPVq3Q1WQoHQ3j35\n08DRKIfwqfV+CxL63W3dZrwL4TrjqorP5CQ36+I6OWALH2zVAgMBAAEwDQYJKoZI\nhvcNAQELBQADQQCEjHVQJoiiVpIIvDWF+4YDRReVuwzrvq2xduWw7CIsDWlYuGZT\nQKVY6tnTy2XpoUk0fqUvMB/M2HGQ1WqZGHs6\n-----END CERTIFICATE-----"
+	}
+
+` + LoadBalancerPropertyVariables + LoadBalancerRequiredOnlyResource
 )
 
 func TestLoadBalancerBackendSetResource_basic(t *testing.T) {
-	t.Skip("Skipping generated test for now as it has not been worked on.")
 	provider := testAccProvider
 	config := testProviderConfig()
 
@@ -146,7 +154,7 @@ func TestLoadBalancerBackendSetResource_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "session_persistence_configuration.0.disable_fallback", "false"),
 					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.0.certificate_name", "example_certificate_bundle"),
-					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.0.verify_depth", "3"),
+					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.0.verify_depth", "6"),
 					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.0.verify_peer_certificate", "false"),
 
 					func(s *terraform.State) (err error) {
@@ -172,8 +180,8 @@ variable "backend_set_policy" { default = "LEAST_CONNECTIONS" }
 variable "backend_set_session_persistence_configuration_cookie_name" { default = "example_cookie" }
 variable "backend_set_session_persistence_configuration_disable_fallback" { default = true }
 variable "backend_set_ssl_configuration_certificate_name" { default = "example_certificate_bundle" }
-variable "backend_set_ssl_configuration_verify_depth" { default = 11 }
-variable "backend_set_ssl_configuration_verify_peer_certificate" { default = true }
+variable "backend_set_ssl_configuration_verify_depth" { default = 6 }
+variable "backend_set_ssl_configuration_verify_peer_certificate" { default = false }
 
                 ` + compartmentIdVariableStr + BackendSetResourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
@@ -194,8 +202,8 @@ variable "backend_set_ssl_configuration_verify_peer_certificate" { default = tru
 					resource.TestCheckResourceAttr(resourceName, "session_persistence_configuration.0.disable_fallback", "true"),
 					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.0.certificate_name", "example_certificate_bundle"),
-					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.0.verify_depth", "11"),
-					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.0.verify_peer_certificate", "true"),
+					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.0.verify_depth", "6"),
+					resource.TestCheckResourceAttr(resourceName, "ssl_configuration.0.verify_peer_certificate", "false"),
 
 					func(s *terraform.State) (err error) {
 						resId2, err = fromInstanceState(s, resourceName, "id")
@@ -222,17 +230,12 @@ variable "backend_set_policy" { default = "LEAST_CONNECTIONS" }
 variable "backend_set_session_persistence_configuration_cookie_name" { default = "example_cookie" }
 variable "backend_set_session_persistence_configuration_disable_fallback" { default = true }
 variable "backend_set_ssl_configuration_certificate_name" { default = "example_certificate_bundle" }
-variable "backend_set_ssl_configuration_verify_depth" { default = 11 }
-variable "backend_set_ssl_configuration_verify_peer_certificate" { default = true }
+variable "backend_set_ssl_configuration_verify_depth" { default = 6 }
+variable "backend_set_ssl_configuration_verify_peer_certificate" { default = false }
 
 data "oci_load_balancer_backend_sets" "test_backend_sets" {
 	#Required
 	load_balancer_id = "${oci_load_balancer_load_balancer.test_load_balancer.id}"
-
-    filter {
-    	name = "id"
-    	values = ["${oci_load_balancer_backend_set.test_backend_set.id}"]
-    }
 }
                 ` + compartmentIdVariableStr + BackendSetResourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
@@ -248,7 +251,6 @@ data "oci_load_balancer_backend_sets" "test_backend_sets" {
 					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.health_checker.0.return_code", "11"),
 					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.health_checker.0.timeout_in_millis", "11"),
 					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.health_checker.0.url_path", "urlPath2"),
-					resource.TestCheckResourceAttrSet(datasourceName, "backendsets.0.load_balancer_id"),
 					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.name", "backendSet1"),
 					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.policy", "LEAST_CONNECTIONS"),
 					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.session_persistence_configuration.#", "1"),
@@ -256,8 +258,8 @@ data "oci_load_balancer_backend_sets" "test_backend_sets" {
 					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.session_persistence_configuration.0.disable_fallback", "true"),
 					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.ssl_configuration.#", "1"),
 					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.ssl_configuration.0.certificate_name", "example_certificate_bundle"),
-					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.ssl_configuration.0.verify_depth", "11"),
-					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.ssl_configuration.0.verify_peer_certificate", "true"),
+					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.ssl_configuration.0.verify_depth", "6"),
+					resource.TestCheckResourceAttr(datasourceName, "backendsets.0.ssl_configuration.0.verify_peer_certificate", "false"),
 				),
 			},
 		},
