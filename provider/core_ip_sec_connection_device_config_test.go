013ed33edbdb442e0c2bf54f928be276fdffec78
Mukarram Baig <mukarram.baig@oracle.com>
2018-07-16 14:18:46 -0700

    Fix generated tests for IPSecConfig device config and device status

---
+++ provider/core_ip_sec_connection_device_config_test.go
@@ -0,0 +1,60 @@
+// Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+const (
+	IpSecConnectionDeviceConfigResourceConfig = IpSecConnectionDeviceConfigResourceDependencies + `
+
+`
+	IpSecConnectionDeviceConfigPropertyVariables = `
+
+`
+	IpSecConnectionDeviceConfigResourceDependencies = IpSecConnectionPropertyVariables + IpSecConnectionRequiredOnlyResource
+)
+
+func TestCoreIpSecConnectionDeviceConfigResource_basic(t *testing.T) {
+	provider := testAccProvider
+	config := testProviderConfig()
+
+	compartmentId := getRequiredEnvSetting("compartment_ocid")
+	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
+
+	singularDatasourceName := "data.oci_core_ipsec_config.test_ip_sec_connection_device_config"
+
+	resource.Test(t, resource.TestCase{
+		Providers: map[string]terraform.ResourceProvider{
+			"oci": provider,
+		},
+		Steps: []resource.TestStep{
+			// verify singular datasource
+			{
+				Config: config + `
+
+data "oci_core_ipsec_config" "test_ip_sec_connection_device_config" {
+	#Required
+	ipsec_id = "${oci_core_ipsec.test_ip_sec_connection.id}"
+}
+                ` + compartmentIdVariableStr + IpSecConnectionDeviceConfigResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "ipsec_id"),
+
+					resource.TestCheckResourceAttr(singularDatasourceName, "compartment_id", compartmentId),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "id"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "time_created"),
+					resource.TestCheckResourceAttr(singularDatasourceName, "tunnels.#", "2"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "tunnels.0.ip_address"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "tunnels.0.shared_secret"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "tunnels.0.time_created"),
+				),
+			},
+		},
+	})
+}
