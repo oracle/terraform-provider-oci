c6c5e0836946f6c5cb3d5563bcbbd951671caa14
Alex Ng <alexander.ng@oracle.com>
2018-09-10 17:32:58 -0700

    Remove all solutions from terraform-provider-oci repo.
    The new home for solutions is: https://github.com/oracle/terraform-examples/tree/master/examples/oci

--- docs/solutions/chef/cookbooks/example_webserver/README.md
+++
@@ -1,4 +0,0 @@
-# example_webserver
-
-This recipe installs a web server and configure the firewall to allow inbound http and ssh connections.
-
--- docs/solutions/sql_server_always_on_availability_groups/modules/instances/bastion/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "private_ip" {
-  value = "${oci_core_instance.instance.*.private_ip}"
-}
-
-output "public_ip" {
-  value = "${oci_core_instance.instance.*.public_ip}"
-}
