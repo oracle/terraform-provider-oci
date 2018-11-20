28998333db3bbafd21d87e01deeeca1388815c39
ccushing <cody.cushing@oracle.com>
2018-11-19 18:44:15 -0800

    Explicitly set compartmentID for User, Group and Compartment resources in Identity example * describe `enable_delete` behavior for Compartment resource in notes

--- docs/examples/identity/group.tf
+++ docs/examples/identity/group.tf
@@ -5,6 +5,7 @@
 resource "oci_identity_group" "group1" {
   name        = "tf-example-group"
   description = "group created by terraform"
+  compartment_id = "${var.tenancy_ocid}"
 }
 
 resource "oci_identity_user_group_membership" "user-group-mem1" {
