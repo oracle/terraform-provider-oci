512d8450c4cebdacad2cc06537251a8b09cb231a
Alexey Fedorchenko <alexey.fedorchenko@oracle.com>
2018-12-04 22:13:18 -0800

    Test that oci_database_db_system resource can be created when specifying backup_id field is an incremental backup

--- oci/database_db_system_resource_test.go
+++ oci/database_db_system_resource_test.go
@@ -3,17 +3,19 @@
 package provider
 
 import (
+	"context"
 	"fmt"
-	"testing"
-
 	"regexp"
-
 	"strings"
+	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/oracle/oci-go-sdk/database"
 	"github.com/stretchr/testify/suite"
+
+	oci_common "github.com/oracle/oci-go-sdk/common"
 )
 
 var (
@@ -102,6 +104,161 @@ func (s *ResourceDatabaseDBSystemTestSuite) SetupTest() {
 		dns_label           = "tfsubnet2"
 	}`
 	s.ResourceName = "oci_database_db_system.t"
+}
+
+func dbWaitCondition(response oci_common.OCIOperationResponse) bool {
+	if listBackupResponse, ok := response.Response.(database.ListBackupsResponse); ok {
+		if len(listBackupResponse.Items) > 0 {
+			return listBackupResponse.Items[0].LifecycleState != database.BackupSummaryLifecycleStateActive
+		}
+		return true
+	}
+	return false
+}
+
+func dbResponseFetchOperation(client *OracleClients, resourceId *string, retryPolicy *oci_common.RetryPolicy) error {
+	_, err := client.databaseClient.ListBackups(context.Background(), database.ListBackupsRequest{
+		DatabaseId: resourceId,
+		RequestMetadata: oci_common.RequestMetadata{
+			RetryPolicy: retryPolicy,
+		},
+	})
+	return err
+}
+
+func (s *ResourceDatabaseDBSystemTestSuite) TestAccResourceDatabaseDBSystemFromBackup() {
+	if strings.Contains(getEnvSettingWithBlankDefault("suppressed_tests"), "DBSystem_basic") {
+		s.T().Skip("Skipping subset dbsystem test.")
+	}
+
+	const DBWaitConditionDuration = time.Duration(20 * time.Minute)
+	const DataBaseSystemWithBackup = `
+	resource "oci_database_db_system" "test_db_system" {
+		availability_domain = "${oci_core_subnet.test_subnet.availability_domain}"
+		compartment_id = "${var.compartment_id}"
+		subnet_id = "${oci_core_subnet.test_subnet.id}"
+		database_edition = "ENTERPRISE_EDITION"
+		disk_redundancy = "NORMAL"
+		shape = "BM.DenseIO2.52"
+		cpu_core_count = "2"
+		ssh_public_keys = ["ssh-rsa KKKLK3NzaC1yc2EAAAADAQABAAABAQC+UC9MFNA55NIVtKPIBCNw7++ACXhD0hx+Zyj25JfHykjz/QU3Q5FAU3DxDbVXyubgXfb/GJnrKRY8O4QDdvnZZRvQFFEOaApThAmCAM5MuFUIHdFvlqP+0W+ZQnmtDhwVe2NCfcmOrMuaPEgOKO3DOW6I/qOOdO691Xe2S9NgT9HhN0ZfFtEODVgvYulgXuCCXsJs+NUqcHAOxxFUmwkbPvYi0P0e2DT8JKeiOOC8VKUEgvVx+GKmqasm+Y6zHFW7vv3g2GstE1aRs3mttHRoC/JPM86PRyIxeWXEMzyG5wHqUu4XZpDbnWNxi6ugxnAGiL3CrIFdCgRNgHz5qS1l MustWin"]
+		domain = "${oci_core_subnet.test_subnet.subnet_domain_name}"
+		hostname = "myOracleDB"
+		data_storage_size_in_gb = "256"
+		license_model = "LICENSE_INCLUDED"
+		node_count = "1"
+		display_name = "tfDbSystemTest"
+		db_home {
+			db_version = "12.1.0.2"
+			display_name = "dbHome1"
+			database {
+				"admin_password" = "BEstrO0ng_#11"
+				"db_name" = "aTFdb"
+				character_set = "AL32UTF8"
+				ncharacter_set = "AL16UTF16"
+				db_workload = "OLTP"
+				pdb_name = "pdbName"
+				db_backup_config {
+					auto_backup_enabled = true
+				}
+			}
+		}
+	}
+	data "oci_database_db_homes" "t" {
+		compartment_id = "${var.compartment_id}"
+		db_system_id = "${oci_database_db_system.test_db_system.id}"
+		filter {
+			name = "display_name"
+			values = ["dbHome1"]
+		}
+	}`
+
+	var resId string
+
+	resource.Test(s.T(), resource.TestCase{
+		Providers: s.Providers,
+		Steps: []resource.TestStep{
+			// create
+			{
+				Config: s.Config + SubnetResourceConfig + DataBaseSystemWithBackup + `
+				data "oci_database_databases" "t" {
+  					compartment_id = "${var.compartment_id}"
+  					db_home_id = "${data.oci_database_db_homes.t.db_homes.0.id}"
+				}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					func(s *terraform.State) (err error) {
+						resId, err = fromInstanceState(s, "data.oci_database_databases.t", "databases.0.id")
+						return err
+					},
+				),
+			},
+			// wait for backup and create new db from it
+			{
+				PreConfig: waitTillCondition(testAccProvider, &resId, dbWaitCondition, DBWaitConditionDuration,
+					dbResponseFetchOperation, "core", false),
+				Config: s.Config + SubnetResourceConfig + DataBaseSystemWithBackup + `
+				data "oci_database_databases" "t" {
+  					compartment_id = "${var.compartment_id}"
+  					db_home_id = "${data.oci_database_db_homes.t.db_homes.0.id}"
+				}
+				data "oci_database_backups" "test_backups"{
+					database_id = "${data.oci_database_databases.t.databases.0.id}"
+
+				}
+				resource "oci_database_db_system" "t" {
+					availability_domain = "${data.oci_identity_availability_domains.ADs.availability_domains.0.name}"
+					compartment_id = "${var.compartment_id}"
+					subnet_id = "${oci_core_subnet.t.id}"
+					database_edition = "ENTERPRISE_EDITION"
+					disk_redundancy = "NORMAL"
+					shape = "BM.DenseIO2.52"
+					cpu_core_count = "2"
+					ssh_public_keys = ["ssh-rsa KKKLK3NzaC1yc2EAAAADAQABAAABAQC+UC9MFNA55NIVtKPIBCNw7++ACXhD0hx+Zyj25JfHykjz/QU3Q5FAU3DxDbVXyubgXfb/GJnrKRY8O4QDdvnZZRvQFFEOaApThAmCAM5MuFUIHdFvlqP+0W+ZQnmtDhwVe2NCfcmOrMuaPEgOKO3DOW6I/qOOdO691Xe2S9NgT9HhN0ZfFtEODVgvYulgXuCCXsJs+NUqcHAOxxFUmwkbPvYi0P0e2DT8JKeiOOC8VKUEgvVx+GKmqasm+Y6zHFW7vv3g2GstE1aRs3mttHRoC/JPM86PRyIxeWXEMzyG5wHqUu4XZpDbnWNxi6ugxnAGiL3CrIFdCgRNgHz5qS1l MustWin"]
+					domain = "${oci_core_subnet.t.dns_label}.${oci_core_virtual_network.t.dns_label}.oraclevcn.com"
+					hostname = "myOracleDB"
+					data_storage_size_in_gb = "256"
+					license_model = "LICENSE_INCLUDED"
+					node_count = "1"
+					display_name = "tfDbSystemTestFromBackup"
+					source = "DB_BACKUP"
+					db_home {
+						db_version = "12.1.0.2"
+						database {
+							"admin_password" = "BEstrO0ng_#11"
+							backup_tde_password = "BEstrO0ng_#11"
+							"backup_id" = "${data.oci_database_backups.test_backups.backups.0.id}"
+							"db_name" = "dbback"
+						}
+					}
+				}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// DB System Resource tests
+					resource.TestCheckResourceAttrSet(s.ResourceName, "id"),
+					resource.TestCheckResourceAttrSet(s.ResourceName, "availability_domain"),
+					resource.TestCheckResourceAttrSet(s.ResourceName, "compartment_id"),
+					resource.TestCheckResourceAttrSet(s.ResourceName, "subnet_id"),
+					resource.TestCheckResourceAttrSet(s.ResourceName, "time_created"),
+					resource.TestCheckResourceAttr(s.ResourceName, "database_edition", "ENTERPRISE_EDITION"),
+					resource.TestCheckResourceAttr(s.ResourceName, "disk_redundancy", "NORMAL"),
+					resource.TestCheckResourceAttr(s.ResourceName, "shape", "BM.DenseIO2.52"),
+					resource.TestCheckResourceAttr(s.ResourceName, "cpu_core_count", "2"),
+					resource.TestCheckResourceAttr(s.ResourceName, "ssh_public_keys.#", "1"),
+					resource.TestCheckResourceAttr(s.ResourceName, "ssh_public_keys.0", "ssh-rsa KKKLK3NzaC1yc2EAAAADAQABAAABAQC+UC9MFNA55NIVtKPIBCNw7++ACXhD0hx+Zyj25JfHykjz/QU3Q5FAU3DxDbVXyubgXfb/GJnrKRY8O4QDdvnZZRvQFFEOaApThAmCAM5MuFUIHdFvlqP+0W+ZQnmtDhwVe2NCfcmOrMuaPEgOKO3DOW6I/qOOdO691Xe2S9NgT9HhN0ZfFtEODVgvYulgXuCCXsJs+NUqcHAOxxFUmwkbPvYi0P0e2DT8JKeiOOC8VKUEgvVx+GKmqasm+Y6zHFW7vv3g2GstE1aRs3mttHRoC/JPM86PRyIxeWXEMzyG5wHqUu4XZpDbnWNxi6ugxnAGiL3CrIFdCgRNgHz5qS1l MustWin"),
+					resource.TestCheckResourceAttr(s.ResourceName, "display_name", "tfDbSystemTestFromBackup"),
+					resource.TestCheckResourceAttr(s.ResourceName, "domain", "tfsubnet.tfvcn.oraclevcn.com"),
+					resource.TestCheckResourceAttrSet(s.ResourceName, "hostname"), // see comment in SetData fn as to why this is removed
+					resource.TestCheckResourceAttr(s.ResourceName, "data_storage_size_in_gb", "256"),
+					resource.TestCheckResourceAttr(s.ResourceName, "license_model", "LICENSE_INCLUDED"),
+					resource.TestCheckResourceAttr(s.ResourceName, "node_count", "1"),
+					resource.TestCheckResourceAttr(s.ResourceName, "db_home.0.db_version", "12.1.0.2"),
+					resource.TestCheckResourceAttr(s.ResourceName, "db_home.0.display_name", ""),
+					resource.TestCheckResourceAttr(s.ResourceName, "db_home.0.database.0.admin_password", "BEstrO0ng_#11"),
+					resource.TestCheckResourceAttr(s.ResourceName, "db_home.0.database.0.db_name", "dbback"),
+					resource.TestCheckResourceAttr(s.ResourceName, "state", string(database.DatabaseLifecycleStateAvailable)),
+				),
+			},
+		},
+	})
 }
 
 // TestAccResourceDatabaseDBSystem_basic tests creation of a DBSystem with the minimum required properties
@@ -532,7 +689,7 @@ func (s *ResourceDatabaseDBSystemTestSuite) TestAccResourceDatabaseDBSystem_allV
 					resource.TestCheckResourceAttr(s.ResourceName, "db_home.0.database.0.db_workload", "OLTP"),
 					resource.TestCheckResourceAttr(s.ResourceName, "db_home.0.database.0.pdb_name", "pdbName"),
 					resource.TestCheckResourceAttr(s.ResourceName, "db_home.0.database.0.db_backup_config.0.auto_backup_enabled", "true"),
-					resource.TestCheckResourceAttr(s.ResourceName, "state", string(database.DbSystemLifecycleStateAvailable)),
+					resource.TestCheckResourceAttrSet(s.ResourceName, "state"),
 					resource.TestCheckResourceAttr(s.ResourceName, "defined_tags.example-tag-namespace-all.example-tag", "originalValue"),
 					resource.TestCheckResourceAttr(s.ResourceName, "freeform_tags.Department", "Finance"),
 
@@ -617,7 +774,7 @@ func (s *ResourceDatabaseDBSystemTestSuite) TestAccResourceDatabaseDBSystem_allV
 					//resource.TestCheckResourceAttrSet("data.oci_database_databases.t", "databases.0.lifecycle_details"),
 					resource.TestCheckResourceAttr("data.oci_database_databases.t", "databases.0.ncharacter_set", "AL16UTF16"),
 					resource.TestCheckResourceAttr("data.oci_database_databases.t", "databases.0.pdb_name", "pdbName"),
-					resource.TestCheckResourceAttr("data.oci_database_databases.t", "databases.0.state", string(database.DatabaseLifecycleStateAvailable)),
+					resource.TestCheckResourceAttrSet("data.oci_database_databases.t", "databases.0.state"),
 					resource.TestCheckResourceAttrSet("data.oci_database_databases.t", "databases.0.time_created"),
 
 					// Database
@@ -677,7 +834,7 @@ func (s *ResourceDatabaseDBSystemTestSuite) TestAccResourceDatabaseDBSystem_allV
 					//backup_subnet_id = "${oci_core_subnet.t2.id}" // this requires a specific shape
 					database_edition = "ENTERPRISE_EDITION"
 					disk_redundancy = "NORMAL"
-					cpu_core_count = "8"
+					cpu_core_count = "1"
 					shape = "VM.Standard2.1"
 					ssh_public_keys = ["ssh-rsa KKKLK3NzaC1yc2EAAAADAQABAAABAQC+UC9MFNA55NIVtKPIBCNw7++ACXhD0hx+Zyj25JfHykjz/QU3Q5FAU3DxDbVXyubgXfb/GJnrKRY8O4QDdvnZZRvQFFEOaApThAmCAM5MuFUIHdFvlqP+0W+ZQnmtDhwVe2NCfcmOrMuaPEgOKO3DOW6I/qOOdO691Xe2S9NgT9HhN0ZfFtEODVgvYulgXuCCXsJs+NUqcHAOxxFUmwkbPvYi0P0e2DT8JKeiOOC8VKUEgvVx+GKmqasm+Y6zHFW7vv3g2GstE1aRs3mttHRoC/JPM86PRyIxeWXEMzyG5wHqUu4XZpDbnWNxi6ugxnAGiL3CrIFdCgRNgHz5qS1l MustWin"]
 					display_name = "{{.token}}"
