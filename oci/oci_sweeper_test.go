d81cab6fd023acdc21b628b62fb877e678e843f3
Abhilash Annaiayappa Vijayakumara <abhilash.vijayakumara@oracle.com>
2018-12-20 10:17:59 -0800

    Remove load_balancer_rule_set_test.go as the UDX feature is not yet ready to be merged into Master

--- oci/oci_sweeper_test.go
+++ oci/oci_sweeper_test.go
@@ -55,7 +55,6 @@ func init() {
 	initCorePublicIpSweeper()
 	initCoreRemotePeeringConnectionSweeper()
 	initCoreRouteTableSweeper()
-	initLoadBalancerRuleSetSweeper()
 	initCoreSecurityListSweeper()
 	initEmailSenderSweeper()
 	initCoreServiceGatewaySweeper()
