f5a19513d06d319c03d69d3f97dc73a0ea82fd62
Alex Ng <alexander.ng@oracle.com>
2019-01-07 17:18:36 -0800

    Fix image examples to avoid using datasource as input to instances
    This practice is discouraged as images may be removed or altered by the service, causing unnecessary diffs to userplans. Instead, we encourage a known image OCID to be specified to avoid unexpected plan changes.

--- docs/examples/compute/image/datasources.tf
+++ docs/examples/compute/image/datasources.tf
@@ -5,7 +5,7 @@ data "oci_identity_availability_domains" "ADs" {
 
 # Gets the custom image that will be created by this Terraform config
 data "oci_core_images" "TFCustomImage" {
-  compartment_id = "${var.tenancy_ocid}"
+  compartment_id = "${var.compartment_ocid}"
 
   filter {
     name   = "id"
@@ -13,10 +13,16 @@ data "oci_core_images" "TFCustomImage" {
   }
 }
 
-# Gets a list of all Oracle Linux 7.4 images that support a given Instance shape
+# Gets a list of images within a tenancy
 data "oci_core_images" "TFSupportedShapeImages" {
-  compartment_id           = "${var.tenancy_ocid}"
-  shape                    = "${var.instance_shape}"
-  operating_system         = "${var.ImageOS}"
-  operating_system_version = "${var.ImageOSVersion}"
+  compartment_id = "${var.tenancy_ocid}"
+
+  # Uncomment below to filter images that support a specific instance shape 
+  #shape                    = "VM.Standard2.1"
+
+  # Uncomment below to filter images that are a specific OS
+  #operating_system         = "Oracle Linux"
+
+  # Uncomment below to filter images that are a specific OS version 
+  #operating_system_version = "7.5"
 }
