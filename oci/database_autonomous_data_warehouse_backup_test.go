2474922218bac6ade01b4f5f8b29a234754e66e0
Ravi Tandon <ravi.tandon@oracle.com>
2019-01-31 12:06:11 -0800

    Update Object Storage Lifecycle tests to use random buckets and consolidate other tests for same random string generation

--- oci/database_autonomous_data_warehouse_backup_test.go
+++ oci/database_autonomous_data_warehouse_backup_test.go
@@ -47,7 +47,7 @@ func TestDatabaseAutonomousDataWarehouseBackupResource_basic(t *testing.T) {
 	datasourceName := "data.oci_database_autonomous_data_warehouse_backups.test_autonomous_data_warehouse_backups"
 	singularDatasourceName := "data.oci_database_autonomous_data_warehouse_backup.test_autonomous_data_warehouse_backup"
 
-	testResourceName := GenerateTestResourceName("adwdb1", 14)
+	testResourceName := randomString(14, charset)
 	setEnvSetting("TF_VAR_autonomous_data_warehouse_db_name", testResourceName)
 
 	resource.Test(t, resource.TestCase{
