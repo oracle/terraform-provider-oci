fa6009176a8e5017cc0d8c75b5d13c4980be7949
Parneet Kaur <parneet.p.kaur@oracle.com>
2019-05-07 15:17:23 -0700

    Update object storage example
    	* Demonstrate object download using local-exec
    	* Refactor example for names

--- examples/object_storage/object.tf
+++ examples/object_storage/object.tf
@@ -1,6 +1,7 @@
 // Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
 
 /* This example demonstrates object store object management. It uses Terraforms built-in `file` function to upload a file.
+ * It also demonstrates the use of `local-exec` to download the object content using a preauthenticated request.
  *
  * WARNING: This should only be used with small files. The file helper does stringification so large files
  * may cause terraform to slow, become unresponsive or exceed allowed memory usage.
@@ -15,7 +16,7 @@ resource "oci_objectstorage_object" "object1" {
   content          = "${file("index.html")}"
 }
 
-resource "oci_objectstorage_object" "source-object" {
+resource "oci_objectstorage_object" "source_object" {
   namespace        = "${data.oci_objectstorage_namespace.ns.namespace}"
   bucket           = "${oci_objectstorage_bucket.bucket1.name}"
   object           = "same_index.html"
@@ -24,7 +25,7 @@ resource "oci_objectstorage_object" "source-object" {
   source           = "index.html"
 }
 
-resource "oci_objectstorage_object" "source-uri-object" {
+resource "oci_objectstorage_object" "source_uri_object" {
   namespace        = "${data.oci_objectstorage_namespace.ns.namespace}"
   bucket           = "${oci_objectstorage_bucket.bucket1.name}"
   object           = "copy_index.html"
@@ -39,16 +40,16 @@ resource "oci_objectstorage_object" "source-uri-object" {
   }
 }
 
-data "oci_objectstorage_object_head" "object-head1" {
+data "oci_objectstorage_object_head" "object_head1" {
   namespace = "${data.oci_objectstorage_namespace.ns.namespace}"
   bucket    = "${oci_objectstorage_bucket.bucket1.name}"
   object    = "${oci_objectstorage_object.object1.object}"
 }
 
-data "oci_objectstorage_object_head" "source-object-head" {
+data "oci_objectstorage_object_head" "source_object_head" {
   namespace = "${data.oci_objectstorage_namespace.ns.namespace}"
   bucket    = "${oci_objectstorage_bucket.bucket1.name}"
-  object    = "${oci_objectstorage_object.source-object.object}"
+  object    = "${oci_objectstorage_object.source_object.object}"
 }
 
 data "oci_objectstorage_objects" "objects1" {
@@ -62,7 +63,7 @@ data "oci_objectstorage_object" "object" {
   object    = "index.html"
 }
 
-output object-data {
+output object_data {
   value = <<EOF
 
   content = ${data.oci_objectstorage_object.object.content}
@@ -74,21 +75,40 @@ EOF
 output object-head-data {
   value = <<EOF
 
-  object = ${data.oci_objectstorage_object_head.object-head1.object}
-  content-length = ${data.oci_objectstorage_object_head.object-head1.content-length}
-  content-type = ${data.oci_objectstorage_object_head.object-head1.content-type}
+  object = ${data.oci_objectstorage_object_head.object_head1.object}
+  content-length = ${data.oci_objectstorage_object_head.object_head1.content-length}
+  content-type = ${data.oci_objectstorage_object_head.object_head1.content-type}
 EOF
 }
 
 output object-source-head-data {
   value = <<EOF
 
-  object = ${data.oci_objectstorage_object_head.source-object-head.object}
-  content-length = ${data.oci_objectstorage_object_head.source-object-head.content-length}
-  content-type = ${data.oci_objectstorage_object_head.source-object-head.content-type}
+  object = ${data.oci_objectstorage_object_head.source_object_head.object}
+  content-length = ${data.oci_objectstorage_object_head.source_object_head.content-length}
+  content-type = ${data.oci_objectstorage_object_head.source_object_head.content-type}
 EOF
 }
 
 output objects {
   value = "${data.oci_objectstorage_objects.objects1.objects}"
+}
+
+# example to download object content to local file using object PAR with local-exec
+
+resource "null_resource" "download_object_content" {
+  # using command
+  provisioner "local-exec" {
+    command = "curl -o object-content https://objectstorage.${var.region}.oraclecloud.com${oci_objectstorage_preauthrequest.object_par.access_uri}"
+  }
+
+  # using script
+  provisioner "local-exec" {
+    command = "sh get-content.sh https://objectstorage.${var.region}.oraclecloud.com${oci_objectstorage_preauthrequest.object_par.access_uri} object-content-using-script"
+  }
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "rm -rf object-content content/"
+  }
 }
