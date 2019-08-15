53af6500286c599d8405255390933735ab5438f8
Abhilash Annaiayappa Vijayakumara <abhilash.vijayakumara@oracle.com>
2019-08-15 14:17:11 -0700

    Support for Automate clustered networking setup for HPC

---
+++ oci/core_cluster_network_instances_data_source.go
@@ -0,0 +1,246 @@
+// Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	oci_core "github.com/oracle/oci-go-sdk/core"
+)
+
+func CoreClusterNetworkInstancesDataSource() *schema.Resource {
+	return &schema.Resource{
+		Read: readCoreClusterNetworkInstances,
+		Schema: map[string]*schema.Schema{
+			"filter": dataSourceFiltersSchema(),
+			"cluster_network_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"compartment_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"display_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"instances": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						// Required
+
+						// Optional
+
+						// Computed
+						"availability_domain": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"compartment_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"display_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"fault_domain": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"instance_configuration_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"load_balancer_backends": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									// Required
+
+									// Optional
+
+									// Computed
+									"backend_health_status": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"backend_name": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"backend_set_name": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"load_balancer_id": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									// internal for work request access
+									"state": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"region": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"shape": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"state": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"time_created": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func readCoreClusterNetworkInstances(d *schema.ResourceData, m interface{}) error {
+	sync := &CoreClusterNetworkInstancesDataSourceCrud{}
+	sync.D = d
+	sync.Client = m.(*OracleClients).computeManagementClient
+
+	return ReadResource(sync)
+}
+
+type CoreClusterNetworkInstancesDataSourceCrud struct {
+	D      *schema.ResourceData
+	Client *oci_core.ComputeManagementClient
+	Res    *oci_core.ListClusterNetworkInstancesResponse
+}
+
+func (s *CoreClusterNetworkInstancesDataSourceCrud) VoidState() {
+	s.D.SetId("")
+}
+
+func (s *CoreClusterNetworkInstancesDataSourceCrud) Get() error {
+	request := oci_core.ListClusterNetworkInstancesRequest{}
+
+	if clusterNetworkId, ok := s.D.GetOkExists("cluster_network_id"); ok {
+		tmp := clusterNetworkId.(string)
+		request.ClusterNetworkId = &tmp
+	}
+
+	if compartmentId, ok := s.D.GetOkExists("compartment_id"); ok {
+		tmp := compartmentId.(string)
+		request.CompartmentId = &tmp
+	}
+
+	if displayName, ok := s.D.GetOkExists("display_name"); ok {
+		tmp := displayName.(string)
+		request.DisplayName = &tmp
+	}
+
+	request.RequestMetadata.RetryPolicy = getRetryPolicy(false, "core")
+
+	response, err := s.Client.ListClusterNetworkInstances(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	s.Res = &response
+	request.Page = s.Res.OpcNextPage
+
+	for request.Page != nil {
+		listResponse, err := s.Client.ListClusterNetworkInstances(context.Background(), request)
+		if err != nil {
+			return err
+		}
+
+		s.Res.Items = append(s.Res.Items, listResponse.Items...)
+		request.Page = listResponse.OpcNextPage
+	}
+
+	return nil
+}
+
+func (s *CoreClusterNetworkInstancesDataSourceCrud) SetData() error {
+	if s.Res == nil {
+		return nil
+	}
+
+	s.D.SetId(GenerateDataSourceID())
+	resources := []map[string]interface{}{}
+
+	for _, r := range s.Res.Items {
+		clusterNetworkInstance := map[string]interface{}{
+			"compartment_id": *r.CompartmentId,
+		}
+
+		if r.AvailabilityDomain != nil {
+			clusterNetworkInstance["availability_domain"] = *r.AvailabilityDomain
+		}
+
+		if r.DisplayName != nil {
+			clusterNetworkInstance["display_name"] = *r.DisplayName
+		}
+
+		if r.FaultDomain != nil {
+			clusterNetworkInstance["fault_domain"] = *r.FaultDomain
+		}
+
+		if r.Id != nil {
+			clusterNetworkInstance["id"] = *r.Id
+		}
+
+		if r.InstanceConfigurationId != nil {
+			clusterNetworkInstance["instance_configuration_id"] = *r.InstanceConfigurationId
+		}
+
+		loadBalancerBackends := []interface{}{}
+		for _, item := range r.LoadBalancerBackends {
+			loadBalancerBackends = append(loadBalancerBackends, InstancePoolInstanceLoadBalancerBackendToMap(item))
+		}
+		clusterNetworkInstance["load_balancer_backends"] = loadBalancerBackends
+
+		if r.Region != nil {
+			clusterNetworkInstance["region"] = *r.Region
+		}
+
+		if r.Shape != nil {
+			clusterNetworkInstance["shape"] = *r.Shape
+		}
+
+		if r.State != nil {
+			clusterNetworkInstance["state"] = *r.State
+		}
+
+		if r.TimeCreated != nil {
+			clusterNetworkInstance["time_created"] = r.TimeCreated.String()
+		}
+
+		resources = append(resources, clusterNetworkInstance)
+	}
+
+	if f, fOk := s.D.GetOkExists("filter"); fOk {
+		resources = ApplyFilters(f.(*schema.Set), resources, CoreClusterNetworkInstancesDataSource().Schema["instances"].Elem.(*schema.Resource).Schema)
+	}
+
+	if err := s.D.Set("instances", resources); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ website/docs/d/core_cluster_network_instances.html.markdown
@@ -0,0 +1,63 @@
+---
+layout: "oci"
+page_title: "Oracle Cloud Infrastructure: oci_core_cluster_network_instances"
+sidebar_current: "docs-oci-datasource-core-cluster_network_instances"
+description: |-
+  Provides the list of Cluster Network Instances in Oracle Cloud Infrastructure Core service
+---
+
+# Data Source: oci_core_cluster_network_instances
+This data source provides the list of Cluster Network Instances in Oracle Cloud Infrastructure Core service.
+
+List the instances in the specified cluster network.
+
+## Example Usage
+
+```hcl
+data "oci_core_cluster_network_instances" "test_cluster_network_instances" {
+	#Required
+	cluster_network_id = "${oci_core_cluster_network.test_cluster_network.id}"
+	compartment_id = "${var.compartment_id}"
+
+	#Optional
+	display_name = "${var.cluster_network_instance_display_name}"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `cluster_network_id` - (Required) The OCID of the cluster network.
+* `compartment_id` - (Required) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment.
+* `display_name` - (Optional) A filter to return only resources that match the given display name exactly. 
+
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `instances` - The list of instances.
+
+### ClusterNetworkInstance Reference
+
+The following attributes are exported:
+
+* `availability_domain` - The availability domain the instance is running in.
+* `compartment_id` - The OCID of the compartment that contains the instance.
+* `display_name` - The user-friendly name.  Does not have to be unique.
+* `fault_domain` - The name of the Fault Domain the instance is running in.
+* `id` - The OCID of the instance.
+* `instance_configuration_id` - The OCID of the instance confgiuration used to create the instance.
+* `load_balancer_backends` - The load balancer backends that are configured for the instance pool instance. 
+	* `backend_health_status` - The health of the backend as observed by the load balancer.
+	* `backend_name` - The name of the backend in the backend set.
+	* `backend_set_name` - The name of the backend set on the load balancer.
+	* `load_balancer_id` - The OCID of the load balancer attached to the instance pool.
+* `region` - The region that contains the availability domain the instance is running in.
+* `shape` - The shape of an instance. The shape determines the number of CPUs, amount of memory, and other resources allocated to the instance.
+
+	You can enumerate all available shapes by calling [ListShapes](https://docs.cloud.oracle.com/iaas/api/#/en/iaas/20160918/Shape/ListShapes). 
+* `state` - The current state of the instance pool instance.
+* `time_created` - The date and time the instance pool instance was created, in the format defined by RFC3339. Example: `2016-08-25T21:10:29.600Z` 
+
