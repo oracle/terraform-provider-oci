ca907d9c58316b5efb9fea3b6954c3879d775c76
jiangong <jian.gong@oracle.com>
2019-09-09 15:49:37 -0700

    Support IP-based policy

---
+++ oci/identity_network_source_data_source.go
@@ -0,0 +1,105 @@
+// Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+
+package oci
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	oci_identity "github.com/oracle/oci-go-sdk/identity"
+)
+
+func IdentityNetworkSourceDataSource() *schema.Resource {
+	fieldMap := make(map[string]*schema.Schema)
+	fieldMap["network_source_id"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Required: true,
+	}
+	return GetSingularDataSourceItemSchema(IdentityNetworkSourceResource(), fieldMap, readSingularIdentityNetworkSource)
+}
+
+func readSingularIdentityNetworkSource(d *schema.ResourceData, m interface{}) error {
+	sync := &IdentityNetworkSourceDataSourceCrud{}
+	sync.D = d
+	sync.Client = m.(*OracleClients).identityClient
+
+	return ReadResource(sync)
+}
+
+type IdentityNetworkSourceDataSourceCrud struct {
+	D      *schema.ResourceData
+	Client *oci_identity.IdentityClient
+	Res    *oci_identity.GetNetworkSourceResponse
+}
+
+func (s *IdentityNetworkSourceDataSourceCrud) VoidState() {
+	s.D.SetId("")
+}
+
+func (s *IdentityNetworkSourceDataSourceCrud) Get() error {
+	request := oci_identity.GetNetworkSourceRequest{}
+
+	if networkSourceId, ok := s.D.GetOkExists("network_source_id"); ok {
+		tmp := networkSourceId.(string)
+		request.NetworkSourceId = &tmp
+	}
+
+	request.RequestMetadata.RetryPolicy = getRetryPolicy(false, "identity")
+
+	response, err := s.Client.GetNetworkSource(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	s.Res = &response
+	return nil
+}
+
+func (s *IdentityNetworkSourceDataSourceCrud) SetData() error {
+	if s.Res == nil {
+		return nil
+	}
+
+	s.D.SetId(*s.Res.Id)
+
+	if s.Res.CompartmentId != nil {
+		s.D.Set("compartment_id", *s.Res.CompartmentId)
+	}
+
+	if s.Res.DefinedTags != nil {
+		s.D.Set("defined_tags", definedTagsToMap(s.Res.DefinedTags))
+	}
+
+	if s.Res.Description != nil {
+		s.D.Set("description", *s.Res.Description)
+	}
+
+	s.D.Set("freeform_tags", s.Res.FreeformTags)
+
+	if s.Res.InactiveStatus != nil {
+		s.D.Set("inactive_state", strconv.FormatInt(*s.Res.InactiveStatus, 10))
+	}
+
+	if s.Res.Name != nil {
+		s.D.Set("name", *s.Res.Name)
+	}
+
+	s.D.Set("public_source_list", s.Res.PublicSourceList)
+
+	s.D.Set("services", s.Res.Services)
+
+	s.D.Set("state", s.Res.LifecycleState)
+
+	if s.Res.TimeCreated != nil {
+		s.D.Set("time_created", s.Res.TimeCreated.String())
+	}
+
+	virtualSourceList := []interface{}{}
+	for _, item := range s.Res.VirtualSourceList {
+		virtualSourceList = append(virtualSourceList, networkSourcesVirtualSourceListToMap(item))
+	}
+	s.D.Set("virtual_source_list", virtualSourceList)
+
+	return nil
+}
---
+++ oci/identity_network_sources_data_source.go
@@ -0,0 +1,130 @@
+// Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+
+package oci
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	oci_identity "github.com/oracle/oci-go-sdk/identity"
+)
+
+func IdentityNetworkSourcesDataSource() *schema.Resource {
+	return &schema.Resource{
+		Read: readIdentityNetworkSources,
+		Schema: map[string]*schema.Schema{
+			"filter": dataSourceFiltersSchema(),
+			"compartment_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"network_sources": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     GetDataSourceItemSchema(IdentityNetworkSourceResource()),
+			},
+		},
+	}
+}
+
+func readIdentityNetworkSources(d *schema.ResourceData, m interface{}) error {
+	sync := &IdentityNetworkSourcesDataSourceCrud{}
+	sync.D = d
+	sync.Client = m.(*OracleClients).identityClient
+
+	return ReadResource(sync)
+}
+
+type IdentityNetworkSourcesDataSourceCrud struct {
+	D      *schema.ResourceData
+	Client *oci_identity.IdentityClient
+	Res    *oci_identity.ListNetworkSourcesResponse
+}
+
+func (s *IdentityNetworkSourcesDataSourceCrud) VoidState() {
+	s.D.SetId("")
+}
+
+func (s *IdentityNetworkSourcesDataSourceCrud) Get() error {
+	request := oci_identity.ListNetworkSourcesRequest{}
+
+	if compartmentId, ok := s.D.GetOkExists("compartment_id"); ok {
+		tmp := compartmentId.(string)
+		request.CompartmentId = &tmp
+	}
+
+	request.RequestMetadata.RetryPolicy = getRetryPolicy(false, "identity")
+
+	response, err := s.Client.ListNetworkSources(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	s.Res = &response
+	request.Page = s.Res.OpcNextPage
+
+	for request.Page != nil {
+		listResponse, err := s.Client.ListNetworkSources(context.Background(), request)
+		if err != nil {
+			return err
+		}
+
+		s.Res.Items = append(s.Res.Items, listResponse.Items...)
+		request.Page = listResponse.OpcNextPage
+	}
+
+	return nil
+}
+
+func (s *IdentityNetworkSourcesDataSourceCrud) SetData() error {
+	if s.Res == nil {
+		return nil
+	}
+
+	s.D.SetId(GenerateDataSourceID())
+	resources := []map[string]interface{}{}
+
+	for _, r := range s.Res.Items {
+		networkSource := map[string]interface{}{
+			"compartment_id": *r.CompartmentId,
+		}
+
+		if r.Description != nil {
+			networkSource["description"] = *r.Description
+		}
+
+		if r.Id != nil {
+			networkSource["id"] = *r.Id
+		}
+
+		if r.Name != nil {
+			networkSource["name"] = *r.Name
+		}
+
+		networkSource["public_source_list"] = r.PublicSourceList
+
+		networkSource["services"] = r.Services
+
+		if r.TimeCreated != nil {
+			networkSource["time_created"] = r.TimeCreated.String()
+		}
+
+		virtualSourceList := []interface{}{}
+		for _, item := range r.VirtualSourceList {
+			virtualSourceList = append(virtualSourceList, networkSourcesVirtualSourceListToMap(item))
+		}
+		networkSource["virtual_source_list"] = virtualSourceList
+
+		resources = append(resources, networkSource)
+	}
+
+	if f, fOk := s.D.GetOkExists("filter"); fOk {
+		resources = ApplyFilters(f.(*schema.Set), resources, IdentityNetworkSourcesDataSource().Schema["network_sources"].Elem.(*schema.Resource).Schema)
+	}
+
+	if err := s.D.Set("network_sources", resources); err != nil {
+		return err
+	}
+
+	return nil
+}
