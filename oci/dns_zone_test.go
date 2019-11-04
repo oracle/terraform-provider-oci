f2576511185da29a58c42a94d45e8bee2c7c484d
John Truong <john.truong@oracle.com>
2019-11-04 13:01:30 -0800

    Using resource name to look up the service name

--- oci/dns_zone_test.go
+++ oci/dns_zone_test.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"fmt"
 	"regexp"
+	"strconv"
 	"testing"
 	"time"
 
@@ -170,6 +171,11 @@ func TestDnsZoneResource_basic(t *testing.T) {
 
 					func(s *terraform.State) (err error) {
 						resId, err = fromInstanceState(s, resourceName, "id")
+						if isEnableExportCompartment, _ := strconv.ParseBool(getEnvSettingWithDefault("enable_export_compartment", "false")); isEnableExportCompartment {
+							if errExport := testExportCompartmentWithResourceName(&resId, &compartmentId, resourceName); errExport != nil {
+								return errExport
+							}
+						}
 						return err
 					},
 				),
