e3b9079ee4f832bec6aad2eff7576b1f79e037a4
Alexey Fedorchenko <alexey.fedorchenko@oracle.com>
2019-11-27 14:37:00 -0800

    change resource manager stack tests to use pre-created stack

--- oci/resourcemanager_stack_tf_state_test.go
+++ oci/resourcemanager_stack_tf_state_test.go
@@ -4,8 +4,11 @@ package oci
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"testing"
+
+	"github.com/hashicorp/terraform/helper/schema"
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
@@ -15,7 +18,7 @@ import (
 
 var (
 	stackTfStateSingularDataSourceRepresentation = map[string]interface{}{
-		"stack_id":   Representation{repType: Required, create: `${data.oci_resourcemanager_stacks.test_stacks.stacks[0].id}`},
+		"stack_id":   Representation{repType: Required, create: `${var.resource_manager_stack_id}`},
 		"local_path": Representation{repType: Required, create: `test.tfstate`},
 	}
 
@@ -33,20 +36,34 @@ func TestResourcemanagerStackTfStateResource_basic(t *testing.T) {
 	provider := testAccProvider
 	config := testProviderConfig()
 
+	client := GetTestClients(&schema.ResourceData{}).resourceManagerClient
+
 	compartmentId := getEnvSettingWithBlankDefault("compartment_ocid")
 	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
+
+	resourceManagerStackId, err := createResourceManagerStack(*client, "TestResourcemanagerStackTfStateResource_basic", compartmentId)
+	if err != nil {
+		t.Errorf("cannot create resource manager stack for the test run: %v", err)
+	}
 
 	singularDatasourceName := "data.oci_resourcemanager_stack_tf_state.test_stack_tf_state"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() { testAccPreCheck(t) },
+		CheckDestroy: func(s *terraform.State) error {
+			os.Remove("test.tfstate")
+			return destroyResourceManagerStack(*client, resourceManagerStackId)
+		},
+		PreventPostDestroyRefresh: true,
 		Providers: map[string]terraform.ResourceProvider{
 			"oci": provider,
 		},
 		Steps: []resource.TestStep{
 			// verify singular datasource
 			{
-				Config: config +
+				Config: config + `
+					variable "resource_manager_stack_id" { default = "` + resourceManagerStackId + `" }
+					` +
 					generateDataSourceFromRepresentationMap("oci_resourcemanager_stack_tf_state", "test_stack_tf_state", Required, Create, stackTfStateSingularDataSourceRepresentation) +
 					compartmentIdVariableStr + StackTfStateResourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
