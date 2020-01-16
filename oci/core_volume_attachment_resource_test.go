826e50c29ff3a63472adae83cec5f0860abcda33
Alexey Fedorchenko <alexey.fedorchenko@oracle.com>
2020-01-16 11:17:38 -0800

    core test changes to fix CI runs

--- oci/core_volume_attachment_resource_test.go
+++ oci/core_volume_attachment_resource_test.go
@@ -47,18 +47,6 @@ func (s *ResourceCoreVolumeAttachmentTestSuite) SetupTest() {
 		security_list_ids = ["${oci_core_virtual_network.t.default_security_list_id}"]
 	}
 
-	variable "InstanceImageOCID" {
-	  type = "map"
-	  default = {
-		// See https://docs.us-phoenix-1.oraclecloud.com/images/
-		// Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
-		us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaadjnj3da72bztpxinmqpih62c2woscbp6l3wjn36by2cvmdhjub6a"
-		us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaawufnve5jxze4xf7orejupw5iq3pms6cuadzjc7klojix6vmk42va"
-		eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaagbrvhganmn7awcr7plaaf5vhabmzhx763z5afiitswjwmzh7upna"
-		uk-london-1 = "ocid1.image.oc1.uk-london-1.aaaaaaaajwtut4l7fo3cvyraate6erdkyf2wdk5vpk6fp6ycng3dv2y3ymvq"
-	  }
-	}
-
 	resource "oci_core_instance" "t" {
 		availability_domain = "${data.oci_identity_availability_domains.ADs.availability_domains.0.name}"
 		compartment_id = "${var.compartment_id}"
@@ -66,6 +54,7 @@ func (s *ResourceCoreVolumeAttachmentTestSuite) SetupTest() {
 		image = "${var.InstanceImageOCID[var.region]}"
 		shape = "VM.Standard2.1"
 		subnet_id = "${oci_core_subnet.t.id}"
+		is_pv_encryption_in_transit_enabled = "true"
 		metadata = {
 			ssh_authorized_keys = "${var.ssh_public_key}"
 		}
@@ -81,6 +70,7 @@ func (s *ResourceCoreVolumeAttachmentTestSuite) SetupTest() {
 		image = "${var.InstanceImageOCID[var.region]}"
 		shape = "VM.Standard2.1"
 		subnet_id = "${oci_core_subnet.t.id}"
+		is_pv_encryption_in_transit_enabled = "true"
 		metadata = {
 			ssh_authorized_keys = "${var.ssh_public_key}"
 		}
@@ -94,7 +84,8 @@ func (s *ResourceCoreVolumeAttachmentTestSuite) SetupTest() {
 		compartment_id = "${var.compartment_id}"
 		display_name = "display_name"
 	}
-`
+` + OciImageIdsVariable
+
 	s.ResourceName[0] = "oci_core_volume_attachment.t"
 	s.ResourceName[1] = "oci_core_volume_attachment.t2"
 }
