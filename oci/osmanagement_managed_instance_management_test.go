ef5175f92a5186262a606173e08e06234d801549
rashik_bhasin <rashik.bhasin@oracle.com>
2020-07-12 18:06:20 -0700

    OS Management test fixed

--- oci/osmanagement_managed_instance_management_test.go
+++ oci/osmanagement_managed_instance_management_test.go
@@ -5,7 +5,9 @@ package oci
 
 import (
 	"fmt"
+	"log"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
@@ -21,7 +23,7 @@ var (
 		generateResourceFromRepresentationMap("oci_osmanagement_managed_instance_group", "test_managed_instance_group", Optional, Update, managedInstanceGroupRepresentation)
 
 	ManagedInstanceManagementRepresentation = map[string]interface{}{
-		"managed_instance_id":    Representation{repType: Required, create: `${var.managed_instance_id}`},
+		"managed_instance_id":    Representation{repType: Required, create: `${oci_core_instance.test_instance.id}`},
 		"parent_software_source": RepresentationGroup{Optional, parentSoftwareSourceRepresentation},
 	}
 
@@ -40,6 +42,26 @@ var (
 		"id":           Representation{repType: Required, create: `${oci_osmanagement_managed_instance_group.test_managed_instance_group.id}`},
 	}
 
+	vnicDetailsRepresentation = map[string]interface{}{
+		"assign_public_ip": Representation{repType: Required, create: `true`},
+		"subnet_id":        Representation{repType: Required, create: `${oci_core_subnet.test_subnet.id}`},
+	}
+
+	routeTablesRepresentation = map[string]interface{}{
+		"manage_default_resource_id": Representation{repType: Required, create: `${oci_core_vcn.test_vcn.default_route_table_id}`},
+		"route_rules":                RepresentationGroup{Required, routeRulesRepresentation},
+	}
+
+	routeRulesRepresentation = map[string]interface{}{
+		"cidr_block":        Representation{repType: Required, create: `0.0.0.0/0`},
+		"network_entity_id": Representation{repType: Required, create: `${oci_core_internet_gateway.test_internet_gateway.id}`},
+	}
+
+	sourceDetailsRepresentation = map[string]interface{}{
+		"source_type": Representation{repType: Required, create: `image`},
+		"source_id":   Representation{repType: Required, create: `${var.OsManagedImageOCID[var.region]}`},
+	}
+
 	ManagedInstanceManagementResourceDependencies = generateResourceFromRepresentationMap("oci_osmanagement_software_source", "test_parent_software_source", Required, Create, getUpdatedRepresentationCopy("arch_type", Representation{repType: Required, create: `X86_64`}, softwareSourceRepresentation)) +
 		generateResourceFromRepresentationMap("oci_osmanagement_software_source", "test_child_software_source", Required, Create, representationCopyWithNewProperties(getMultipleUpdatedRepresenationCopy([]string{"arch_type", "display_name"},
 			[]interface{}{
@@ -48,7 +70,21 @@ var (
 			}, softwareSourceRepresentation),
 			map[string]interface{}{
 				"parent_id": Representation{repType: Required, create: `${oci_osmanagement_software_source.test_parent_software_source.id}`},
-			})) + generateResourceFromRepresentationMap("oci_osmanagement_managed_instance_group", "test_managed_instance_group", Required, Create, managedInstanceGroupRepresentation)
+			})) +
+		generateResourceFromRepresentationMap("oci_osmanagement_managed_instance_group", "test_managed_instance_group", Required, Create, managedInstanceGroupRepresentation) +
+		generateResourceFromRepresentationMap("oci_core_instance", "test_instance", Required, Create, representationCopyWithNewProperties(instanceRepresentation, map[string]interface{}{
+			"create_vnic_details": RepresentationGroup{Required, vnicDetailsRepresentation},
+			"source_details":      RepresentationGroup{Required, sourceDetailsRepresentation},
+			"image":               Representation{repType: Required, create: `${var.OsManagedImageOCID[var.region]}`},
+		})) +
+		generateResourceFromRepresentationMap("oci_core_vcn", "test_vcn", Required, Create, representationCopyWithNewProperties(vcnRepresentation, map[string]interface{}{
+			"dns_label":  Representation{repType: Required, create: `testvcn`},
+			"cidr_block": Representation{repType: Required, create: `10.1.0.0/16`},
+		})) +
+		generateResourceFromRepresentationMap("oci_core_internet_gateway", "test_internet_gateway", Required, Create, internetGatewayRepresentation) +
+		generateResourceFromRepresentationMap("oci_core_default_route_table", "default_route_table", Required, Create, routeTablesRepresentation) +
+		generateResourceFromRepresentationMap("oci_core_subnet", "test_subnet", Required, Create, representationCopyWithNewProperties(subnetRepresentation, map[string]interface{}{"availability_domain": Representation{repType: Required, create: `${lower("${data.oci_identity_availability_domains.test_availability_domains.availability_domains.0.name}")}`}, "cidr_block": Representation{repType: Required, create: `10.1.20.0/24`}, "dns_label": Representation{repType: Required, create: `testsubnet`}, "route_table_id": Representation{repType: Required, create: `${oci_core_vcn.test_vcn.default_route_table_id}`}})) +
+		AvailabilityDomainConfig + OsManagedImageIdsVariable
 )
 
 func TestOsmanagementManagedInstanceManagementResource_basic(t *testing.T) {
@@ -61,9 +97,6 @@ func TestOsmanagementManagedInstanceManagementResource_basic(t *testing.T) {
 	compartmentId := getEnvSettingWithBlankDefault("compartment_ocid")
 	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
 
-	managedInstanceId := getEnvSettingWithBlankDefault("managed_instance_id")
-	managedInstanceIdVariableStr := fmt.Sprintf("variable \"managed_instance_id\" { default = \"%s\" }\n", managedInstanceId)
-
 	resourceName := "oci_osmanagement_managed_instance_management.test_managed_instance_management"
 
 	resource.Test(t, resource.TestCase{
@@ -72,9 +105,18 @@ func TestOsmanagementManagedInstanceManagementResource_basic(t *testing.T) {
 			"oci": provider,
 		},
 		Steps: []resource.TestStep{
+			// create dependencies
+			{
+				Config: config + compartmentIdVariableStr + ManagedInstanceManagementResourceDependencies,
+				Check: func(s *terraform.State) (err error) {
+					log.Printf("[DEBUG] OS Management Resource should be created after 5 minutes as OS Agent times to activate")
+					time.Sleep(5 * time.Minute)
+					return nil
+				},
+			},
 			// verify create
 			{
-				Config: config + compartmentIdVariableStr + managedInstanceIdVariableStr + ManagedInstanceManagementResourceDependencies +
+				Config: config + compartmentIdVariableStr + ManagedInstanceManagementResourceDependencies +
 					generateResourceFromRepresentationMap("oci_osmanagement_managed_instance_management", "test_managed_instance_management", Required, Create, ManagedInstanceManagementRepresentation),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet(resourceName, "managed_instance_id"),
@@ -87,11 +129,11 @@ func TestOsmanagementManagedInstanceManagementResource_basic(t *testing.T) {
 			},
 			// delete before next create
 			{
-				Config: config + compartmentIdVariableStr + managedInstanceIdVariableStr + ManagedInstanceManagementResourceDependencies,
+				Config: config + compartmentIdVariableStr + ManagedInstanceManagementResourceDependencies,
 			},
 			// verify create with optionals
 			{
-				Config: config + compartmentIdVariableStr + managedInstanceIdVariableStr + ManagedInstanceManagementResourceDependencies +
+				Config: config + compartmentIdVariableStr + ManagedInstanceManagementResourceDependencies +
 					generateResourceFromRepresentationMap("oci_osmanagement_managed_instance_management", "test_managed_instance_management", Optional, Create, ManagedInstanceManagementRepresentation),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet(resourceName, "managed_instance_id"),
@@ -99,7 +141,7 @@ func TestOsmanagementManagedInstanceManagementResource_basic(t *testing.T) {
 			},
 			// verify update with optionals
 			{
-				Config: config + compartmentIdVariableStr + managedInstanceIdVariableStr + ManagedInstanceManagementResourceDependencies +
+				Config: config + compartmentIdVariableStr + ManagedInstanceManagementResourceDependencies +
 					generateResourceFromRepresentationMap("oci_osmanagement_managed_instance_management", "test_managed_instance_management", Optional, Create, representationCopyWithNewProperties(ManagedInstanceManagementRepresentation, map[string]interface{}{
 						"child_software_sources":  RepresentationGroup{Optional, childSoftwareSourceRepresentation},
 						"managed_instance_groups": RepresentationGroup{Optional, managedInstanceGroupsRepresentation},
