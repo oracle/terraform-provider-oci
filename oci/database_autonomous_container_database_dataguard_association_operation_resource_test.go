a483affc40b83f28b6be9f9b810ad38173675c53
zexin wan <zexin.wan@oracle.com>
2020-10-10 03:33:18 -0700

    added scenario test for ADB-D dataguard operation and fix doc issue

---
+++ oci/database_autonomous_container_database_dataguard_association_operation_resource_test.go
@@ -0,0 +1,156 @@
+// Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package oci
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+
+	"github.com/terraform-providers/terraform-provider-oci/httpreplay"
+)
+
+var (
+	AutonomousContainerDatabaseDataguardAssociationOperationSwitchOverResourceConfig = generateResourceFromRepresentationMap("oci_database_autonomous_container_database_dataguard_association_operation", "switchover", Optional, Create,
+		map[string]interface{}{
+			"operation":                        Representation{repType: Required, create: `switchover`},
+			"autonomous_container_database_id": Representation{repType: Required, create: `${oci_database_autonomous_container_database.test_autonomous_container_database.id}`},
+			"autonomous_container_database_dataguard_association_id": Representation{repType: Required, create: `${data.oci_database_autonomous_container_database_dataguard_associations.test.autonomous_container_database_dataguard_associations[0]["id"]}`},
+		})
+	AutonomousContainerDatabaseDataguardAssociationOperationFailOverResourceConfig = generateResourceFromRepresentationMap("oci_database_autonomous_container_database_dataguard_association_operation", "failover", Optional, Create,
+		map[string]interface{}{
+			"operation":                        Representation{repType: Required, create: `failover`},
+			"autonomous_container_database_id": Representation{repType: Required, create: `${oci_database_autonomous_container_database.test_autonomous_container_database.id}`},
+			"autonomous_container_database_dataguard_association_id": Representation{repType: Required, create: `${data.oci_database_autonomous_container_database_dataguard_associations.test.autonomous_container_database_dataguard_associations[0]["id"]}`},
+		})
+	AutonomousContainerDatabaseDataguardAssociationOperationReinstateResourceConfig = generateResourceFromRepresentationMap("oci_database_autonomous_container_database_dataguard_association_operation", "reinstate", Optional, Create,
+		map[string]interface{}{
+			"operation":                        Representation{repType: Required, create: `reinstate`},
+			"autonomous_container_database_id": Representation{repType: Required, create: `${data.oci_database_autonomous_container_database_dataguard_associations.test.autonomous_container_database_dataguard_associations[0]["peer_autonomous_container_database_id"]}`},
+			"autonomous_container_database_dataguard_association_id": Representation{repType: Required, create: `${data.oci_database_autonomous_container_database_dataguard_associations.test.autonomous_container_database_dataguard_associations[0]["peer_autonomous_container_database_dataguard_association_id"]}`},
+		})
+)
+
+func TestDatabaseAutonomousContainerDatabaseDataguardAssociationOperationResource_basic(t *testing.T) {
+	httpreplay.SetScenario("TestDatabaseAutonomousContainerDatabaseDataguardAssociationOperationResource_basic")
+	defer httpreplay.SaveScenario()
+
+	provider := testAccProvider
+	config := testProviderConfig()
+
+	compartmentId := getEnvSettingWithBlankDefault("compartment_ocid")
+	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
+
+	datasourceName := "data.oci_database_autonomous_container_database_dataguard_associations.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		Providers: map[string]terraform.ResourceProvider{
+			"oci": provider,
+		},
+		Steps: []resource.TestStep{
+			// verify create
+			{
+				Config: config +
+					generateDataSourceFromRepresentationMap("oci_database_autonomous_container_database_dataguard_associations", "test", Optional, Create, autonomousContainerDatabaseDataguardAssociationDataSourceRepresentation) +
+					compartmentIdVariableStr + AutonomousContainerDatabaseDataguardAssociationResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_id"),
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.#"),
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.0.autonomous_container_database_id"),
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.0.id"),
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.0.peer_autonomous_container_database_dataguard_association_id"),
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.0.peer_autonomous_container_database_id"),
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.0.peer_lifecycle_state"),
+					resource.TestCheckResourceAttr(datasourceName, "autonomous_container_database_dataguard_associations.0.peer_role", "STANDBY"),
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.0.protection_mode"),
+					resource.TestCheckResourceAttr(datasourceName, "autonomous_container_database_dataguard_associations.0.role", "PRIMARY"),
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.0.state"),
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.0.time_created"),
+				),
+			},
+			// switchover
+			{
+				Config: config +
+					generateDataSourceFromRepresentationMap("oci_database_autonomous_container_database_dataguard_associations", "test", Optional, Create, autonomousContainerDatabaseDataguardAssociationDataSourceRepresentation) +
+					compartmentIdVariableStr + AutonomousContainerDatabaseDataguardAssociationResourceConfig +
+					AutonomousContainerDatabaseDataguardAssociationOperationSwitchOverResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					func(s *terraform.State) (err error) {
+						time.Sleep(5 * time.Minute)
+						return nil
+					},
+				),
+			},
+			// verify switchover result
+			{
+				Config: config +
+					generateDataSourceFromRepresentationMap("oci_database_autonomous_container_database_dataguard_associations", "test", Optional, Create, autonomousContainerDatabaseDataguardAssociationDataSourceRepresentation) +
+					compartmentIdVariableStr + AutonomousContainerDatabaseDataguardAssociationResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_id"),
+
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.#"),
+					resource.TestCheckResourceAttr(datasourceName, "autonomous_container_database_dataguard_associations.0.peer_role", "PRIMARY"),
+					resource.TestCheckResourceAttr(datasourceName, "autonomous_container_database_dataguard_associations.0.role", "STANDBY"),
+				),
+			},
+			// failover
+			{
+				Config: config +
+					generateDataSourceFromRepresentationMap("oci_database_autonomous_container_database_dataguard_associations", "test", Optional, Create, autonomousContainerDatabaseDataguardAssociationDataSourceRepresentation) +
+					compartmentIdVariableStr + AutonomousContainerDatabaseDataguardAssociationResourceConfig +
+					AutonomousContainerDatabaseDataguardAssociationOperationFailOverResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					func(s *terraform.State) (err error) {
+						time.Sleep(5 * time.Minute)
+						return nil
+					},
+				),
+			},
+			// verify failover result
+			{
+				Config: config +
+					generateDataSourceFromRepresentationMap("oci_database_autonomous_container_database_dataguard_associations", "test", Optional, Create, autonomousContainerDatabaseDataguardAssociationDataSourceRepresentation) +
+					compartmentIdVariableStr + AutonomousContainerDatabaseDataguardAssociationResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_id"),
+
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.#"),
+					resource.TestCheckResourceAttr(datasourceName, "autonomous_container_database_dataguard_associations.0.peer_role", "DISABLED_STANDBY"),
+					resource.TestCheckResourceAttr(datasourceName, "autonomous_container_database_dataguard_associations.0.role", "PRIMARY"),
+				),
+			},
+			// reinstate
+			{
+				Config: config +
+					generateDataSourceFromRepresentationMap("oci_database_autonomous_container_database_dataguard_associations", "test", Optional, Create, autonomousContainerDatabaseDataguardAssociationDataSourceRepresentation) +
+					compartmentIdVariableStr + AutonomousContainerDatabaseDataguardAssociationResourceConfig +
+					AutonomousContainerDatabaseDataguardAssociationOperationReinstateResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					func(s *terraform.State) (err error) {
+						time.Sleep(5 * time.Minute)
+						return nil
+					},
+				),
+			},
+			// verify reinstate result
+			{
+				Config: config +
+					generateDataSourceFromRepresentationMap("oci_database_autonomous_container_database_dataguard_associations", "test", Optional, Create, autonomousContainerDatabaseDataguardAssociationDataSourceRepresentation) +
+					compartmentIdVariableStr + AutonomousContainerDatabaseDataguardAssociationResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_id"),
+
+					resource.TestCheckResourceAttrSet(datasourceName, "autonomous_container_database_dataguard_associations.#"),
+					resource.TestCheckResourceAttr(datasourceName, "autonomous_container_database_dataguard_associations.0.peer_role", "STANDBY"),
+					resource.TestCheckResourceAttr(datasourceName, "autonomous_container_database_dataguard_associations.0.role", "PRIMARY"),
+				),
+			},
+		},
+	})
+}
--- website/docs/d/database_autonomous_container_database_dataguard_association.html.markdown
+++ website/docs/d/database_autonomous_container_database_dataguard_association.html.markdown
@@ -1,3 +1,4 @@
+
 ---
 subcategory: "Database"
 layout: "oci"
@@ -10,7 +11,7 @@ description: |-
 # Data Source: oci_database_autonomous_container_database_dataguard_association
 This data source provides details about a specific Autonomous Container Database Dataguard Association resource in Oracle Cloud Infrastructure Database service.
 
-Gets an Autonomous Container Database dataguard association for the specified Autonomous Container Database.
+Gets an Autonomous Container Database enabled with Autonomous Data Guard associated with the specified Autonomous Container Database.
 
 
 ## Example Usage
@@ -27,7 +28,7 @@ data "oci_database_autonomous_container_database_dataguard_association" "test_au
 
 The following arguments are supported:
 
-* `autonomous_container_database_dataguard_association_id` - (Required) The Autonomous Container Database Dataguard Association [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm).
+* `autonomous_container_database_dataguard_association_id` - (Required) The Autonomous Container Database-Autonomous Data Guard association [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm).
 * `autonomous_container_database_id` - (Required) The Autonomous Container Database [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm).
 
 
@@ -35,18 +36,18 @@ The following arguments are supported:
 
 The following attributes are exported:
 
-* `apply_lag` - The lag time between updates to the primary autonomous container database and application of the redo data on the standby autonomous container database, as computed by the reporting database.  Example: `9 seconds` 
-* `apply_rate` - The rate at which redo logs are synced between the associated container databases.  Example: `180 Mb per second` 
+* `apply_lag` - The lag time between updates to the primary Autonomous Container Database and application of the redo data on the standby Autonomous Container Database, as computed by the reporting database.  Example: `9 seconds` 
+* `apply_rate` - The rate at which redo logs are synchronized between the associated Autonomous Container Databases.  Example: `180 Mb per second` 
 * `autonomous_container_database_id` - The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the Autonomous Container Database that has a relationship with the peer Autonomous Container Database. 
-* `id` - The OCID of the Autonomous Dataguard created for given Autonomous Container Database.
+* `id` - The OCID of the Autonomous Data Guard created for a given Autonomous Container Database.
 * `lifecycle_details` - Additional information about the current lifecycleState, if available. 
-* `peer_autonomous_container_database_dataguard_association_id` - The OCID of the peer Autonomous Container Database Dataguard Association.
+* `peer_autonomous_container_database_dataguard_association_id` - The OCID of the peer Autonomous Container Database-Autonomous Data Guard association.
 * `peer_autonomous_container_database_id` - The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the peer Autonomous Container Database. 
-* `peer_lifecycle_state` - The current state of the Autonomous Dataguard.
-* `peer_role` - The role of the Autonomous Dataguard enabled Autonomous Container Database.
-* `protection_mode` - The protection mode of this Data Guard association. For more information, see [Oracle Data Guard Protection Modes](http://docs.oracle.com/database/122/SBYDB/oracle-data-guard-protection-modes.htm#SBYDB02000) in the Oracle Data Guard documentation. 
-* `role` - The role of the Autonomous Dataguard enabled Autonomous Container Database.
-* `state` - The current state of the Autonomous Dataguard.
+* `peer_lifecycle_state` - The current state of the Autonomous Data Guard.
+* `peer_role` - The role of the Autonomous Data Guard-enabled Autonomous Container Database.
+* `protection_mode` - The protection mode of this Autonomous Data Guard association. For more information, see [Oracle Data Guard Protection Modes](http://docs.oracle.com/database/122/SBYDB/oracle-data-guard-protection-modes.htm#SBYDB02000) in the Oracle Data Guard documentation. 
+* `role` - The role of the Autonomous Data Guard-enabled Autonomous Container Database.
+* `state` - The current state of the Autonomous Data Guard.
 * `time_created` - The date and time the Autonomous DataGuard association was created.
 * `time_last_role_changed` - The date and time when the last role change action happened.
 
