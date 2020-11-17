52064640ab76e9b9aa9703a88fb167f4ce0dd4e3
srismish <srishti.prakash.mishra@oracle.com>
2020-11-17 15:07:53 -0800

    Object storage test fix

--- oci/objectstorage_object_test.go
+++ oci/objectstorage_object_test.go
@@ -52,7 +52,7 @@ var (
 		"bucket":                            Representation{repType: Required, create: `${oci_objectstorage_bucket.test_bucket.name}`},
 		"namespace":                         Representation{repType: Required, create: `${oci_objectstorage_bucket.test_bucket.namespace}`},
 		"object":                            Representation{repType: Required, create: `my-test-object-3`},
-		"content_length_limit":              Representation{repType: Optional, create: `17`, update: `15`},
+		"content_length_limit":              Representation{repType: Optional, create: `17`, update: `16`},
 		"base64_encode_content":             Representation{repType: Optional, create: `true`},
 		"version_id":                        Representation{repType: Optional, create: `${oci_objectstorage_object.test_object.version_id}`},
 		"http_response_cache_control":       Representation{repType: Optional, create: `no-cache`, update: `no-store`},
@@ -452,7 +452,8 @@ func TestObjectStorageObjectResource_failContentLengthLimit(t *testing.T) {
 				Config: config + compartmentIdVariableStr + ObjectResourceDependencies +
 					generateResourceFromRepresentationMap("oci_objectstorage_object", "test_object", Optional, Update,
 						getUpdatedRepresentationCopy("object", Representation{repType: Required, create: `my-test-object-1`, update: `my-test-object-3`}, objectRepresentation)) +
-					generateDataSourceFromRepresentationMap("oci_objectstorage_object", "test_object", Optional, Update, objectSingularDataSourceRepresentation),
+					generateDataSourceFromRepresentationMap("oci_objectstorage_object", "test_object", Optional, Update,
+						getUpdatedRepresentationCopy("content_length_limit", Representation{repType: Optional, create: `17`, update: `15`},objectSingularDataSourceRepresentation)),
 				ExpectError: regexp.MustCompile("the requested object's content length is"),
 			},
 		},
