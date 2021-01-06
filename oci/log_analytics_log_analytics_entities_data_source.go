401e7c3c93c47101d69300a6c35f41b24be2c848
govindrao55 <govindrao.kulkarni@oracle.com>
2021-01-06 16:00:56 +0000

    Support for Log Analytics Entities

---
+++ oci/log_analytics_log_analytics_entities_data_source.go
@@ -0,0 +1,227 @@
+// Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package oci
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	oci_log_analytics "github.com/oracle/oci-go-sdk/v35/loganalytics"
+)
+
+func init() {
+	RegisterDatasource("oci_log_analytics_log_analytics_entities", LogAnalyticsLogAnalyticsEntitiesDataSource())
+}
+
+func LogAnalyticsLogAnalyticsEntitiesDataSource() *schema.Resource {
+	return &schema.Resource{
+		Read: readLogAnalyticsLogAnalyticsEntities,
+		Schema: map[string]*schema.Schema{
+			"filter": dataSourceFiltersSchema(),
+			"cloud_resource_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"compartment_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"entity_type_name": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"hostname": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"hostname_contains": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"is_management_agent_id_null": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"lifecycle_details_contains": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"name_contains": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"namespace": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"source_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"state": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"log_analytics_entity_collection": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+
+						"items": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem:     GetDataSourceItemSchema(LogAnalyticsLogAnalyticsEntityResource()),
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func readLogAnalyticsLogAnalyticsEntities(d *schema.ResourceData, m interface{}) error {
+	sync := &LogAnalyticsLogAnalyticsEntitiesDataSourceCrud{}
+	sync.D = d
+	sync.Client = m.(*OracleClients).logAnalyticsClient()
+
+	return ReadResource(sync)
+}
+
+type LogAnalyticsLogAnalyticsEntitiesDataSourceCrud struct {
+	D      *schema.ResourceData
+	Client *oci_log_analytics.LogAnalyticsClient
+	Res    *oci_log_analytics.ListLogAnalyticsEntitiesResponse
+}
+
+func (s *LogAnalyticsLogAnalyticsEntitiesDataSourceCrud) VoidState() {
+	s.D.SetId("")
+}
+
+func (s *LogAnalyticsLogAnalyticsEntitiesDataSourceCrud) Get() error {
+	request := oci_log_analytics.ListLogAnalyticsEntitiesRequest{}
+
+	if cloudResourceId, ok := s.D.GetOkExists("cloud_resource_id"); ok {
+		tmp := cloudResourceId.(string)
+		request.CloudResourceId = &tmp
+	}
+
+	if compartmentId, ok := s.D.GetOkExists("compartment_id"); ok {
+		tmp := compartmentId.(string)
+		request.CompartmentId = &tmp
+	}
+
+	if entityTypeName, ok := s.D.GetOkExists("entity_type_name"); ok {
+		interfaces := entityTypeName.([]interface{})
+		tmp := make([]string, len(interfaces))
+		for i := range interfaces {
+			if interfaces[i] != nil {
+				tmp[i] = interfaces[i].(string)
+			}
+		}
+		if len(tmp) != 0 || s.D.HasChange("states") {
+			request.EntityTypeName = tmp
+		}
+	}
+
+	if hostname, ok := s.D.GetOkExists("hostname"); ok {
+		tmp := hostname.(string)
+		request.Hostname = &tmp
+	}
+
+	if hostnameContains, ok := s.D.GetOkExists("hostname_contains"); ok {
+		tmp := hostnameContains.(string)
+		request.HostnameContains = &tmp
+	}
+
+	if isManagementAgentIdNull, ok := s.D.GetOkExists("is_management_agent_id_null"); ok {
+		request.IsManagementAgentIdNull = oci_log_analytics.ListLogAnalyticsEntitiesIsManagementAgentIdNullEnum(isManagementAgentIdNull.(string))
+	}
+
+	if lifecycleDetailsContains, ok := s.D.GetOkExists("lifecycle_details_contains"); ok {
+		tmp := lifecycleDetailsContains.(string)
+		request.LifecycleDetailsContains = &tmp
+	}
+
+	if name, ok := s.D.GetOkExists("name"); ok {
+		tmp := name.(string)
+		request.Name = &tmp
+	}
+
+	if nameContains, ok := s.D.GetOkExists("name_contains"); ok {
+		tmp := nameContains.(string)
+		request.NameContains = &tmp
+	}
+
+	if namespace, ok := s.D.GetOkExists("namespace"); ok {
+		tmp := namespace.(string)
+		request.NamespaceName = &tmp
+	}
+
+	if sourceId, ok := s.D.GetOkExists("source_id"); ok {
+		tmp := sourceId.(string)
+		request.SourceId = &tmp
+	}
+
+	if state, ok := s.D.GetOkExists("state"); ok {
+		request.LifecycleState = oci_log_analytics.ListLogAnalyticsEntitiesLifecycleStateEnum(state.(string))
+	}
+
+	request.RequestMetadata.RetryPolicy = getRetryPolicy(false, "log_analytics")
+
+	response, err := s.Client.ListLogAnalyticsEntities(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	s.Res = &response
+	request.Page = s.Res.OpcNextPage
+
+	for request.Page != nil {
+		listResponse, err := s.Client.ListLogAnalyticsEntities(context.Background(), request)
+		if err != nil {
+			return err
+		}
+
+		s.Res.Items = append(s.Res.Items, listResponse.Items...)
+		request.Page = listResponse.OpcNextPage
+	}
+
+	return nil
+}
+
+func (s *LogAnalyticsLogAnalyticsEntitiesDataSourceCrud) SetData() error {
+	if s.Res == nil {
+		return nil
+	}
+
+	s.D.SetId(GenerateDataSourceHashID("LogAnalyticsLogAnalyticsEntitiesDataSource-", LogAnalyticsLogAnalyticsEntitiesDataSource(), s.D))
+	resources := []map[string]interface{}{}
+	logAnalyticsEntity := map[string]interface{}{}
+
+	items := []interface{}{}
+	for _, item := range s.Res.Items {
+		items = append(items, LogAnalyticsEntitySummaryToMap(item))
+	}
+	logAnalyticsEntity["items"] = items
+
+	if f, fOk := s.D.GetOkExists("filter"); fOk {
+		items = ApplyFiltersInCollection(f.(*schema.Set), items, LogAnalyticsLogAnalyticsEntitiesDataSource().Schema["log_analytics_entity_collection"].Elem.(*schema.Resource).Schema)
+		logAnalyticsEntity["items"] = items
+	}
+
+	resources = append(resources, logAnalyticsEntity)
+	if err := s.D.Set("log_analytics_entity_collection", resources); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ website/docs/d/log_analytics_log_analytics_entity.html.markdown
@@ -0,0 +1,57 @@
+---
+subcategory: "Log Analytics"
+layout: "oci"
+page_title: "Oracle Cloud Infrastructure: oci_log_analytics_log_analytics_entity"
+sidebar_current: "docs-oci-datasource-log_analytics-log_analytics_entity"
+description: |-
+  Provides details about a specific Log Analytics Entity in Oracle Cloud Infrastructure Log Analytics service
+---
+
+# Data Source: oci_log_analytics_log_analytics_entity
+This data source provides details about a specific Log Analytics Entity resource in Oracle Cloud Infrastructure Log Analytics service.
+
+Retrieve the log analytics entity with the given id.
+
+## Example Usage
+
+```hcl
+data "oci_log_analytics_log_analytics_entity" "test_log_analytics_entity" {
+	#Required
+	log_analytics_entity_id = oci_log_analytics_log_analytics_entity.test_log_analytics_entity.id
+	namespace = var.log_analytics_entity_namespace
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `log_analytics_entity_id` - (Required) The log analytics entity OCID. 
+* `namespace` - (Required) The Logging Analytics namespace used for the request. 
+
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `are_logs_collected` - The Boolean flag to indicate if logs are collected for an entity for log analytics usage. 
+* `cloud_resource_id` - The OCID of the Cloud resource which this entity is a representation of. This may be blank when the entity represents a non-cloud resource that the customer may have on their premises. 
+* `compartment_id` - Compartment Identifier [OCID] (https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm).
+* `defined_tags` - Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{"foo-namespace.bar-key": "value"}` 
+* `entity_type_internal_name` - Internal name for the log analytics entity type. 
+* `entity_type_name` - Log analytics entity type name. 
+* `freeform_tags` - Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{"bar-key": "value"}` 
+* `hostname` - The hostname where the entity represented here is actually present. This would be the output one would get if they run `echo $HOSTNAME` on Linux or an equivalent OS command. This may be different from management agents host since logs may be collected remotely. 
+* `id` - The log analytics entity OCID. This ID is a reference used by log analytics features and it represents a resource that is provisioned and managed by the customer on their premises or on the cloud. 
+* `lifecycle_details` - lifecycleDetails has additional information regarding substeps such as management agent plugin deployment. 
+* `management_agent_compartment_id` - Management agent (management-agents resource kind) compartment OCID 
+* `management_agent_display_name` - Management agent (management-agents resource kind) display name 
+* `management_agent_id` - The OCID of the Management Agent. 
+* `name` - Log analytics entity name. The name must be unique, within the tenancy, and cannot be changed. 
+* `properties` - The name/value pairs for parameter values to be used in file patterns specified in log sources. 
+* `source_id` - This indicates the type of source. It is primarily for Enterprise Manager Repository ID. 
+* `state` - The current state of the log analytics entity. 
+* `time_created` - The date and time the resource was created, in the format defined by RFC3339. 
+* `time_updated` - The date and time the resource was last updated, in the format defined by RFC3339. 
+* `timezone_region` - The timezone region of the log analytics entity. 
+
