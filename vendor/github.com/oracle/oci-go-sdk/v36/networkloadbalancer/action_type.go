51aab47ea10f4faa04780f6379ac274c08a90efa
srismish <srishti.prakash.mishra@oracle.com>
2021-03-19 13:19:06 -0700

    Drop commit - Preview SDK for NLB

---
+++ vendor/github.com/oracle/oci-go-sdk/v36/networkloadbalancer/action_type.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2016, 2018, 2021, Oracle and/or its affiliates.  All rights reserved.
+// This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
+// Code generated. DO NOT EDIT.
+
+// NetworkLoadBalancer API
+//
+// A description of the network load balancer API
+//
+
+package networkloadbalancer
+
+// ActionTypeEnum Enum with underlying type: string
+type ActionTypeEnum string
+
+// Set of constants representing the allowable values for ActionTypeEnum
+const (
+	ActionTypeCreated    ActionTypeEnum = "CREATED"
+	ActionTypeUpdated    ActionTypeEnum = "UPDATED"
+	ActionTypeDeleted    ActionTypeEnum = "DELETED"
+	ActionTypeInProgress ActionTypeEnum = "IN_PROGRESS"
+	ActionTypeRelated    ActionTypeEnum = "RELATED"
+)
+
+var mappingActionType = map[string]ActionTypeEnum{
+	"CREATED":     ActionTypeCreated,
+	"UPDATED":     ActionTypeUpdated,
+	"DELETED":     ActionTypeDeleted,
+	"IN_PROGRESS": ActionTypeInProgress,
+	"RELATED":     ActionTypeRelated,
+}
+
+// GetActionTypeEnumValues Enumerates the set of values for ActionTypeEnum
+func GetActionTypeEnumValues() []ActionTypeEnum {
+	values := make([]ActionTypeEnum, 0)
+	for _, v := range mappingActionType {
+		values = append(values, v)
+	}
+	return values
+}
