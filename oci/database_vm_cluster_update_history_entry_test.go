d8a5377ac1b5b16961b5e9f5477f4e6237d3c7a5
govindrao55 <govindrao.kulkarni@oracle.com>
2021-06-08 23:40:40 +0000

    - Added - Support for ExaCC GI upgrade

---
+++ oci/database_vm_cluster_update_history_entry_test.go
@@ -0,0 +1,96 @@
+// Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package oci
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
+	"github.com/terraform-providers/terraform-provider-oci/httpreplay"
+)
+
+var (
+	vmClusterUpdateHistoryEntrySingularDataSourceRepresentation = map[string]interface{}{
+		"update_history_entry_id": Representation{repType: Required, create: `{}`},
+		"vm_cluster_id":           Representation{repType: Required, create: `${oci_database_vm_cluster.test_vm_cluster.id}`},
+	}
+
+	vmClusterUpdateHistoryEntryDataSourceRepresentation = map[string]interface{}{
+		"vm_cluster_id": Representation{repType: Required, create: `${oci_database_vm_cluster.test_vm_cluster.id}`},
+		"state":         Representation{repType: Optional, create: `AVAILABLE`},
+		"update_type":   Representation{repType: Optional, create: `GI_UPGRADE`},
+	}
+
+	VmClusterUpdateHistoryEntryResourceConfig = VmClusterNetworkValidatedResourceConfig +
+		generateResourceFromRepresentationMap("oci_database_vm_cluster", "test_vm_cluster", Required, Create, vmClusterRepresentation)
+)
+
+func TestDatabaseVmClusterUpdateHistoryEntryResource_basic(t *testing.T) {
+	httpreplay.SetScenario("TestDatabaseVmClusterUpdateHistoryEntryResource_basic")
+	defer httpreplay.SaveScenario()
+	if !strings.Contains(getEnvSettingWithBlankDefault("enabled_tests"), "TestDatabaseVmClusterUpdateHistoryEntryResource_basic") {
+		t.Skip("test not supported due to GI Update not supported in terraform which is pre-requisite for this test")
+	}
+	provider := testAccProvider
+	config := testProviderConfig()
+
+	compartmentId := getEnvSettingWithBlankDefault("compartment_ocid")
+	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
+
+	datasourceName := "data.oci_database_vm_cluster_update_history_entries.test_vm_cluster_update_history_entries"
+	singularDatasourceName := "data.oci_database_vm_cluster_update_history_entry.test_vm_cluster_update_history_entry"
+
+	saveConfigContent("", "", "", t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		Providers: map[string]terraform.ResourceProvider{
+			"oci": provider,
+		},
+		Steps: []resource.TestStep{
+			// verify datasource
+			{
+				Config: config +
+					generateDataSourceFromRepresentationMap("oci_database_vm_cluster_update_history_entries", "test_vm_cluster_update_history_entries", Required, Create, vmClusterUpdateHistoryEntryDataSourceRepresentation) +
+					compartmentIdVariableStr + VmClusterUpdateHistoryEntryResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(datasourceName, "state", "AVAILABLE"),
+					resource.TestCheckResourceAttr(datasourceName, "update_type", "GI_UPGRADE"),
+					resource.TestCheckResourceAttrSet(datasourceName, "vm_cluster_id"),
+
+					resource.TestCheckResourceAttrSet(datasourceName, "vm_cluster_update_history_entries.#"),
+					resource.TestCheckResourceAttrSet(datasourceName, "vm_cluster_update_history_entries.0.id"),
+					resource.TestCheckResourceAttrSet(datasourceName, "vm_cluster_update_history_entries.0.state"),
+					resource.TestCheckResourceAttrSet(datasourceName, "vm_cluster_update_history_entries.0.time_completed"),
+					resource.TestCheckResourceAttrSet(datasourceName, "vm_cluster_update_history_entries.0.time_started"),
+					resource.TestCheckResourceAttrSet(datasourceName, "vm_cluster_update_history_entries.0.update_action"),
+					resource.TestCheckResourceAttrSet(datasourceName, "vm_cluster_update_history_entries.0.update_id"),
+					resource.TestCheckResourceAttrSet(datasourceName, "vm_cluster_update_history_entries.0.update_type"),
+				),
+			},
+			// verify singular datasource
+			{
+				Config: config +
+					generateDataSourceFromRepresentationMap("oci_database_vm_cluster_update_history_entry", "test_vm_cluster_update_history_entry", Required, Create, vmClusterUpdateHistoryEntrySingularDataSourceRepresentation) +
+					compartmentIdVariableStr + VmClusterUpdateHistoryEntryResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "update_history_entry_id"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "vm_cluster_id"),
+
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "id"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "state"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "time_completed"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "time_started"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "update_action"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "update_id"),
+					resource.TestCheckResourceAttrSet(singularDatasourceName, "update_type"),
+				),
+			},
+		},
+	})
+}
---
+++ oci/database_vm_cluster_updates_data_source.go
@@ -0,0 +1,198 @@
+// Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package oci
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	oci_database "github.com/oracle/oci-go-sdk/v44/database"
+)
+
+func init() {
+	RegisterDatasource("oci_database_vm_cluster_updates", DatabaseVmClusterUpdatesDataSource())
+}
+
+func DatabaseVmClusterUpdatesDataSource() *schema.Resource {
+	return &schema.Resource{
+		Read: readDatabaseVmClusterUpdates,
+		Schema: map[string]*schema.Schema{
+			"filter": dataSourceFiltersSchema(),
+			"state": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"update_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"vm_cluster_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"vm_cluster_updates": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						// Required
+
+						// Optional
+
+						// Computed
+						"available_actions": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"description": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"last_action": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"lifecycle_details": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"state": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"time_released": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"update_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"version": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func readDatabaseVmClusterUpdates(d *schema.ResourceData, m interface{}) error {
+	sync := &DatabaseVmClusterUpdatesDataSourceCrud{}
+	sync.D = d
+	sync.Client = m.(*OracleClients).databaseClient()
+
+	return ReadResource(sync)
+}
+
+type DatabaseVmClusterUpdatesDataSourceCrud struct {
+	D      *schema.ResourceData
+	Client *oci_database.DatabaseClient
+	Res    *oci_database.ListVmClusterUpdatesResponse
+}
+
+func (s *DatabaseVmClusterUpdatesDataSourceCrud) VoidState() {
+	s.D.SetId("")
+}
+
+func (s *DatabaseVmClusterUpdatesDataSourceCrud) Get() error {
+	request := oci_database.ListVmClusterUpdatesRequest{}
+
+	if state, ok := s.D.GetOkExists("state"); ok {
+		request.LifecycleState = oci_database.VmClusterUpdateSummaryLifecycleStateEnum(state.(string))
+	}
+
+	if updateType, ok := s.D.GetOkExists("update_type"); ok {
+		request.UpdateType = oci_database.ListVmClusterUpdatesUpdateTypeEnum(updateType.(string))
+	}
+
+	if vmClusterId, ok := s.D.GetOkExists("vm_cluster_id"); ok {
+		tmp := vmClusterId.(string)
+		request.VmClusterId = &tmp
+	}
+
+	request.RequestMetadata.RetryPolicy = getRetryPolicy(false, "database")
+
+	response, err := s.Client.ListVmClusterUpdates(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	s.Res = &response
+	request.Page = s.Res.OpcNextPage
+
+	for request.Page != nil {
+		listResponse, err := s.Client.ListVmClusterUpdates(context.Background(), request)
+		if err != nil {
+			return err
+		}
+
+		s.Res.Items = append(s.Res.Items, listResponse.Items...)
+		request.Page = listResponse.OpcNextPage
+	}
+
+	return nil
+}
+
+func (s *DatabaseVmClusterUpdatesDataSourceCrud) SetData() error {
+	if s.Res == nil {
+		return nil
+	}
+
+	s.D.SetId(GenerateDataSourceHashID("DatabaseVmClusterUpdatesDataSource-", DatabaseVmClusterUpdatesDataSource(), s.D))
+	resources := []map[string]interface{}{}
+
+	for _, r := range s.Res.Items {
+		vmClusterUpdate := map[string]interface{}{}
+
+		vmClusterUpdate["available_actions"] = r.AvailableActions
+
+		if r.Description != nil {
+			vmClusterUpdate["description"] = *r.Description
+		}
+
+		if r.Id != nil {
+			vmClusterUpdate["id"] = *r.Id
+		}
+
+		vmClusterUpdate["last_action"] = r.LastAction
+
+		if r.LifecycleDetails != nil {
+			vmClusterUpdate["lifecycle_details"] = *r.LifecycleDetails
+		}
+
+		vmClusterUpdate["state"] = r.LifecycleState
+
+		if r.TimeReleased != nil {
+			vmClusterUpdate["time_released"] = r.TimeReleased.String()
+		}
+
+		vmClusterUpdate["update_type"] = r.UpdateType
+
+		if r.Version != nil {
+			vmClusterUpdate["version"] = *r.Version
+		}
+
+		resources = append(resources, vmClusterUpdate)
+	}
+
+	if f, fOk := s.D.GetOkExists("filter"); fOk {
+		resources = ApplyFilters(f.(*schema.Set), resources, DatabaseVmClusterUpdatesDataSource().Schema["vm_cluster_updates"].Elem.(*schema.Resource).Schema)
+	}
+
+	if err := s.D.Set("vm_cluster_updates", resources); err != nil {
+		return err
+	}
+
+	return nil
+}
