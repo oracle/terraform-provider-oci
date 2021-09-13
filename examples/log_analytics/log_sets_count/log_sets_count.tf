dad6ea33edfd4280c9482d0e4f0e808d67c64f6d
Yueying Yang <yueying.yang@oracle.com>
2021-09-13 10:53:19 -0700

    Added - Support for oci_log_analytics_log_sets_count data_source

---
+++ examples/log_analytics/log_sets_count/log_sets_count.tf
@@ -0,0 +1,18 @@
+// Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+/*
+ * This example shows how to query the count of log sets
+ */
+
+variable "compartment_ocid" {}
+
+# Fetch namespace name from object store GET /n
+data "oci_objectstorage_namespace" "ns" {
+  compartment_id             = var.compartment_ocid
+}
+
+# Get the count of total log sets by namespace
+data "oci_log_analytics_log_sets_count" "logSetsCount" {
+  namespace                  = data.oci_objectstorage_namespace.ns.namespace
+}
