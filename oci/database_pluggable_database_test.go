c0366499f2cffd28f792571f345e18f9b5e7c70f
Sarang Modak <sarang.modak@oracle.com>
2021-10-18 13:18:38 +0000

    Added - Support for TDE Wallet Password Optional for ExaCS and DBCS

--- oci/database_pluggable_database_test.go
+++ oci/database_pluggable_database_test.go
@@ -43,14 +43,15 @@ var (
 	}
 
 	pluggableDatabaseRepresentation = map[string]interface{}{
-		"container_database_id": Representation{RepType: Required, Create: `${data.oci_database_database.t.id}`},
-		"pdb_admin_password":    Representation{RepType: Required, Create: `BEstrO0ng_#11`},
-		"pdb_name":              Representation{RepType: Required, Create: `SalesPdb`},
-		"tde_wallet_password":   Representation{RepType: Required, Create: `BEstrO0ng_#11`},
-		"defined_tags":          Representation{RepType: Optional, Create: `${map("${oci_identity_tag_namespace.tag-namespace1.name}.${oci_identity_tag.tag1.name}", "value")}`, Update: `${map("${oci_identity_tag_namespace.tag-namespace1.name}.${oci_identity_tag.tag1.name}", "updatedValue")}`},
-		"freeform_tags":         Representation{RepType: Optional, Create: map[string]string{"Department": "Finance"}, Update: map[string]string{"Department": "Accounting"}},
-		"lifecycle":             RepresentationGroup{Required, ignoreChangesLBRepresentation},
-		"depends_on":            Representation{RepType: Required, Create: []string{"oci_database_db_system.t"}},
+		"container_database_id":              Representation{RepType: Required, Create: `${data.oci_database_database.t.id}`},
+		"pdb_admin_password":                 Representation{RepType: Required, Create: `BEstrO0ng_#11`},
+		"pdb_name":                           Representation{RepType: Required, Create: `SalesPdb`},
+		"tde_wallet_password":                Representation{RepType: Required, Create: `BEstrO0ng_#11`},
+		"defined_tags":                       Representation{RepType: Optional, Create: `${map("${oci_identity_tag_namespace.tag-namespace1.name}.${oci_identity_tag.tag1.name}", "value")}`, Update: `${map("${oci_identity_tag_namespace.tag-namespace1.name}.${oci_identity_tag.tag1.name}", "updatedValue")}`},
+		"freeform_tags":                      Representation{RepType: Optional, Create: map[string]string{"Department": "Finance"}, Update: map[string]string{"Department": "Accounting"}},
+		"should_pdb_admin_account_be_locked": Representation{RepType: Optional, Create: `false`},
+		"lifecycle":                          RepresentationGroup{Required, ignoreChangesLBRepresentation},
+		"depends_on":                         Representation{RepType: Required, Create: []string{"oci_database_db_system.t"}},
 	}
 
 	ResourcePluggableDatabaseBaseConfig = `
@@ -245,6 +246,7 @@ func TestDatabasePluggableDatabaseResource_basic(t *testing.T) {
 				resource.TestCheckResourceAttrSet(resourceName, "open_mode"),
 				resource.TestCheckResourceAttr(resourceName, "pdb_admin_password", "BEstrO0ng_#11"),
 				resource.TestCheckResourceAttr(resourceName, "pdb_name", "SalesPdb"),
+				resource.TestCheckResourceAttr(resourceName, "should_pdb_admin_account_be_locked", "false"),
 				resource.TestCheckResourceAttrSet(resourceName, "state"),
 				resource.TestCheckResourceAttr(resourceName, "tde_wallet_password", "BEstrO0ng_#11"),
 				resource.TestCheckResourceAttrSet(resourceName, "time_created"),
@@ -274,6 +276,7 @@ func TestDatabasePluggableDatabaseResource_basic(t *testing.T) {
 				resource.TestCheckResourceAttrSet(resourceName, "open_mode"),
 				resource.TestCheckResourceAttr(resourceName, "pdb_admin_password", "BEstrO0ng_#11"),
 				resource.TestCheckResourceAttr(resourceName, "pdb_name", "SalesPdb"),
+				resource.TestCheckResourceAttr(resourceName, "should_pdb_admin_account_be_locked", "false"),
 				resource.TestCheckResourceAttrSet(resourceName, "state"),
 				resource.TestCheckResourceAttr(resourceName, "tde_wallet_password", "BEstrO0ng_#11"),
 				resource.TestCheckResourceAttrSet(resourceName, "time_created"),
@@ -340,6 +343,7 @@ func TestDatabasePluggableDatabaseResource_basic(t *testing.T) {
 			ImportStateVerify: true,
 			ImportStateVerifyIgnore: []string{
 				"pdb_admin_password",
+				"should_pdb_admin_account_be_locked",
 				"tde_wallet_password",
 			},
 			ResourceName: resourceName,
--- website/docs/r/database_pluggable_database.html.markdown
+++ website/docs/r/database_pluggable_database.html.markdown
@@ -11,7 +11,7 @@ description: |-
 This resource provides the Pluggable Database resource in Oracle Cloud Infrastructure Database service.
 
 Creates and starts a pluggable database in the specified container database.
-Use the [StartPluggableDatabase](#/en/database/latest/PluggableDatabase/StartPluggableDatabase] and [StopPluggableDatabase](#/en/database/latest/PluggableDatabase/StopPluggableDatabase] APIs to start and stop the pluggable database.
+Use the [StartPluggableDatabase](https://docs.cloud.oracle.com/iaas/api/#/en/database/latest/PluggableDatabase/StartPluggableDatabase) and [StopPluggableDatabase](https://docs.cloud.oracle.com/iaas/api/#/en/database/latest/PluggableDatabase/StopPluggableDatabase) APIs to start and stop the pluggable database.
 
 
 ## Example Usage
@@ -20,13 +20,14 @@ Use the [StartPluggableDatabase](#/en/database/latest/PluggableDatabase/StartPlu
 resource "oci_database_pluggable_database" "test_pluggable_database" {
 	#Required
 	container_database_id = oci_database_database.test_database.id
-	pdb_admin_password = var.pluggable_database_pdb_admin_password
 	pdb_name = var.pluggable_database_pdb_name
-	tde_wallet_password = var.pluggable_database_tde_wallet_password
 
 	#Optional
 	defined_tags = var.pluggable_database_defined_tags
 	freeform_tags = {"Department"= "Finance"}
+	pdb_admin_password = var.pluggable_database_pdb_admin_password
+	should_pdb_admin_account_be_locked = var.pluggable_database_should_pdb_admin_account_be_locked
+	tde_wallet_password = var.pluggable_database_tde_wallet_password
 }
 ```
 
@@ -37,9 +38,10 @@ The following arguments are supported:
 * `container_database_id` - (Required) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the CDB
 * `defined_tags` - (Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm). 
 * `freeform_tags` - (Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{"Department": "Finance"}` 
-* `pdb_admin_password` - (Required) A strong password for PDB Admin. The password must be at least nine characters and contain at least two uppercase, two lowercase, two numbers, and two special characters. The special characters must be _, \#, or -.
+* `pdb_admin_password` - (Optional) A strong password for PDB Admin. The password must be at least nine characters and contain at least two uppercase, two lowercase, two numbers, and two special characters. The special characters must be _, \#, or -.
 * `pdb_name` - (Required) The name for the pluggable database (PDB). The name is unique in the context of a [container database](https://docs.cloud.oracle.com/iaas/api/#/en/database/latest/Database/). The name must begin with an alphabetic character and can contain a maximum of thirty alphanumeric characters. Special characters are not permitted. The pluggable database name should not be same as the container database name.
-* `tde_wallet_password` - (Required) The existing TDE wallet password of the CDB.
+* `should_pdb_admin_account_be_locked` - (Optional) The locked mode of the pluggable database admin account. If false, the user needs to provide the PDB Admin Password to connect to it. If true, the pluggable database will be locked and user cannot login to it. 
+* `tde_wallet_password` - (Optional) The existing TDE wallet password of the CDB.
 
 
 ** IMPORTANT **
