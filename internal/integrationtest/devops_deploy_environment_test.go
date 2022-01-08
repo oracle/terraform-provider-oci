17128deaeb90ea206d251cdf00f770694c450db7
Nagendra Nigade <nagendra.nigade@oracle.com>
2022-01-08 19:54:34 +0530

    Added Support for - DevOps Project Service - fixed defined tags assertion test case fix

--- internal/integrationtest/devops_deploy_environment_test.go
+++ internal/integrationtest/devops_deploy_environment_test.go
@@ -57,7 +57,7 @@ var (
 		"description":             acctest.Representation{RepType: acctest.Optional, Create: `description`, Update: `description2`},
 		"display_name":            acctest.Representation{RepType: acctest.Optional, Create: `displayName`, Update: `displayName2`},
 		"freeform_tags":           acctest.Representation{RepType: acctest.Optional, Create: map[string]string{"bar-key": "value"}, Update: map[string]string{"Department": "Accounting"}},
-		"lifecycle":               acctest.RepresentationGroup{acctest.Required, ignoreDefinedTagsDifferencesRepresentation},
+		"lifecycle":               acctest.RepresentationGroup{RepType: acctest.Required, Group: ignoreDefinedTagsDifferencesRepresentation},
 	}
 
 	DeployEnvironmentResourceDependencies = AvailabilityDomainConfig +
