0c53bda508a41d1bfa546803b91b6b37e7455562
Sarang Modak <sarang.modak@oracle.com>
2022-01-13 02:56:10 +0000

    Added VM DB Operating System & GI Upgrade

---
+++ internal/service/database/database_db_systems_upgrade_resource.go
@@ -0,0 +1,674 @@
+// Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package database
+
+import (
+	"context"
+
+	"github.com/terraform-providers/terraform-provider-oci/internal/client"
+	"github.com/terraform-providers/terraform-provider-oci/internal/tfresource"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	oci_database "github.com/oracle/oci-go-sdk/v65/database"
+	oci_work_requests "github.com/oracle/oci-go-sdk/v65/workrequests"
+)
+
+func DatabaseDbSystemsUpgradeResource() *schema.Resource {
+	return &schema.Resource{
+		Timeouts: tfresource.DefaultTimeout,
+		Create:   createDatabaseDbSystemsUpgrade,
+		Read:     readDatabaseDbSystemsUpgrade,
+		Delete:   deleteDatabaseDbSystemsUpgrade,
+		Schema: map[string]*schema.Schema{
+			// Required
+			"action": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"db_system_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			// Optional
+			"is_snapshot_retention_days_force_updated": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"new_gi_version": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"snapshot_retention_period_in_days": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+
+			// Computed
+			"availability_domain": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"backup_network_nsg_ids": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Set:      tfresource.LiteralTypeHashCodeForSets,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"backup_subnet_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cluster_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"compartment_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cpu_core_count": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"data_storage_percentage": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"data_storage_size_in_gb": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"database_edition": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"db_system_options": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						// Required
+
+						// Optional
+
+						// Computed
+						"storage_management": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"defined_tags": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem:     schema.TypeString,
+			},
+			"disk_redundancy": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"display_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"domain": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"fault_domains": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"freeform_tags": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem:     schema.TypeString,
+			},
+			"hostname": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"iorm_config_cache": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						// Required
+
+						// Optional
+
+						// Computed
+						"db_plans": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									// Required
+
+									// Optional
+
+									// Computed
+									"db_name": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"flash_cache_limit": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"share": {
+										Type:     schema.TypeInt,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"lifecycle_details": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"objective": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"state": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"kms_key_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"last_maintenance_run_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"last_patch_history_entry_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"license_model": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"lifecycle_details": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"listener_port": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"maintenance_window": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						// Required
+
+						// Optional
+
+						// Computed
+						"days_of_week": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									// Required
+
+									// Optional
+
+									// Computed
+									"name": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"hours_of_day": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeInt,
+							},
+						},
+						"lead_time_in_weeks": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"months": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									// Required
+
+									// Optional
+
+									// Computed
+									"name": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"preference": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"weeks_of_month": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeInt,
+							},
+						},
+					},
+				},
+			},
+			"next_maintenance_run_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"node_count": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"nsg_ids": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Set:      tfresource.LiteralTypeHashCodeForSets,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"point_in_time_data_disk_clone_timestamp": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"reco_storage_size_in_gb": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"scan_dns_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"scan_dns_record_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"scan_ip_ids": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"shape": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"source_db_system_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"sparse_diskgroup": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"ssh_public_keys": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"state": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"subnet_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"time_created": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"time_zone": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"version": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"vip_ids": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"zone_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func createDatabaseDbSystemsUpgrade(d *schema.ResourceData, m interface{}) error {
+	sync := &DatabaseDbSystemsUpgradeResourceCrud{}
+	sync.D = d
+	sync.Client = m.(*client.OracleClients).DatabaseClient()
+	sync.WorkRequestClient = m.(*client.OracleClients).WorkRequestClient
+
+	return tfresource.CreateResource(d, sync)
+}
+
+func readDatabaseDbSystemsUpgrade(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func deleteDatabaseDbSystemsUpgrade(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+type DatabaseDbSystemsUpgradeResourceCrud struct {
+	tfresource.BaseCrud
+	Client                 *oci_database.DatabaseClient
+	Res                    *oci_database.DbSystem
+	DisableNotFoundRetries bool
+	WorkRequestClient      *oci_work_requests.WorkRequestClient
+}
+
+func (s *DatabaseDbSystemsUpgradeResourceCrud) ID() string {
+	return *s.Res.Id
+}
+
+func (s *DatabaseDbSystemsUpgradeResourceCrud) CreatedPending() []string {
+	return []string{
+		string(oci_database.DbSystemLifecycleStateUpgrading),
+	}
+}
+
+func (s *DatabaseDbSystemsUpgradeResourceCrud) CreatedTarget() []string {
+	return []string{
+		string(oci_database.DbSystemLifecycleStateAvailable),
+	}
+}
+
+func (s *DatabaseDbSystemsUpgradeResourceCrud) DeletedPending() []string {
+	return []string{
+		string(oci_database.DbSystemLifecycleStateTerminating),
+	}
+}
+
+func (s *DatabaseDbSystemsUpgradeResourceCrud) DeletedTarget() []string {
+	return []string{
+		string(oci_database.DbSystemLifecycleStateTerminated),
+	}
+}
+
+func (s *DatabaseDbSystemsUpgradeResourceCrud) Create() error {
+	request := oci_database.UpgradeDbSystemRequest{}
+
+	if action, ok := s.D.GetOkExists("action"); ok {
+		request.Action = oci_database.UpgradeDbSystemDetailsActionEnum(action.(string))
+	}
+
+	if dbSystemId, ok := s.D.GetOkExists("db_system_id"); ok {
+		tmp := dbSystemId.(string)
+		request.DbSystemId = &tmp
+	}
+
+	if isSnapshotRetentionDaysForceUpdated, ok := s.D.GetOkExists("is_snapshot_retention_days_force_updated"); ok {
+		tmp := isSnapshotRetentionDaysForceUpdated.(bool)
+		request.IsSnapshotRetentionDaysForceUpdated = &tmp
+	}
+
+	if newGiVersion, ok := s.D.GetOkExists("new_gi_version"); ok {
+		tmp := newGiVersion.(string)
+		request.NewGiVersion = &tmp
+	}
+
+	if snapshotRetentionPeriodInDays, ok := s.D.GetOkExists("snapshot_retention_period_in_days"); ok {
+		tmp := snapshotRetentionPeriodInDays.(int)
+		request.SnapshotRetentionPeriodInDays = &tmp
+	}
+
+	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(s.DisableNotFoundRetries, "database")
+
+	response, err := s.Client.UpgradeDbSystem(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	workId := response.OpcWorkRequestId
+	s.Res = &response.DbSystem
+
+	if workId != nil {
+		identifier, err := tfresource.WaitForWorkRequestWithErrorHandling(s.WorkRequestClient, workId, "dbSystem", oci_work_requests.WorkRequestResourceActionTypeUpdated, s.D.Timeout(schema.TimeoutUpdate), s.DisableNotFoundRetries)
+		if identifier != nil {
+			s.D.SetId(*identifier)
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	return s.Get()
+}
+
+func (s *DatabaseDbSystemsUpgradeResourceCrud) Get() error {
+	request := oci_database.GetDbSystemRequest{}
+
+	tmp := s.D.Id()
+	request.DbSystemId = &tmp
+
+	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(s.DisableNotFoundRetries, "database")
+
+	response, err := s.Client.GetDbSystem(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	s.Res = &response.DbSystem
+	return nil
+}
+
+func (s *DatabaseDbSystemsUpgradeResourceCrud) SetData() error {
+	if s.Res.AvailabilityDomain != nil {
+		s.D.Set("availability_domain", *s.Res.AvailabilityDomain)
+	}
+
+	backupNetworkNsgIds := []interface{}{}
+	for _, item := range s.Res.BackupNetworkNsgIds {
+		backupNetworkNsgIds = append(backupNetworkNsgIds, item)
+	}
+	s.D.Set("backup_network_nsg_ids", schema.NewSet(tfresource.LiteralTypeHashCodeForSets, backupNetworkNsgIds))
+
+	if s.Res.BackupSubnetId != nil {
+		s.D.Set("backup_subnet_id", *s.Res.BackupSubnetId)
+	}
+
+	if s.Res.ClusterName != nil {
+		s.D.Set("cluster_name", *s.Res.ClusterName)
+	}
+
+	if s.Res.CompartmentId != nil {
+		s.D.Set("compartment_id", *s.Res.CompartmentId)
+	}
+
+	if s.Res.CpuCoreCount != nil {
+		s.D.Set("cpu_core_count", *s.Res.CpuCoreCount)
+	}
+
+	if s.Res.DataStoragePercentage != nil {
+		s.D.Set("data_storage_percentage", *s.Res.DataStoragePercentage)
+	}
+
+	if s.Res.DataStorageSizeInGBs != nil {
+		s.D.Set("data_storage_size_in_gb", *s.Res.DataStorageSizeInGBs)
+	}
+
+	s.D.Set("database_edition", s.Res.DatabaseEdition)
+
+	if s.Res.DbSystemOptions != nil {
+		s.D.Set("db_system_options", []interface{}{DbSystemOptionsToMap(s.Res.DbSystemOptions)})
+	} else {
+		s.D.Set("db_system_options", nil)
+	}
+
+	if s.Res.DefinedTags != nil {
+		s.D.Set("defined_tags", tfresource.DefinedTagsToMap(s.Res.DefinedTags))
+	}
+
+	s.D.Set("disk_redundancy", s.Res.DiskRedundancy)
+
+	if s.Res.DisplayName != nil {
+		s.D.Set("display_name", *s.Res.DisplayName)
+	}
+
+	if s.Res.Domain != nil {
+		s.D.Set("domain", *s.Res.Domain)
+	}
+
+	s.D.Set("fault_domains", s.Res.FaultDomains)
+
+	s.D.Set("freeform_tags", s.Res.FreeformTags)
+
+	if s.Res.Hostname != nil {
+		s.D.Set("hostname", *s.Res.Hostname)
+	}
+
+	if s.Res.IormConfigCache != nil {
+		s.D.Set("iorm_config_cache", []interface{}{ExadataIormConfigToMap(s.Res.IormConfigCache)})
+	} else {
+		s.D.Set("iorm_config_cache", nil)
+	}
+
+	if s.Res.KmsKeyId != nil {
+		s.D.Set("kms_key_id", *s.Res.KmsKeyId)
+	}
+
+	if s.Res.LastMaintenanceRunId != nil {
+		s.D.Set("last_maintenance_run_id", *s.Res.LastMaintenanceRunId)
+	}
+
+	if s.Res.LastPatchHistoryEntryId != nil {
+		s.D.Set("last_patch_history_entry_id", *s.Res.LastPatchHistoryEntryId)
+	}
+
+	s.D.Set("license_model", s.Res.LicenseModel)
+
+	if s.Res.LifecycleDetails != nil {
+		s.D.Set("lifecycle_details", *s.Res.LifecycleDetails)
+	}
+
+	if s.Res.ListenerPort != nil {
+		s.D.Set("listener_port", *s.Res.ListenerPort)
+	}
+
+	if s.Res.MaintenanceWindow != nil {
+		s.D.Set("maintenance_window", []interface{}{MaintenanceWindowToMap(s.Res.MaintenanceWindow)})
+	} else {
+		s.D.Set("maintenance_window", nil)
+	}
+
+	if s.Res.NextMaintenanceRunId != nil {
+		s.D.Set("next_maintenance_run_id", *s.Res.NextMaintenanceRunId)
+	}
+
+	if s.Res.NodeCount != nil {
+		s.D.Set("node_count", *s.Res.NodeCount)
+	}
+
+	nsgIds := []interface{}{}
+	for _, item := range s.Res.NsgIds {
+		nsgIds = append(nsgIds, item)
+	}
+	s.D.Set("nsg_ids", schema.NewSet(tfresource.LiteralTypeHashCodeForSets, nsgIds))
+
+	if s.Res.PointInTimeDataDiskCloneTimestamp != nil {
+		s.D.Set("point_in_time_data_disk_clone_timestamp", s.Res.PointInTimeDataDiskCloneTimestamp.String())
+	}
+
+	if s.Res.RecoStorageSizeInGB != nil {
+		s.D.Set("reco_storage_size_in_gb", *s.Res.RecoStorageSizeInGB)
+	}
+
+	if s.Res.ScanDnsName != nil {
+		s.D.Set("scan_dns_name", *s.Res.ScanDnsName)
+	}
+
+	if s.Res.ScanDnsRecordId != nil {
+		s.D.Set("scan_dns_record_id", *s.Res.ScanDnsRecordId)
+	}
+
+	s.D.Set("scan_ip_ids", s.Res.ScanIpIds)
+
+	if s.Res.Shape != nil {
+		s.D.Set("shape", *s.Res.Shape)
+	}
+
+	if s.Res.SourceDbSystemId != nil {
+		s.D.Set("source_db_system_id", *s.Res.SourceDbSystemId)
+	}
+
+	if s.Res.SparseDiskgroup != nil {
+		s.D.Set("sparse_diskgroup", *s.Res.SparseDiskgroup)
+	}
+
+	s.D.Set("ssh_public_keys", s.Res.SshPublicKeys)
+
+	s.D.Set("state", s.Res.LifecycleState)
+
+	if s.Res.SubnetId != nil {
+		s.D.Set("subnet_id", *s.Res.SubnetId)
+	}
+
+	if s.Res.TimeCreated != nil {
+		s.D.Set("time_created", s.Res.TimeCreated.String())
+	}
+
+	if s.Res.TimeZone != nil {
+		s.D.Set("time_zone", *s.Res.TimeZone)
+	}
+
+	if s.Res.Version != nil {
+		s.D.Set("version", *s.Res.Version)
+	}
+
+	s.D.Set("vip_ids", s.Res.VipIds)
+
+	if s.Res.ZoneId != nil {
+		s.D.Set("zone_id", *s.Res.ZoneId)
+	}
+
+	return nil
+}
