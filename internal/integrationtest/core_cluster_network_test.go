9a94c426a0c8220b300c3e7e75b0e495e16581c3
Nagendra <nagendra.nigade@oracle.com>
2022-01-21 06:52:40 +0000

    Added support for - Cluster Network: new optional parameter to Update API

--- internal/integrationtest/core_cluster_network_test.go
+++ internal/integrationtest/core_cluster_network_test.go
@@ -53,7 +53,7 @@ var (
 		"freeform_tags":           acctest.Representation{RepType: acctest.Optional, Create: map[string]string{"Department": "Finance"}, Update: map[string]string{"Department": "Accounting"}},
 	}
 	clusterNetworkInstancePoolsRepresentation = map[string]interface{}{
-		"instance_configuration_id": acctest.Representation{RepType: acctest.Required, Create: `${oci_core_instance_configuration.test_instance_configuration.id}`},
+		"instance_configuration_id": acctest.Representation{RepType: acctest.Required, Create: `${oci_core_instance_configuration.test_instance_configuration.id}`, Update: `${oci_core_instance_configuration.test_instance_configuration.id}`},
 		"size":                      acctest.Representation{RepType: acctest.Required, Create: `1`, Update: `2`},
 		"defined_tags":              acctest.Representation{RepType: acctest.Optional, Create: `${map("${oci_identity_tag_namespace.tag-namespace1.name}.${oci_identity_tag.tag1.name}", "value")}`, Update: `${map("${oci_identity_tag_namespace.tag-namespace1.name}.${oci_identity_tag.tag1.name}", "updatedValue")}`},
 		"display_name":              acctest.Representation{RepType: acctest.Optional, Create: `hpc-cluster-network-pool`, Update: `hpc-cluster-network-pool2`},
