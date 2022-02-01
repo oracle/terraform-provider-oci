d82585994dce611563559c5b2c512dbfcc23eea3
Adao Junior <junior@users.noreply.github.com>
2022-02-01 16:38:51 -0600

    always free loadbalancer update to flexible

--- examples/always_free/main.tf
+++ examples/always_free/main.tf
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
+// Copyright (c) 2017, 2022, Oracle and/or its affiliates. All rights reserved.
 // Licensed under the Mozilla Public License v2.0
 
 variable "tenancy_ocid" {
@@ -210,11 +210,15 @@ resource "oci_core_instance" "free_instance1" {
   }
 }
 
-resource "oci_load_balancer" "free_load_balancer" {
+resource "oci_load_balancer_load_balancer" "free_load_balancer" {
   #Required
   compartment_id = var.compartment_ocid
   display_name   = "alwaysFreeLoadBalancer"
-  shape          = "10Mbps-Micro"
+  shape          = "flexible"
+  shape_details {
+    maximum_bandwidth_in_mbps = 10
+    minimum_bandwidth_in_mbps = 10
+  }
 
   subnet_ids = [
     oci_core_subnet.test_subnet.id,
@@ -223,7 +227,7 @@ resource "oci_load_balancer" "free_load_balancer" {
 
 resource "oci_load_balancer_backend_set" "free_load_balancer_backend_set" {
   name             = "lbBackendSet1"
-  load_balancer_id = oci_load_balancer.free_load_balancer.id
+  load_balancer_id = oci_load_balancer_load_balancer.free_load_balancer.id
   policy           = "ROUND_ROBIN"
 
   health_checker {
@@ -243,7 +247,7 @@ resource "oci_load_balancer_backend" "free_load_balancer_test_backend0" {
   #Required
   backendset_name  = oci_load_balancer_backend_set.free_load_balancer_backend_set.name
   ip_address       = oci_core_instance.free_instance0.public_ip
-  load_balancer_id = oci_load_balancer.free_load_balancer.id
+  load_balancer_id = oci_load_balancer_load_balancer.free_load_balancer.id
   port             = "80"
 }
 
@@ -251,19 +255,19 @@ resource "oci_load_balancer_backend" "free_load_balancer_test_backend1" {
   #Required
   backendset_name  = oci_load_balancer_backend_set.free_load_balancer_backend_set.name
   ip_address       = oci_core_instance.free_instance1.public_ip
-  load_balancer_id = oci_load_balancer.free_load_balancer.id
+  load_balancer_id = oci_load_balancer_load_balancer.free_load_balancer.id
   port             = "80"
 }
 
 resource "oci_load_balancer_hostname" "test_hostname1" {
   #Required
   hostname         = "app.free.com"
-  load_balancer_id = oci_load_balancer.free_load_balancer.id
+  load_balancer_id = oci_load_balancer_load_balancer.free_load_balancer.id
   name             = "hostname1"
 }
 
 resource "oci_load_balancer_listener" "load_balancer_listener0" {
-  load_balancer_id         = oci_load_balancer.free_load_balancer.id
+  load_balancer_id         = oci_load_balancer_load_balancer.free_load_balancer.id
   name                     = "http"
   default_backend_set_name = oci_load_balancer_backend_set.free_load_balancer_backend_set.name
   hostname_names           = [oci_load_balancer_hostname.test_hostname1.name]
@@ -289,12 +293,12 @@ resource "oci_load_balancer_rule_set" "test_rule_set" {
     status_code     = "405"
   }
 
-  load_balancer_id = oci_load_balancer.free_load_balancer.id
+  load_balancer_id = oci_load_balancer_load_balancer.free_load_balancer.id
   name             = "test_rule_set_name"
 }
 
 resource "oci_load_balancer_certificate" "load_balancer_certificate" {
-  load_balancer_id   = oci_load_balancer.free_load_balancer.id
+  load_balancer_id   = oci_load_balancer_load_balancer.free_load_balancer.id
   ca_certificate     = "-----BEGIN CERTIFICATE-----\nMIIC9jCCAd4CCQD2rPUVJETHGzANBgkqhkiG9w0BAQsFADA9MQswCQYDVQQGEwJV\nUzELMAkGA1UECAwCV0ExEDAOBgNVBAcMB1NlYXR0bGUxDzANBgNVBAoMBk9yYWNs\nZTAeFw0xOTAxMTcyMjU4MDVaFw0yMTAxMTYyMjU4MDVaMD0xCzAJBgNVBAYTAlVT\nMQswCQYDVQQIDAJXQTEQMA4GA1UEBwwHU2VhdHRsZTEPMA0GA1UECgwGT3JhY2xl\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA30+wt7OlUB/YpmWbTRkx\nnLG0lKWiV+oupNKj8luXmC5jvOFTUejt1pQhpA47nCqywlOAfk2N8hJWTyJZUmKU\n+DWVV2So2B/obYxpiiyWF2tcF/cYi1kBYeAIu5JkVFwDe4ITK/oQUFEhIn3Qg/oC\nMQ2985/MTdCXONgnbmePU64GrJwfvOeJcQB3VIL1BBfISj4pPw5708qTRv5MJBOO\njLKRM68KXC5us4879IrSA77NQr1KwjGnQlykyCgGvvgwgrUTd5c/dH8EKrZVcFi6\nytM66P/1CTpk1YpbI4gqiG0HBbuXG4JRIjyzW4GT4JXeSjgvrkIYL8k/M4Az1WEc\n2wIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQAuI53m8Va6EafDi6GQdQrzNNQFCAVQ\nxIABAB0uaSYCs3H+pqTktHzOrOluSUEogXRl0UU5/OuvxAz4idA4cfBdId4i7AcY\nqZsBjA/xqH/rxR3pcgfaGyxQzrUsJFf0ZwnzqYJs7fUvuatHJYi/cRBxrKR2+4Oj\nlUbb9TSmezlzHK5CaD5XzN+lZqbsSvN3OQbOryJCbtjZVQFGZ1SmL6OLrwpbBKuP\nn2ob+gaP57YSzO3zk1NDXMlQPHRsdSOqocyKx8y+7J0g6MqPvBzIe+wI3QW85MQY\nj1/IHmj84LNGp7pHCyiYx/oI+00gRch04H2pJv0TP3sAQ37gplBwDrUo\n-----END CERTIFICATE-----"
   certificate_name   = "certificate1"
   private_key        = "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEA30+wt7OlUB/YpmWbTRkxnLG0lKWiV+oupNKj8luXmC5jvOFT\nUejt1pQhpA47nCqywlOAfk2N8hJWTyJZUmKU+DWVV2So2B/obYxpiiyWF2tcF/cY\n\ni1kBYeAIu5JkVFwDe4ITK/oQUFEhIn3Qg/oCMQ2985/MTdCXONgnbmePU64GrJwf\nvOeJcQB3VIL1BBfISj4pPw5708qTRv5MJBOOjLKRM68KXC5us4879IrSA77NQr1K\nwjGnQlykyCgGvvgwgrUTd5c/dH8EKrZVcFi6ytM66P/1CTpk1YpbI4gqiG0HBbuX\nG4JRIjyzW4GT4JXeSjgvrkIYL8k/M4Az1WEc2wIDAQABAoIBAGQznukfG/uS/qTT\njNcQifl0p8HXfLwUIa/lsJkMTj6D+k8DkF59tVMGjv3NQSQ26JVX4J1L8XiAj+fc\nUtYr1Ap4CLX5PeYUkzesvKK6lPKXQvCh+Ip2eq9PVrvL2WcdDpb5695cy7suXD7c\n05aUtS0LrINH3eXAxkpEe5UHtQFni5YLrCLEXd+SSA3OKdCB+23HRELc1iCTvqjK\n5AtR916uHTBhtREHRMvWIdu4InRUsedlJhaJOLJ8G8r64JUtfm3wLUK1U8HFOsd0\nLAx9ZURU6cXl4osTWiy1vigGaM8Xuish2HkOLNYZADDUiDBB3SshmW5IDAJ5XTn5\nqVrszRECgYEA79j1y+WLTyV7yz7XkWk3OqoQXG4b2JfKItJI1M95UwllzQ8U/krM\n+QZjP3NTtB9i1YoHyaEfic103hV9Fkgz8jvKS5ocLGJulpN4CgqbHN6v9EJ3dqTk\no6X8mpx2eP2E0ngRekFyC/OCp0Zhe2KR9PXhijMa5eB2LTeCMIS/tzkCgYEA7lmk\nIdVjcpfqY7UFJ2R8zqPJHOne2+llrl9vzo6N5kx4DzAg7MP6XO9MekOvfmD1X1Lm\nFckXWFEF+0TlN5YvCTR/+OmVufYM3xp4GBT8RZdLFbyI4+xpAAeSC4SeM0ZkC9Jt\nrKqCS24+Kqy/+qSqtkxiPLQrXSdCSfCUlmn0ALMCgYBB7SLy3q+CG82BOk7Km18g\n8un4XhOtX1uiYqa+SCETH/wpd0HP/AOHV6gkIrEZS59BDuXBGFaw7BZ5jPKLE2Gj\n7adXTI797Dh1jydpqyyjrNo0i6iGpiBqkw9x+Bvged7ucy5qql6MxmxdSk01Owzf\nhk5uTEnScfZJy34vk+2WkQKBgBXx5uy+iuN4HTqE5i6UT/FunwusdLpmqNf/LXol\nIed8TumHEuD5wklgNvhi1vuZzb2zEkAbPa0B+L0DwN73UulUDhxK1WBDyTeZZklB\nVWDK5zzfGPNzRs+b4tRwp2gtKPT1sOde45QyWELxmNNo6dbS/ZB9Pijbfnz0S5n1\ns2OFAoGBAJUohI1+d2hKlkSUzpCorQarDe8lFVEbGMu0kX0JNDI7QU+H8vDp9NOl\nGqLm3sCVBYypT8sWfchgZpcVaLRLQCQtWy4+CbMN6DT3j/uBWeDpayU5Gvqt0/no\nvwqbG6b0NEYLRPLEdsS/c8TV9mMlvb0EW+GXfmkpTrTNt3hyXniu\n-----END RSA PRIVATE KEY-----"
@@ -306,7 +310,7 @@ resource "oci_load_balancer_certificate" "load_balancer_certificate" {
 }
 
 resource "oci_load_balancer_listener" "load_balancer_listener1" {
-  load_balancer_id         = oci_load_balancer.free_load_balancer.id
+  load_balancer_id         = oci_load_balancer_load_balancer.free_load_balancer.id
   name                     = "https"
   default_backend_set_name = oci_load_balancer_backend_set.free_load_balancer_backend_set.name
   port                     = 443
@@ -319,7 +323,7 @@ resource "oci_load_balancer_listener" "load_balancer_listener1" {
 }
 
 output "lb_public_ip" {
-  value = [oci_load_balancer.free_load_balancer.ip_address_details]
+  value = [oci_load_balancer_load_balancer.free_load_balancer.ip_address_details]
 }
 
 data "oci_core_vnic_attachments" "app_vnics" {
