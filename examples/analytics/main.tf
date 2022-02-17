d8acbc80f0da576ea464102babc8be1ef22b2b78
Suman Karmakar <suman.karmakar@oracle.com>
2022-02-17 12:29:54 +0000

    Added - Support for customer managed encryption keys in Oracle Analytics Cloud

--- examples/analytics/main.tf
+++ examples/analytics/main.tf
@@ -1,6 +1,7 @@
 // Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
 // Licensed under the Mozilla Public License v2.0
 
+
 // These variables would commonly be defined as environment variables or sourced in a .env file
 variable "tenancy_ocid" {
 }
@@ -23,6 +24,8 @@ variable "region" {
 variable "idcs_access_token" {
 }
 
+variable "kms_key_id" {
+}
 
 provider "oci" {
   region           = var.region
@@ -35,7 +38,7 @@ provider "oci" {
 resource "oci_analytics_analytics_instance" "test_oce_instance_public" {
   compartment_id     = var.compartment_ocid
   description        = "OAC instance"
-//  email_notification = var.email_notification
+  email_notification = var.email_notification
   feature_set        = "ENTERPRISE_ANALYTICS"
   license_type       = "LICENSE_INCLUDED"
 
@@ -65,13 +68,16 @@ resource "oci_analytics_analytics_instance" "test_oce_instance_public" {
       whitelisted_ips = [oci_core_vcn.test_vcn.cidr_block]
     }
   }
+
+  # Optionally specify a master encryption key for analytics metadata.
+  kms_key_id = var.kms_key_id
 }
 
 # Create a private access channel for the instance
 resource "oci_analytics_analytics_instance_private_access_channel" "test_private_access_channel" {
 #Required
   analytics_instance_id = oci_analytics_analytics_instance.test_oce_instance_public.id
-  display_name = "Example Private Access Channel"
+  display_name = "ExamplePAC"
   subnet_id = oci_core_subnet.test_subnet.id
   vcn_id = oci_core_vcn.test_vcn.id
   private_source_dns_zones {
@@ -81,14 +87,14 @@ resource "oci_analytics_analytics_instance_private_access_channel" "test_private
 }
 
 # Create a vanity url for the instance
-resource "oci_analytics_analytics_instances_vanity_url" "test_analytics_instances_vanity_url" {
+resource "oci_analytics_analytics_instance_vanity_url" "test_analytics_instances_vanity_url" {
   #Required
   analytics_instance_id = oci_analytics_analytics_instance.test_oce_instance_public.id
   ca_certificate        = file("/path/to/the/file/RootCA.crt")
-  hosts                 = ["analyticsdev"]
+  hosts                 = ["analyticsdev.mycompany.com"]
   private_key           = file("/path/to/the/file/analyticsdevCA.key")
   public_certificate    = file("/path/to/the/file/analyticsdevCA.crt")
-
+  
   #Optional
   description = "description"
   # passphrase is required if the private key was created with a passphrase
