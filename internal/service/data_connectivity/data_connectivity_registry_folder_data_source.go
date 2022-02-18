99974d8c5ae6342519babf21172ed6dc206aa67a
Pranay <pranay.deshpande@oracle.com>
2022-02-18 11:28:14 +0530

    squash commit for provider

---
+++ internal/service/data_connectivity/data_connectivity_registry_folder_data_source.go
@@ -0,0 +1,116 @@
+// Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package data_connectivity
+
+import (
+	"context"
+	"github.com/terraform-providers/terraform-provider-oci/internal/client"
+	"github.com/terraform-providers/terraform-provider-oci/internal/tfresource"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	oci_data_connectivity "github.com/oracle/oci-go-sdk/v58/dataconnectivity"
+)
+
+func DataConnectivityRegistryFolderDataSource() *schema.Resource {
+	fieldMap := make(map[string]*schema.Schema)
+	fieldMap["folder_key"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Required: true,
+	}
+	fieldMap["registry_id"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Required: true,
+	}
+	return tfresource.GetSingularDataSourceItemSchema(DataConnectivityRegistryFolderResource(), fieldMap, readSingularDataConnectivityRegistryFolder)
+}
+
+func readSingularDataConnectivityRegistryFolder(d *schema.ResourceData, m interface{}) error {
+	sync := &DataConnectivityRegistryFolderDataSourceCrud{}
+	sync.D = d
+	sync.Client = m.(*client.OracleClients).DataConnectivityManagementClient()
+
+	return tfresource.ReadResource(sync)
+}
+
+type DataConnectivityRegistryFolderDataSourceCrud struct {
+	D      *schema.ResourceData
+	Client *oci_data_connectivity.DataConnectivityManagementClient
+	Res    *oci_data_connectivity.GetFolderResponse
+}
+
+func (s *DataConnectivityRegistryFolderDataSourceCrud) VoidState() {
+	s.D.SetId("")
+}
+
+func (s *DataConnectivityRegistryFolderDataSourceCrud) Get() error {
+	request := oci_data_connectivity.GetFolderRequest{}
+
+	if folderKey, ok := s.D.GetOkExists("folder_key"); ok {
+		tmp := folderKey.(string)
+		request.FolderKey = &tmp
+	}
+
+	if registryId, ok := s.D.GetOkExists("registry_id"); ok {
+		tmp := registryId.(string)
+		request.RegistryId = &tmp
+	}
+
+	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(false, "data_connectivity")
+
+	response, err := s.Client.GetFolder(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	s.Res = &response
+	return nil
+}
+
+func (s *DataConnectivityRegistryFolderDataSourceCrud) SetData() error {
+	if s.Res == nil {
+		return nil
+	}
+
+	s.D.SetId(tfresource.GenerateDataSourceHashID("DataConnectivityRegistryFolderDataSource-", DataConnectivityRegistryFolderDataSource(), s.D))
+
+	if s.Res.Description != nil {
+		s.D.Set("description", *s.Res.Description)
+	}
+
+	if s.Res.Identifier != nil {
+		s.D.Set("identifier", *s.Res.Identifier)
+	}
+
+	if s.Res.Key != nil {
+		s.D.Set("key", *s.Res.Key)
+	}
+
+	if s.Res.ModelType != nil {
+		s.D.Set("model_type", *s.Res.ModelType)
+	}
+
+	if s.Res.ModelVersion != nil {
+		s.D.Set("model_version", *s.Res.ModelVersion)
+	}
+
+	if s.Res.Name != nil {
+		s.D.Set("name", *s.Res.Name)
+	}
+
+	if s.Res.ObjectStatus != nil {
+		s.D.Set("object_status", *s.Res.ObjectStatus)
+	}
+
+	if s.Res.ObjectVersion != nil {
+		s.D.Set("object_version", *s.Res.ObjectVersion)
+	}
+
+	if s.Res.ParentRef != nil {
+		s.D.Set("parent_ref", []interface{}{ParentReferenceToMap(s.Res.ParentRef)})
+	} else {
+		s.D.Set("parent_ref", nil)
+	}
+
+	return nil
+}
