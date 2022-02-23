e8b91ac7183dd0218fb72c43c263a3c95bc3ebb7
Yahang Zhang <yahang.zhang@oracle.com>
2022-02-23 23:25:48 +0000

    Added - Support for BGP disable and BFD feature in Fastconnect service

--- internal/service/core/core_virtual_circuits_data_source.go
+++ internal/service/core/core_virtual_circuits_data_source.go
@@ -115,6 +115,8 @@ func (s *CoreVirtualCircuitsDataSourceCrud) SetData() error {
 			virtualCircuit["bandwidth_shape_name"] = *r.BandwidthShapeName
 		}
 
+		virtualCircuit["bgp_admin_state"] = r.BgpAdminState
+
 		virtualCircuit["bgp_ipv6session_state"] = r.BgpIpv6SessionState
 
 		virtualCircuit["bgp_management"] = r.BgpManagement
@@ -154,6 +156,10 @@ func (s *CoreVirtualCircuitsDataSourceCrud) SetData() error {
 		}
 
 		virtualCircuit["ip_mtu"] = r.IpMtu
+
+		if r.IsBfdEnabled != nil {
+			virtualCircuit["is_bfd_enabled"] = *r.IsBfdEnabled
+		}
 
 		if r.OracleBgpAsn != nil {
 			virtualCircuit["oracle_bgp_asn"] = *r.OracleBgpAsn
