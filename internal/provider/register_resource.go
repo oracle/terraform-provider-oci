e8007d44299d527b246ecfd74d9315e4b8d67b41
rashik.bhasin <rashik.bhasin@oracle.com>
2022-03-02 22:41:11 +0000

    Added - Account Management - Finance Data (Subscriptions and Usage) osub_organization_subscription

--- internal/provider/register_resource.go
+++ internal/provider/register_resource.go
@@ -506,6 +506,7 @@ func init() {
 	RegisterResource("oci_osp_gateway_subscription", tf_osp_gateway.OspGatewaySubscriptionResource())
 	// osub_usage service
 	// osub_subscription service
+	// osub_organization_subscription service
 	// resourcemanager service
 	// sch service
 	RegisterResource("oci_sch_service_connector", tf_sch.SchServiceConnectorResource())
