4e0d31f2a19f9fdc12546bf322fbf6876eafffd9
John Truong <john.truong@oracle.com>
2022-04-27 15:06:07 -0700

    Exempted - SDK for resourcemanager

--- vendor/github.com/oracle/oci-go-sdk/v65/resourcemanager/delete_private_endpoint_request_response.go
+++ vendor/github.com/oracle/oci-go-sdk/v65/resourcemanager/delete_private_endpoint_request_response.go
@@ -11,27 +11,32 @@ import (
 	"strings"
 )
 
-// GetJobLogsContentRequest wrapper for the GetJobLogsContent operation
-type GetJobLogsContentRequest struct {
+// DeletePrivateEndpointRequest wrapper for the DeletePrivateEndpoint operation
+type DeletePrivateEndpointRequest struct {
 
-	// The OCID (https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the job.
-	JobId *string `mandatory:"true" contributesTo:"path" name:"jobId"`
+	// The OCID (https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the private endpoint.
+	PrivateEndpointId *string `mandatory:"true" contributesTo:"path" name:"privateEndpointId"`
 
 	// Unique Oracle-assigned identifier for the request. If you need to contact Oracle about a
 	// particular request, please provide the request ID.
 	OpcRequestId *string `mandatory:"false" contributesTo:"header" name:"opc-request-id"`
+
+	// For optimistic concurrency control. In the `PUT` or `DELETE` call for a resource, set the `if-match`
+	// parameter to the value of the etag from a previous `GET` or `POST` response for that resource.  The resource
+	// will be updated or deleted only if the etag you provide matches the resource's current etag value.
+	IfMatch *string `mandatory:"false" contributesTo:"header" name:"if-match"`
 
 	// Metadata about the request. This information will not be transmitted to the service, but
 	// represents information that the SDK will consume to drive retry behavior.
 	RequestMetadata common.RequestMetadata
 }
 
-func (request GetJobLogsContentRequest) String() string {
+func (request DeletePrivateEndpointRequest) String() string {
 	return common.PointerString(request)
 }
 
 // HTTPRequest implements the OCIRequest interface
-func (request GetJobLogsContentRequest) HTTPRequest(method, path string, binaryRequestBody *common.OCIReadSeekCloser, extraHeaders map[string]string) (http.Request, error) {
+func (request DeletePrivateEndpointRequest) HTTPRequest(method, path string, binaryRequestBody *common.OCIReadSeekCloser, extraHeaders map[string]string) (http.Request, error) {
 
 	_, err := request.ValidateEnumValue()
 	if err != nil {
@@ -41,21 +46,21 @@ func (request GetJobLogsContentRequest) HTTPRequest(method, path string, binaryR
 }
 
 // BinaryRequestBody implements the OCIRequest interface
-func (request GetJobLogsContentRequest) BinaryRequestBody() (*common.OCIReadSeekCloser, bool) {
+func (request DeletePrivateEndpointRequest) BinaryRequestBody() (*common.OCIReadSeekCloser, bool) {
 
 	return nil, false
 
 }
 
 // RetryPolicy implements the OCIRetryableRequest interface. This retrieves the specified retry policy.
-func (request GetJobLogsContentRequest) RetryPolicy() *common.RetryPolicy {
+func (request DeletePrivateEndpointRequest) RetryPolicy() *common.RetryPolicy {
 	return request.RequestMetadata.RetryPolicy
 }
 
 // ValidateEnumValue returns an error when providing an unsupported enum value
 // This function is being called during constructing API request process
 // Not recommended for calling this function directly
-func (request GetJobLogsContentRequest) ValidateEnumValue() (bool, error) {
+func (request DeletePrivateEndpointRequest) ValidateEnumValue() (bool, error) {
 	errMessage := []string{}
 	if len(errMessage) > 0 {
 		return true, fmt.Errorf(strings.Join(errMessage, "\n"))
@@ -63,24 +68,21 @@ func (request GetJobLogsContentRequest) ValidateEnumValue() (bool, error) {
 	return false, nil
 }
 
-// GetJobLogsContentResponse wrapper for the GetJobLogsContent operation
-type GetJobLogsContentResponse struct {
+// DeletePrivateEndpointResponse wrapper for the DeletePrivateEndpoint operation
+type DeletePrivateEndpointResponse struct {
 
 	// The underlying http response
 	RawResponse *http.Response
 
-	// The string instance
-	Value *string `presentIn:"body" encoding:"plain-text"`
-
-	// Unique identifier for the request
+	// Unique identifier for the request.
 	OpcRequestId *string `presentIn:"header" name:"opc-request-id"`
 }
 
-func (response GetJobLogsContentResponse) String() string {
+func (response DeletePrivateEndpointResponse) String() string {
 	return common.PointerString(response)
 }
 
 // HTTPResponse implements the OCIResponse interface
-func (response GetJobLogsContentResponse) HTTPResponse() *http.Response {
+func (response DeletePrivateEndpointResponse) HTTPResponse() *http.Response {
 	return response.RawResponse
 }
