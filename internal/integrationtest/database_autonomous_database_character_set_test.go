c92ae24173e5cba545f1ad2c7885ede9333cb1c5
Andrew Jenner <andrew.jenner@oracle.com>
2022-06-23 13:58:45 -0700

    Added - Support for character sets for Autonomous Database on Shared Infrastructure

--- internal/integrationtest/database_autonomous_database_character_set_test.go
+++ internal/integrationtest/database_autonomous_database_character_set_test.go
@@ -18,6 +18,26 @@ import (
 var (
 	autonomousDatabaseCharacterSetDataSourceRepresentation = map[string]interface{}{}
 
+	autonomousDatabaseCharacterSetDataSourceRepresentationDedicatedDatabase = map[string]interface{}{
+		"character_set_type": acctest.Representation{RepType: acctest.Optional, Create: `DATABASE`},
+		"is_shared":          acctest.Representation{RepType: acctest.Optional, Create: `false`},
+	}
+
+	autonomousDatabaseCharacterSetDataSourceRepresentationSharedDatabase = map[string]interface{}{
+		"character_set_type": acctest.Representation{RepType: acctest.Optional, Create: `DATABASE`},
+		"is_shared":          acctest.Representation{RepType: acctest.Optional, Create: `true`},
+	}
+
+	autonomousDatabaseCharacterSetDataSourceRepresentationDedicatedNational = map[string]interface{}{
+		"character_set_type": acctest.Representation{RepType: acctest.Optional, Create: `NATIONAL`},
+		"is_shared":          acctest.Representation{RepType: acctest.Optional, Create: `false`},
+	}
+
+	autonomousDatabaseCharacterSetDataSourceRepresentationSharedNational = map[string]interface{}{
+		"character_set_type": acctest.Representation{RepType: acctest.Optional, Create: `NATIONAL`},
+		"is_shared":          acctest.Representation{RepType: acctest.Optional, Create: `true`},
+	}
+
 	AutonomousDatabaseCharacterSetResourceConfig = ""
 )
 
@@ -40,6 +60,118 @@ func TestDatabaseAutonomousDatabaseCharacterSetResource_basic(t *testing.T) {
 		{
 			Config: config +
 				acctest.GenerateDataSourceFromRepresentationMap("oci_database_autonomous_database_character_sets", "test_autonomous_database_character_sets", acctest.Required, acctest.Create, autonomousDatabaseCharacterSetDataSourceRepresentation) +
+				compartmentIdVariableStr + AutonomousDatabaseCharacterSetResourceConfig,
+			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+
+				resource.TestCheckResourceAttrSet(datasourceName, "autonomous_database_character_sets.#"),
+				resource.TestCheckResourceAttrSet(datasourceName, "autonomous_database_character_sets.0.name"),
+			),
+		},
+	})
+}
+
+func TestDatabaseAutonomousDatabaseCharacterSetResource_DedicatedDatabase(t *testing.T) {
+	httpreplay.SetScenario("TestDatabaseAutonomousDatabaseCharacterSetResource_DedicatedDatabase(")
+	defer httpreplay.SaveScenario()
+
+	config := acctest.ProviderTestConfig()
+
+	compartmentId := utils.GetEnvSettingWithBlankDefault("compartment_ocid")
+	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
+
+	datasourceName := "data.oci_database_autonomous_database_character_sets.test_autonomous_database_character_sets"
+
+	acctest.SaveConfigContent("", "", "", t)
+
+	acctest.ResourceTest(t, nil, []resource.TestStep{
+		// verify datasource
+		{
+			Config: config +
+				acctest.GenerateDataSourceFromRepresentationMap("oci_database_autonomous_database_character_sets", "test_autonomous_database_character_sets", acctest.Required, acctest.Create, autonomousDatabaseCharacterSetDataSourceRepresentationDedicatedDatabase) +
+				compartmentIdVariableStr + AutonomousDatabaseCharacterSetResourceConfig,
+			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+
+				resource.TestCheckResourceAttrSet(datasourceName, "autonomous_database_character_sets.#"),
+				resource.TestCheckResourceAttrSet(datasourceName, "autonomous_database_character_sets.0.name"),
+			),
+		},
+	})
+}
+
+func TestDatabaseAutonomousDatabaseCharacterSetResource_DedicatedNational(t *testing.T) {
+	httpreplay.SetScenario("TestDatabaseAutonomousDatabaseCharacterSetResource_DedicatedNational")
+	defer httpreplay.SaveScenario()
+
+	config := acctest.ProviderTestConfig()
+
+	compartmentId := utils.GetEnvSettingWithBlankDefault("compartment_ocid")
+	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
+
+	datasourceName := "data.oci_database_autonomous_database_character_sets.test_autonomous_database_character_sets"
+
+	acctest.SaveConfigContent("", "", "", t)
+
+	acctest.ResourceTest(t, nil, []resource.TestStep{
+		// verify datasource
+		{
+			Config: config +
+				acctest.GenerateDataSourceFromRepresentationMap("oci_database_autonomous_database_character_sets", "test_autonomous_database_character_sets", acctest.Required, acctest.Create, autonomousDatabaseCharacterSetDataSourceRepresentationDedicatedNational) +
+				compartmentIdVariableStr + AutonomousDatabaseCharacterSetResourceConfig,
+			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+
+				resource.TestCheckResourceAttrSet(datasourceName, "autonomous_database_character_sets.#"),
+				resource.TestCheckResourceAttrSet(datasourceName, "autonomous_database_character_sets.0.name"),
+			),
+		},
+	})
+}
+
+func TestDatabaseAutonomousDatabaseCharacterSetResource_SharedDatabase(t *testing.T) {
+	httpreplay.SetScenario("TestDatabaseAutonomousDatabaseCharacterSetResource_SharedDatabase")
+	defer httpreplay.SaveScenario()
+
+	config := acctest.ProviderTestConfig()
+
+	compartmentId := utils.GetEnvSettingWithBlankDefault("compartment_ocid")
+	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
+
+	datasourceName := "data.oci_database_autonomous_database_character_sets.test_autonomous_database_character_sets"
+
+	acctest.SaveConfigContent("", "", "", t)
+
+	acctest.ResourceTest(t, nil, []resource.TestStep{
+		// verify datasource
+		{
+			Config: config +
+				acctest.GenerateDataSourceFromRepresentationMap("oci_database_autonomous_database_character_sets", "test_autonomous_database_character_sets", acctest.Required, acctest.Create, autonomousDatabaseCharacterSetDataSourceRepresentationSharedDatabase) +
+				compartmentIdVariableStr + AutonomousDatabaseCharacterSetResourceConfig,
+			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+
+				resource.TestCheckResourceAttrSet(datasourceName, "autonomous_database_character_sets.#"),
+				resource.TestCheckResourceAttrSet(datasourceName, "autonomous_database_character_sets.0.name"),
+			),
+		},
+	})
+}
+
+func TestDatabaseAutonomousDatabaseCharacterSetResource_SharedNational(t *testing.T) {
+	httpreplay.SetScenario("TestDatabaseAutonomousDatabaseCharacterSetResource_SharedNational")
+	defer httpreplay.SaveScenario()
+
+	config := acctest.ProviderTestConfig()
+
+	compartmentId := utils.GetEnvSettingWithBlankDefault("compartment_ocid")
+	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
+
+	datasourceName := "data.oci_database_autonomous_database_character_sets.test_autonomous_database_character_sets"
+
+	acctest.SaveConfigContent("", "", "", t)
+
+	acctest.ResourceTest(t, nil, []resource.TestStep{
+		// verify datasource
+		{
+			Config: config + // change this line, refer to main test
+				acctest.GenerateDataSourceFromRepresentationMap("oci_database_autonomous_database_character_sets", "test_autonomous_database_character_sets", acctest.Required, acctest.Create, autonomousDatabaseCharacterSetDataSourceRepresentationSharedNational) +
 				compartmentIdVariableStr + AutonomousDatabaseCharacterSetResourceConfig,
 			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
 
