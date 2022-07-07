372ef5467de0e80a5704ac63e024ca187cd92d4a
Divya Meharwade <divya.meharwade@oracle.com>
2022-07-07 10:47:02 +0530

    Exempted - v4.83.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 const Version = "4.83.0"
-const ReleaseDate = "2022-07-05"
+const ReleaseDate = "2022-07-07"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
