9c41724d12025079a8526b112bcb41596e007731
Rakshith Siddanahalli Murukannappa <rakshith.siddanahalli.murukan@oracle.com>
2022-07-15 15:16:42 -0700

    Added - Support for Network Firewall

---
+++ examples/network_firewall/network_firewall_policy/main.tf
@@ -0,0 +1,302 @@
+// Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+variable "compartment_id" {}
+
+variable "network_firewall_policy_decryption_profiles_are_certificate_extensions_restricted" {
+  default = false
+}
+
+variable "network_firewall_policy_decryption_profiles_is_auto_include_alt_name" {
+  default = false
+}
+
+variable "network_firewall_policy_decryption_profiles_is_expired_certificate_blocked" {
+  default = false
+}
+
+variable "network_firewall_policy_decryption_profiles_is_out_of_capacity_blocked" {
+  default = false
+}
+
+variable "network_firewall_policy_decryption_profiles_is_revocation_status_timeout_blocked" {
+  default = false
+}
+
+variable "network_firewall_policy_decryption_profiles_is_unknown_revocation_status_blocked" {
+  default = false
+}
+
+variable "network_firewall_policy_decryption_profiles_is_unsupported_cipher_blocked" {
+  default = false
+}
+
+variable "network_firewall_policy_decryption_profiles_is_unsupported_version_blocked" {
+  default = false
+}
+
+variable "network_firewall_policy_decryption_profiles_is_untrusted_issuer_blocked" {
+  default = false
+}
+
+variable "network_firewall_policy_decryption_profiles_type" {
+  default = "SSL_INBOUND_INSPECTION"
+}
+
+//Application can of type TCP, UDP, or ICMP or ICMP6
+variable "network_firewall_policy_application_lists_type" {
+  default = "ICMP"
+}
+
+variable "network_firewall_policy_application_lists_icmp_type" {
+
+}
+
+variable "network_firewall_policy_application_lists_icmp_code" {
+
+}
+
+variable "network_firewall_policy_application_lists_minimum_port" {
+
+}
+
+variable "network_firewall_policy_application_lists_maximum_port" {
+
+}
+
+variable "network_firewall_policy_url_lists_type" {
+  default = "SIMPLE"
+}
+
+variable "network_firewall_policy_url_lists_pattern" {
+}
+
+variable "network_firewall_policy_decryption_rules_action" {
+  default = "DECRYPT"
+}
+
+variable "kms_key_ocid" {}
+
+variable "network_firewall_policy_decryption_rules_condition_destinations" {
+  default = ["hr_source"]
+}
+
+variable "network_firewall_policy_decryption_rules_condition_sources" {
+  default = ["hr_source"]
+}
+
+variable "network_firewall_policy_decryption_rules_decryption_profile" {
+}
+
+variable "network_firewall_policy_decryption_rules_name" {
+}
+
+variable "network_firewall_policy_decryption_rules_secret" {
+}
+
+variable "network_firewall_policy_defined_tags_value" {
+  default = "value"
+}
+
+variable "network_firewall_policy_display_name" {
+  default = "displayName"
+}
+
+variable "network_firewall_policy_freeform_tags" {
+  default = { "bar-key" = "value" }
+}
+
+variable "network_firewall_policy_id" {
+  default = ""
+}
+
+variable "network_firewall_policy_mapped_secrets_type" {
+  default = "SSL_INBOUND_INSPECTION"
+}
+
+variable "network_firewall_policy_mapped_secrets_version_number" {
+  default = 10
+}
+
+variable "network_firewall_policy_security_rules_action" {
+}
+
+variable "network_firewall_policy_security_rules_condition_applications" {
+  default = ["app-1"]
+}
+
+variable "network_firewall_policy_security_rules_condition_destinations" {
+  default = ["hr_source"]
+}
+
+variable "network_firewall_policy_security_rules_condition_sources" {
+  default = ["hr_source"]
+}
+
+variable "network_firewall_policy_security_rules_condition_urls" {
+  default = ["hr"]
+}
+
+variable "network_firewall_policy_security_rules_inspection" {
+  default = "INTRUSION_DETECTION"
+}
+
+variable "network_firewall_policy_security_rules_name" {
+  default = "hr_access"
+}
+
+variable "network_firewall_policy_state" {
+  default = "ACTIVE"
+}
+
+variable  "network_firewall_policy_ip_address_lists_name" {
+}
+
+variable  "network_firewall_policy_ip_address_lists_value" {
+  default=["10.180.1.0/24", "10.180.2.0/25"]
+}
+
+variable "network_firewall_policy_application_lists_key" {
+}
+
+variable "network_firewall_policy_decryption_profiles_key" {
+}
+
+variable "network_firewall_policy_mapped_secrets_key" {
+}
+
+variable "network_firewall_policy_url_lists_key" {
+}
+
+variable "kms_vault_ocid" {
+}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
+
+resource "oci_vault_secret" "test_secret" {
+  #Required
+  compartment_id = var.compartment_id
+  secret_content {
+    #Required
+    content_type = "BASE64"
+
+    #Optional
+    content = "PHZhcj4mbHQ7YmFzZTY0X2VuY29kZWRfc2VjcmV0X2NvbnRlbnRzJmd0OzwvdmFyPg=="
+    name    = "name"
+    stage   = "CURRENT"
+  }
+  key_id = var.kms_key_ocid
+  secret_name = "TFsample1"
+  vault_id    = var.kms_vault_ocid
+}
+
+resource "oci_network_firewall_network_firewall_policy" "test_network_firewall_policy" {
+  #Required
+  compartment_id = var.compartment_id
+
+  #Optional
+  //application_lists = var.network_firewall_policy_application_lists
+  application_lists {
+    #Required
+    type                           = var.network_firewall_policy_application_lists_type
+    key                            = var.network_firewall_policy_application_lists_key
+
+    #Optional
+    icmp_type                      = var.network_firewall_policy_application_lists_icmp_type
+    icmp_code                      = var.network_firewall_policy_application_lists_icmp_code
+    minimum_port                   = var.network_firewall_policy_application_lists_minimum_port
+    maximum_port                   = var.network_firewall_policy_application_lists_maximum_port
+  }
+  decryption_profiles {
+    #Required
+    is_out_of_capacity_blocked     = var.network_firewall_policy_decryption_profiles_is_out_of_capacity_blocked
+    is_unsupported_cipher_blocked  = var.network_firewall_policy_decryption_profiles_is_unsupported_cipher_blocked
+    is_unsupported_version_blocked = var.network_firewall_policy_decryption_profiles_is_unsupported_version_blocked
+    type                           = var.network_firewall_policy_decryption_profiles_type
+    key                            = var.network_firewall_policy_decryption_profiles_key
+
+    #Optional
+    are_certificate_extensions_restricted = var.network_firewall_policy_decryption_profiles_are_certificate_extensions_restricted
+    is_auto_include_alt_name              = var.network_firewall_policy_decryption_profiles_is_auto_include_alt_name
+    is_expired_certificate_blocked        = var.network_firewall_policy_decryption_profiles_is_expired_certificate_blocked
+    is_revocation_status_timeout_blocked  = var.network_firewall_policy_decryption_profiles_is_revocation_status_timeout_blocked
+    is_unknown_revocation_status_blocked  = var.network_firewall_policy_decryption_profiles_is_unknown_revocation_status_blocked
+    is_untrusted_issuer_blocked           = var.network_firewall_policy_decryption_profiles_is_untrusted_issuer_blocked
+  }
+  decryption_rules {
+    #Required
+    action = var.network_firewall_policy_decryption_rules_action
+    condition {
+
+      #Optional
+      destinations = var.network_firewall_policy_decryption_rules_condition_destinations
+      sources      = var.network_firewall_policy_decryption_rules_condition_sources
+    }
+    name = var.network_firewall_policy_decryption_rules_name
+
+    #Optional
+    decryption_profile = var.network_firewall_policy_decryption_rules_decryption_profile
+    secret             = var.network_firewall_policy_decryption_rules_secret
+  }
+  #defined_tags = {
+   # "${oci_identity_tag_namespace.tag-namespace1.name}.${oci_identity_tag.tag1.name}" = "value"
+  #}
+  display_name     = var.network_firewall_policy_display_name
+  freeform_tags    = var.network_firewall_policy_freeform_tags
+  ip_address_lists {
+    ip_address_list_name = var.network_firewall_policy_ip_address_lists_name
+    ip_address_list_value = var.network_firewall_policy_ip_address_lists_value
+  }
+  mapped_secrets {
+    #Required
+    type            = var.network_firewall_policy_mapped_secrets_type
+    key             = var.network_firewall_policy_mapped_secrets_key
+    vault_secret_id = oci_vault_secret.test_secret.id
+    version_number  = var.network_firewall_policy_mapped_secrets_version_number
+  }
+  security_rules {
+    #Required
+    action = var.network_firewall_policy_security_rules_action
+    condition {
+
+      #Optional
+      applications = var.network_firewall_policy_security_rules_condition_applications
+      destinations = var.network_firewall_policy_security_rules_condition_destinations
+      sources      = var.network_firewall_policy_security_rules_condition_sources
+      urls         = var.network_firewall_policy_security_rules_condition_urls
+    }
+    name = var.network_firewall_policy_security_rules_name
+
+    #Optional
+    inspection = var.network_firewall_policy_security_rules_inspection
+  }
+  url_lists {
+    #Required
+    type                           = var.network_firewall_policy_url_lists_type
+    key                            = var.network_firewall_policy_url_lists_key
+
+    #Optional
+    pattern                        = var.network_firewall_policy_url_lists_pattern
+  }
+}
+
+data "oci_network_firewall_network_firewall_policies" "test_network_firewall_policies" {
+  #Required
+  compartment_id = var.compartment_id
+
+  #Optional
+  display_name = var.network_firewall_policy_display_name
+  id           = var.network_firewall_policy_id
+  state        = var.network_firewall_policy_state
+}
---
+++ website/docs/d/network_firewall_network_firewall.html.markdown
@@ -0,0 +1,51 @@
+---
+subcategory: "Network Firewall"
+layout: "oci"
+page_title: "Oracle Cloud Infrastructure: oci_network_firewall_network_firewall"
+sidebar_current: "docs-oci-datasource-network_firewall-network_firewall"
+description: |-
+  Provides details about a specific Network Firewall in Oracle Cloud Infrastructure Network Firewall service
+---
+
+# Data Source: oci_network_firewall_network_firewall
+This data source provides details about a specific Network Firewall resource in Oracle Cloud Infrastructure Network Firewall service.
+
+Gets a NetworkFirewall by identifier
+
+## Example Usage
+
+```hcl
+data "oci_network_firewall_network_firewall" "test_network_firewall" {
+	#Required
+	network_firewall_id = oci_network_firewall_network_firewall.test_network_firewall.id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `network_firewall_id` - (Required) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the Network Firewall resource.
+
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `availability_domain` - Availability Domain where Network Firewall instance is created. To get a list of availability domains for a tenancy, use [ListAvailabilityDomains](https://docs.cloud.oracle.com/iaas/api/#/en/identity/20160918/AvailabilityDomain/ListAvailabilityDomains) operation. Example: `kIdk:PHX-AD-1` 
+* `compartment_id` - The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment containing the Network Firewall.
+* `defined_tags` - Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{"foo-namespace.bar-key": "value"}` 
+* `display_name` - A user-friendly name for the Network Firewall. Does not have to be unique, and it's changeable. Avoid entering confidential information.
+* `freeform_tags` - Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{"bar-key": "value"}` 
+* `id` - The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the Network Firewall resource.
+* `ipv4address` - IPv4 address for the Network Firewall.
+* `ipv6address` - IPv6 address for the Network Firewall.
+* `lifecycle_details` - A message describing the current state in more detail. For example, can be used to provide actionable information for a resource in Failed state.
+* `network_firewall_policy_id` - The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the Network Firewall Policy.
+* `network_security_group_ids` - An array of network security groups [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) associated with the Network Firewall.
+* `state` - The current state of the Network Firewall.
+* `subnet_id` - The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the subnet associated with the Network Firewall.
+* `system_tags` - Usage of system tag keys. These predefined keys are scoped to namespaces. Example: `{"orcl-cloud.free-tier-retained": "true"}` 
+* `time_created` - The time instant at which the Network Firewall was created in the format defined by [RFC3339](https://tools.ietf.org/html/rfc3339). Example: `2016-08-25T21:10:29.600Z` 
+* `time_updated` - The time instant at which the Network Firewall was updated in the format defined by [RFC3339](https://tools.ietf.org/html/rfc3339). Example: `2016-08-25T21:10:29.600Z` 
+
