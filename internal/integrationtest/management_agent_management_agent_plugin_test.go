634ddf0baa4c628f6332a6b805ee399be9830a92
rambridg <richard.ambridge@oracle.com>
2022-07-21 12:30:37 +0100

    Added - management_agents compartment_id_in_subtree and agent in pluginlist

--- internal/integrationtest/management_agent_management_agent_plugin_test.go
+++ internal/integrationtest/management_agent_management_agent_plugin_test.go
@@ -17,6 +17,7 @@ import (
 var (
 	managementAgentPluginDataSourceRepresentation = map[string]interface{}{
 		"compartment_id": acctest.Representation{RepType: acctest.Required, Create: `${var.compartment_id}`},
+		"agent_id":       acctest.Representation{RepType: acctest.Optional, Create: `${var.test_agent_id}`},
 		"platform_type":  acctest.Representation{RepType: acctest.Optional, Create: []string{`LINUX`}},
 		"state":          acctest.Representation{RepType: acctest.Optional, Create: `ACTIVE`},
 	}
@@ -44,6 +45,51 @@ func TestManagementAgentManagementAgentPluginResource_basic(t *testing.T) {
 			Config: config +
 				acctest.GenerateDataSourceFromRepresentationMap("oci_management_agent_management_agent_plugins", "test_management_agent_plugins", acctest.Required, acctest.Create, managementAgentPluginDataSourceRepresentation) +
 				compartmentIdVariableStr + ManagementAgentPluginResourceConfig,
+			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+				resource.TestCheckResourceAttr(datasourceName, "compartment_id", compartmentId),
+
+				resource.TestCheckResourceAttrSet(datasourceName, "management_agent_plugins.#"),
+				resource.TestCheckResourceAttrSet(datasourceName, "management_agent_plugins.0.display_name"),
+				resource.TestCheckResourceAttrSet(datasourceName, "management_agent_plugins.0.id"),
+				resource.TestCheckResourceAttrSet(datasourceName, "management_agent_plugins.0.is_console_deployable"),
+				resource.TestCheckResourceAttrSet(datasourceName, "management_agent_plugins.0.name"),
+				resource.TestCheckResourceAttrSet(datasourceName, "management_agent_plugins.0.state"),
+				resource.TestCheckResourceAttrSet(datasourceName, "management_agent_plugins.0.supported_platform_types.#"),
+				resource.TestCheckResourceAttrSet(datasourceName, "management_agent_plugins.0.version"),
+			),
+		},
+	})
+}
+
+func TestManagementAgentManagementAgentPluginResource_withagent(t *testing.T) {
+	httpreplay.SetScenario("TestManagementAgentManagementAgentPluginResource_withagent")
+	defer httpreplay.SaveScenario()
+
+	config := acctest.ProviderTestConfig()
+
+	compartmentId := utils.GetEnvSettingWithBlankDefault("compartment_ocid")
+	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
+
+	managementAgentIds, err := getManagementAgentIds(compartmentId)
+	if err != nil {
+		t.Errorf("Failed to get agents in compartment %s", err)
+	}
+	if len(managementAgentIds) == 0 {
+		t.Errorf("Failed to find any active agents in compartment %s", compartmentId)
+	}
+	managementAgentId := managementAgentIds[0]
+	managementAgentIdVariableStr := fmt.Sprintf("variable \"test_agent_id\" { default = \"%s\" }\n", managementAgentId)
+
+	datasourceName := "data.oci_management_agent_management_agent_plugins.test_management_agent_plugins"
+
+	acctest.SaveConfigContent("", "", "", t)
+
+	acctest.ResourceTest(t, nil, []resource.TestStep{
+		// verify datasource
+		{
+			Config: config +
+				acctest.GenerateDataSourceFromRepresentationMap("oci_management_agent_management_agent_plugins", "test_management_agent_plugins", acctest.Optional, acctest.Create, managementAgentPluginDataSourceRepresentation) +
+				compartmentIdVariableStr + managementAgentIdVariableStr + ManagementAgentPluginResourceConfig,
 			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
 				resource.TestCheckResourceAttr(datasourceName, "compartment_id", compartmentId),
 
