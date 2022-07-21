c7566ba395486388c2490f1f4f2972de8995fcc3
abzambre <abhishek.zambre@oracle.com>
2022-07-21 11:10:55 -0700

    Added - Support for new fields port, protocol, role, and ssl secret id to enable/modify database management service

--- internal/integrationtest/database_cloud_database_management_test.go
+++ internal/integrationtest/database_cloud_database_management_test.go
@@ -14,33 +14,77 @@ import (
 )
 
 var (
-	DatabaseCloudDatabaseManagementRepresentation = map[string]interface{}{
+	cloudDatabaseManagementPrivateEndpointRepresentation = map[string]interface{}{
+		"compartment_id": acctest.Representation{RepType: acctest.Required, Create: `${var.compartment_id}`},
+		"name":           acctest.Representation{RepType: acctest.Required, Create: `TFPEforTCPS`, Update: `name2`},
+		"subnet_id":      acctest.Representation{RepType: acctest.Required, Create: `${oci_core_subnet.test_subnet.id}`},
+	}
+
+	cloudDatabaseManagementRepresentation = map[string]interface{}{
 		"database_id": acctest.Representation{RepType: acctest.Required, Create: `${data.oci_database_databases.t.databases.0.id}`},
 		//Update: `ADVANCED` to be uncommented to explicitly call ModifyDatabaseManagement
-		"management_type":      acctest.Representation{RepType: acctest.Required, Create: `BASIC` /*, Update: `ADVANCED`*/},
-		"private_end_point_id": acctest.Representation{RepType: acctest.Required, Create: `ocid1.dbmgmtprivateendpoint.oc1.ap-hyderabad-1.amaaaaaacsc5xjaamlmllhfxmxict6jf3irizwsydralyklninmwsrovggkq`},
-		"service_name":         acctest.Representation{RepType: acctest.Required, Create: `DB0809_hyd17q.sub02231620340.dbmgmtcustomer.oraclevcn.com`},
-		"credentialdetails":    acctest.RepresentationGroup{RepType: acctest.Required, Group: DatabaseCloudDatabaseManagementCredentialDetailsRepresentation},
+		"management_type":      acctest.Representation{RepType: acctest.Required, Create: `BASIC`, Update: `ADVANCED`},
+		"private_end_point_id": acctest.Representation{RepType: acctest.Required, Create: `${oci_database_management_db_management_private_endpoint.test_db_management_private_endpoint.id}`},
+		"service_name":         acctest.Representation{RepType: acctest.Required, Create: `${data.oci_database_databases.t.databases.0.db_unique_name}.${oci_core_subnet.test_subnet.subnet_domain_name}`},
+		"credentialdetails":    acctest.RepresentationGroup{RepType: acctest.Required, Group: cloudDatabaseManagementCredentialDetailsRepresentation},
 		//Update: `false` to be uncommented to explicitly call DisableDatabaseManagement
-		"enable_management": acctest.Representation{RepType: acctest.Required, Create: `true`, Update: `false`},
+		"enable_management": acctest.Representation{RepType: acctest.Required, Create: `true` /*, Update: `false`*/},
+		"protocol":          acctest.Representation{RepType: acctest.Optional, Create: `TCP`, Update: `TCPS`},
+		"port":              acctest.Representation{RepType: acctest.Optional, Create: `1521`, Update: `1522`},
+		"role":              acctest.Representation{RepType: acctest.Optional, Create: `NORMAL`, Update: `SYSDBA`},
+		"ssl_secret_id":     acctest.Representation{RepType: acctest.Optional, Create: `${var.sslSecretId}`, Update: `${var.sslSecretId}`},
 	}
 
-	DatabaseCloudDatabaseManagementCredentialDetailsRepresentation = map[string]interface{}{
-		"user_name":          acctest.Representation{RepType: acctest.Required, Create: `dbsnmp`},
-		"password_secret_id": acctest.Representation{RepType: acctest.Required, Create: `ocid1.vaultsecret.oc1.ap-hyderabad-1.amaaaaaacsc5xjaa2q7r6kfzdm44ylxqwomht6uinb5zyhezka7sl2t62ecq`},
+	cloudDatabaseManagementCredentialDetailsRepresentation = map[string]interface{}{
+		"user_name":          acctest.Representation{RepType: acctest.Required, Create: `sys`},
+		"password_secret_id": acctest.Representation{RepType: acctest.Required, Create: `${var.sslSecretId}`},
 	}
 
-	DatabaseCloudDatabaseManagementDbSystemForDatabaseManagementRepresentation = `
+	cloudDatabaseManagementPrivateEndpointConfig = acctest.GenerateResourceFromRepresentationMap("oci_database_management_db_management_private_endpoint", "test_db_management_private_endpoint", acctest.Required, acctest.Create, cloudDatabaseManagementPrivateEndpointRepresentation)
+
+	CloudDatabaseManagementResourceDependenciesBase = AvailabilityDomainConfig + DefinedTagsDependencies + CoreVcnResourceConfig +
+		`
+			resource "oci_core_subnet" "test_subnet" {
+			  availability_domain = "${data.oci_identity_availability_domains.test_availability_domains.availability_domains.0.name}"
+			  cidr_block          = "10.0.2.0/24"
+			  display_name        = "TFADSubnet"
+			  dns_label           = "adsubnet"
+			  compartment_id      = "${var.compartment_id}"
+			  vcn_id              = "${oci_core_vcn.test_vcn.id}"
+			  security_list_ids   = ["${oci_core_security_list.test_security_list.id}"]
+			  route_table_id      = "${oci_core_vcn.test_vcn.default_route_table_id}"
+			  dhcp_options_id     = "${oci_core_vcn.test_vcn.default_dhcp_options_id}"
+			}
+			
+			resource "oci_core_security_list" "test_security_list" {
+			  compartment_id = "${var.compartment_id}"
+			  vcn_id         = "${oci_core_vcn.test_vcn.id}"
+			  display_name   = "TFExampleSecurityList"
+			
+			  // allow outbound tcp traffic on all ports
+			  egress_security_rules {
+				destination = "0.0.0.0/0"
+				protocol    = "6"
+			  }
+			
+			  ingress_security_rules {
+				protocol  = "6"
+				source    = "0.0.0.0/0"
+			  }
+			}
+		`
+
+	CloudDatabaseManagementResourceDependencies = CloudDatabaseManagementResourceDependenciesBase + cloudDatabaseManagementPrivateEndpointConfig + `
 		resource "oci_database_db_system" "t" {
-			availability_domain = "${var.availability_domain}"
+			availability_domain = "${lower("${data.oci_identity_availability_domains.test_availability_domains.availability_domains.0.name}")}"
 			compartment_id = "${var.compartment_id}"
-			subnet_id = "${var.subnet_id}"
+			subnet_id = "${oci_core_subnet.test_subnet.id}"
 			database_edition = "ENTERPRISE_EDITION"
 			disk_redundancy = "NORMAL"
 			shape = "VM.Standard2.1"
 			ssh_public_keys = ["ssh-rsa KKKLK3NzaC1yc2EAAAADAQABAAABAQC+UC9MFNA55NIVtKPIBCNw7++ACXhD0hx+Zyj25JfHykjz/QU3Q5FAU3DxDbVXyubgXfb/GJnrKRY8O4QDdvnZZRvQFFEOaApThAmCAM5MuFUIHdFvlqP+0W+ZQnmtDhwVe2NCfcmOrMuaPEgOKO3DOW6I/qOOdO691Xe2S9NgT9HhN0ZfFtEODVgvYulgXuCCXsJs+NUqcHAOxxFUmwkbPvYi0P0e2DT8JKeiOOC8VKUEgvVx+GKmqasm+Y6zHFW7vv3g2GstE1aRs3mttHRoC/JPM86PRyIxeWXEMzyG5wHqUu4XZpDbnWNxi6ugxnAGiL3CrIFdCgRNgHz5qS1l MustWin"]
-			display_name = "-tf-dbSystem-001"
-			domain = "${var.domain_name}"
+			display_name = "TFVMDBSystemForTCPS"
+			domain = "${oci_core_subnet.test_subnet.subnet_domain_name}"
 			hostname = "myOracleDB" // this will be lowercased server side
 			data_storage_size_in_gb = "256"
 			license_model = "LICENSE_INCLUDED"
@@ -48,7 +92,7 @@ var (
 			fault_domains = ["FAULT-DOMAIN-1"]
 			db_home {
 				db_version = "19.0.0.0"
-				display_name = "-tf-db-home"
+				display_name = "aTFdbhome"
 				database {
 					admin_password = "FIpassword12##"
 					db_name = "aTFdb"
@@ -115,14 +159,8 @@ func TestDatabaseCloudDatabaseManagementResource_basic(t *testing.T) {
 	compartmentId := utils.GetEnvSettingWithBlankDefault("compartment_ocid")
 	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
 
-	domainName := utils.GetEnvSettingWithBlankDefault("domain_name")
-	domainNameVariableStr := fmt.Sprintf("variable \"domain_name\" { default = \"%s\" }\n", domainName)
-
-	subnetId := utils.GetEnvSettingWithBlankDefault("subnet_id")
-	subnetIdVariableStr := fmt.Sprintf("variable \"subnet_id\" { default = \"%s\" }\n", subnetId)
-
-	availabilityDomain := utils.GetEnvSettingWithBlankDefault("availability_domain")
-	availabilityDomainVariableStr := fmt.Sprintf("variable \"availability_domain\" { default = \"%s\" }\n", availabilityDomain)
+	sslSecretId := utils.GetEnvSettingWithDefault("ssl_secret_id", "test_secret_id")
+	sslSecretIdVariableStr := fmt.Sprintf("variable \"sslSecretId\" { default = \"%s\" }\n", sslSecretId)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() { acctest.PreCheck(t) },
@@ -132,23 +170,20 @@ func TestDatabaseCloudDatabaseManagementResource_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Create dbSystem
 			{
-				Config: config + compartmentIdVariableStr + domainNameVariableStr + subnetIdVariableStr +
-					availabilityDomainVariableStr + DatabaseCloudDatabaseManagementDbSystemForDatabaseManagementRepresentation,
+				Config: config + compartmentIdVariableStr + sslSecretIdVariableStr + CloudDatabaseManagementResourceDependencies,
 			},
-			// enable database management
+			// enable database management with required fields
 			{
-				Config: config + compartmentIdVariableStr + domainNameVariableStr + subnetIdVariableStr +
-					availabilityDomainVariableStr + DatabaseCloudDatabaseManagementDbSystemForDatabaseManagementRepresentation +
-					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Required, acctest.Create, DatabaseCloudDatabaseManagementRepresentation),
+				Config: config + compartmentIdVariableStr + sslSecretIdVariableStr + CloudDatabaseManagementResourceDependencies +
+					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Required, acctest.Create, cloudDatabaseManagementRepresentation),
 				Check: acctest.ComposeAggregateTestCheckFuncWrapper(
 					resource.TestCheckResourceAttr("data.oci_database_databases.t", "databases.#", "1"),
 				),
 			},
-			// verify enable database management
+			// verify enable database management with required fields
 			{
-				Config: config + compartmentIdVariableStr + domainNameVariableStr + subnetIdVariableStr +
-					availabilityDomainVariableStr + DatabaseCloudDatabaseManagementDbSystemForDatabaseManagementRepresentation +
-					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Required, acctest.Create, DatabaseCloudDatabaseManagementRepresentation),
+				Config: config + compartmentIdVariableStr + sslSecretIdVariableStr + CloudDatabaseManagementResourceDependencies +
+					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Required, acctest.Create, cloudDatabaseManagementRepresentation),
 				Check: acctest.ComposeAggregateTestCheckFuncWrapper(
 					resource.TestCheckResourceAttr("data.oci_database_databases.t", "databases.#", "1"),
 					resource.TestCheckResourceAttrSet("data.oci_database_database.t", "db_name"),
@@ -156,22 +191,55 @@ func TestDatabaseCloudDatabaseManagementResource_basic(t *testing.T) {
 					resource.TestCheckResourceAttr("data.oci_database_database.t", "database_management_config.0.management_type", "BASIC"),
 				),
 			},
-			// Update / disable database management
+			// Update / disable database management with required fields
 			{
-				Config: config + compartmentIdVariableStr + domainNameVariableStr + subnetIdVariableStr +
-					availabilityDomainVariableStr + DatabaseCloudDatabaseManagementDbSystemForDatabaseManagementRepresentation +
-					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Required, acctest.Update, DatabaseCloudDatabaseManagementRepresentation),
+				Config: config + compartmentIdVariableStr + sslSecretIdVariableStr + CloudDatabaseManagementResourceDependencies +
+					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Required, acctest.Update, cloudDatabaseManagementRepresentation),
 			},
-			// verify Update / disable database management
+			// verify Update / disable database management with required fields
 			{
-				Config: config + compartmentIdVariableStr + domainNameVariableStr + subnetIdVariableStr +
-					availabilityDomainVariableStr + DatabaseCloudDatabaseManagementDbSystemForDatabaseManagementRepresentation +
-					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Required, acctest.Update, DatabaseCloudDatabaseManagementRepresentation),
+				Config: config + compartmentIdVariableStr + sslSecretIdVariableStr + CloudDatabaseManagementResourceDependencies +
+					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Required, acctest.Update, cloudDatabaseManagementRepresentation),
 				Check: acctest.ComposeAggregateTestCheckFuncWrapper(
 					resource.TestCheckResourceAttr("data.oci_database_databases.t", "databases.#", "1"),
 					resource.TestCheckResourceAttrSet("data.oci_database_database.t", "db_name"),
-					resource.TestCheckResourceAttr("data.oci_database_database.t", "database_management_config.0.management_status", "DISABLED"),
-					//resource.TestCheckResourceAttr("data.oci_database_database.t", "database_management_config.0.management_type", "ADVANCED"),
+					//resource.TestCheckResourceAttr("data.oci_database_database.t", "database_management_config.0.management_status", "DISABLED"),
+					resource.TestCheckResourceAttr("data.oci_database_database.t", "database_management_config.0.management_type", "ADVANCED"),
+				),
+			},
+			// enable database management with optional fields (for TCPS)
+			{
+				Config: config + compartmentIdVariableStr + sslSecretIdVariableStr + CloudDatabaseManagementResourceDependencies +
+					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Optional, acctest.Create, cloudDatabaseManagementRepresentation),
+				Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+					resource.TestCheckResourceAttr("data.oci_database_databases.t", "databases.#", "1"),
+				),
+			},
+			// verify enable database management with optional fields (for TCPS)
+			{
+				Config: config + compartmentIdVariableStr + sslSecretIdVariableStr + CloudDatabaseManagementResourceDependencies +
+					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Optional, acctest.Create, cloudDatabaseManagementRepresentation),
+				Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+					resource.TestCheckResourceAttr("data.oci_database_databases.t", "databases.#", "1"),
+					resource.TestCheckResourceAttrSet("data.oci_database_database.t", "db_name"),
+					resource.TestCheckResourceAttr("data.oci_database_database.t", "database_management_config.0.management_status", "ENABLED"),
+					resource.TestCheckResourceAttr("data.oci_database_database.t", "database_management_config.0.management_type", "BASIC"),
+				),
+			},
+			// Update / disable database management with optional fields (for TCPS)
+			{
+				Config: config + compartmentIdVariableStr + sslSecretIdVariableStr + CloudDatabaseManagementResourceDependencies +
+					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Optional, acctest.Update, cloudDatabaseManagementRepresentation),
+			},
+			// verify Update / disable database management with optional fields (for TCPS)
+			{
+				Config: config + compartmentIdVariableStr + sslSecretIdVariableStr + CloudDatabaseManagementResourceDependencies +
+					acctest.GenerateResourceFromRepresentationMap("oci_database_cloud_database_management", "test_database_cloud_database_management", acctest.Optional, acctest.Update, cloudDatabaseManagementRepresentation),
+				Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+					resource.TestCheckResourceAttr("data.oci_database_databases.t", "databases.#", "1"),
+					resource.TestCheckResourceAttrSet("data.oci_database_database.t", "db_name"),
+					//resource.TestCheckResourceAttr("data.oci_database_database.t", "database_management_config.0.management_status", "DISABLED"),
+					resource.TestCheckResourceAttr("data.oci_database_database.t", "database_management_config.0.management_type", "ADVANCED"),
 				),
 			},
 		},
