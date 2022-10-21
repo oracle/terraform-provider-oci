88ff1449da39780f8594d9c270f1cc9e4e21384c
Terraform Team Automation <no-reply@oracle.com>
2022-10-21 01:10:46 +0000

    Added - Support for Dev and Deployment of Site Guard as Service in OCI

---
+++ vendor/github.com/oracle/oci-go-sdk/v65/disasterrecovery/dr_plan_lifecycle_state.go
@@ -0,0 +1,82 @@
+// Copyright (c) 2016, 2018, 2022, Oracle and/or its affiliates.  All rights reserved.
+// This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
+// Code generated. DO NOT EDIT.
+
+// Full Stack Disaster Recovery API
+//
+// Use the Full Stack Disaster Recovery (FSDR) API to manage disaster recovery for business applications.
+// FSDR is an OCI disaster recovery orchestration and management service that provides comprehensive disaster recovery
+// capabilities for all layers of an application stack, including infrastructure, middleware, database, and application.
+//
+
+package disasterrecovery
+
+import (
+	"strings"
+)
+
+// DrPlanLifecycleStateEnum Enum with underlying type: string
+type DrPlanLifecycleStateEnum string
+
+// Set of constants representing the allowable values for DrPlanLifecycleStateEnum
+const (
+	DrPlanLifecycleStateCreating       DrPlanLifecycleStateEnum = "CREATING"
+	DrPlanLifecycleStateUpdating       DrPlanLifecycleStateEnum = "UPDATING"
+	DrPlanLifecycleStateActive         DrPlanLifecycleStateEnum = "ACTIVE"
+	DrPlanLifecycleStateInactive       DrPlanLifecycleStateEnum = "INACTIVE"
+	DrPlanLifecycleStateDeleting       DrPlanLifecycleStateEnum = "DELETING"
+	DrPlanLifecycleStateDeleted        DrPlanLifecycleStateEnum = "DELETED"
+	DrPlanLifecycleStateFailed         DrPlanLifecycleStateEnum = "FAILED"
+	DrPlanLifecycleStateNeedsAttention DrPlanLifecycleStateEnum = "NEEDS_ATTENTION"
+)
+
+var mappingDrPlanLifecycleStateEnum = map[string]DrPlanLifecycleStateEnum{
+	"CREATING":        DrPlanLifecycleStateCreating,
+	"UPDATING":        DrPlanLifecycleStateUpdating,
+	"ACTIVE":          DrPlanLifecycleStateActive,
+	"INACTIVE":        DrPlanLifecycleStateInactive,
+	"DELETING":        DrPlanLifecycleStateDeleting,
+	"DELETED":         DrPlanLifecycleStateDeleted,
+	"FAILED":          DrPlanLifecycleStateFailed,
+	"NEEDS_ATTENTION": DrPlanLifecycleStateNeedsAttention,
+}
+
+var mappingDrPlanLifecycleStateEnumLowerCase = map[string]DrPlanLifecycleStateEnum{
+	"creating":        DrPlanLifecycleStateCreating,
+	"updating":        DrPlanLifecycleStateUpdating,
+	"active":          DrPlanLifecycleStateActive,
+	"inactive":        DrPlanLifecycleStateInactive,
+	"deleting":        DrPlanLifecycleStateDeleting,
+	"deleted":         DrPlanLifecycleStateDeleted,
+	"failed":          DrPlanLifecycleStateFailed,
+	"needs_attention": DrPlanLifecycleStateNeedsAttention,
+}
+
+// GetDrPlanLifecycleStateEnumValues Enumerates the set of values for DrPlanLifecycleStateEnum
+func GetDrPlanLifecycleStateEnumValues() []DrPlanLifecycleStateEnum {
+	values := make([]DrPlanLifecycleStateEnum, 0)
+	for _, v := range mappingDrPlanLifecycleStateEnum {
+		values = append(values, v)
+	}
+	return values
+}
+
+// GetDrPlanLifecycleStateEnumStringValues Enumerates the set of values in String for DrPlanLifecycleStateEnum
+func GetDrPlanLifecycleStateEnumStringValues() []string {
+	return []string{
+		"CREATING",
+		"UPDATING",
+		"ACTIVE",
+		"INACTIVE",
+		"DELETING",
+		"DELETED",
+		"FAILED",
+		"NEEDS_ATTENTION",
+	}
+}
+
+// GetMappingDrPlanLifecycleStateEnum performs case Insensitive comparison on enum value and return the desired enum
+func GetMappingDrPlanLifecycleStateEnum(val string) (DrPlanLifecycleStateEnum, bool) {
+	enum, ok := mappingDrPlanLifecycleStateEnumLowerCase[strings.ToLower(val)]
+	return enum, ok
+}
---
+++ vendor/github.com/oracle/oci-go-sdk/v65/disasterrecovery/update_dr_plan_details.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2016, 2018, 2022, Oracle and/or its affiliates.  All rights reserved.
+// This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
+// Code generated. DO NOT EDIT.
+
+// Full Stack Disaster Recovery API
+//
+// Use the Full Stack Disaster Recovery (FSDR) API to manage disaster recovery for business applications.
+// FSDR is an OCI disaster recovery orchestration and management service that provides comprehensive disaster recovery
+// capabilities for all layers of an application stack, including infrastructure, middleware, database, and application.
+//
+
+package disasterrecovery
+
+import (
+	"fmt"
+	"github.com/oracle/oci-go-sdk/v65/common"
+	"strings"
+)
+
+// UpdateDrPlanDetails The details for updating a DR Plan.
+type UpdateDrPlanDetails struct {
+
+	// The display name of the DR Plan being updated.
+	// Example: `EBS Switchover PHX to IAD`
+	DisplayName *string `mandatory:"false" json:"displayName"`
+
+	// An ordered list of plan groups in a DR Plan.
+	PlanGroups []UpdateDrPlanGroupDetails `mandatory:"false" json:"planGroups"`
+
+	// Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only.
+	// Example: `{"Department": "Finance"}`
+	FreeformTags map[string]string `mandatory:"false" json:"freeformTags"`
+
+	// Defined tags for this resource. Each key is predefined and scoped to a namespace.
+	// Example: `{"Operations": {"CostCenter": "42"}}`
+	DefinedTags map[string]map[string]interface{} `mandatory:"false" json:"definedTags"`
+}
+
+func (m UpdateDrPlanDetails) String() string {
+	return common.PointerString(m)
+}
+
+// ValidateEnumValue returns an error when providing an unsupported enum value
+// This function is being called during constructing API request process
+// Not recommended for calling this function directly
+func (m UpdateDrPlanDetails) ValidateEnumValue() (bool, error) {
+	errMessage := []string{}
+
+	if len(errMessage) > 0 {
+		return true, fmt.Errorf(strings.Join(errMessage, "\n"))
+	}
+	return false, nil
+}
