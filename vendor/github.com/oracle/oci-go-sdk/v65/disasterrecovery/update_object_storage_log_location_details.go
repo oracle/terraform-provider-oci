d25553f4fbd1218cdff183c2ede5eed14d9b1cd7
Terraform Team Automation <no-reply@oracle.com>
2022-10-21 01:10:46 +0000

    Added - Support for Dev and Deployment of Site Guard as Service in OCI

---
+++ vendor/github.com/oracle/oci-go-sdk/v65/disasterrecovery/update_object_storage_log_location_details.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2016, 2018, 2022, Oracle and/or its affiliates.  All rights reserved.
+// This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
+// Code generated. DO NOT EDIT.
+
+// Full Stack Disaster Recovery API
+//
+// Use the Full Stack Disaster Recovery (FSDR) API to manage disaster recovery for business applications.
+// FSDR is an OCI disaster recovery orchestration and management service that provides comprehensive disaster recovery
+// capabilities for all layers of an application stack, including infrastructure, middleware, database, and application.
+//
+
+package disasterrecovery
+
+import (
+	"fmt"
+	"github.com/oracle/oci-go-sdk/v65/common"
+	"strings"
+)
+
+// UpdateObjectStorageLogLocationDetails Information about updating an Object Storage log location for a DR Protection Group.
+type UpdateObjectStorageLogLocationDetails struct {
+
+	// The namespace in Object Storage (Note - this is usually the tenancy name).
+	// Example: `myocitenancy`
+	Namespace *string `mandatory:"true" json:"namespace"`
+
+	// The bucket name inside the Object Storage namespace.
+	// Example: `operation_logs`
+	Bucket *string `mandatory:"true" json:"bucket"`
+}
+
+func (m UpdateObjectStorageLogLocationDetails) String() string {
+	return common.PointerString(m)
+}
+
+// ValidateEnumValue returns an error when providing an unsupported enum value
+// This function is being called during constructing API request process
+// Not recommended for calling this function directly
+func (m UpdateObjectStorageLogLocationDetails) ValidateEnumValue() (bool, error) {
+	errMessage := []string{}
+
+	if len(errMessage) > 0 {
+		return true, fmt.Errorf(strings.Join(errMessage, "\n"))
+	}
+	return false, nil
+}
