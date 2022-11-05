3a660e77b4aadaa1d0cc1837113dd1cf4059191a
Viral Sinha <viral.sinha@oracle.com>
2022-11-04 18:39:08 -0700

    Finalize changelog and release for version v4.99.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "4.98.0"
-const ReleaseDate = "2022-11-05"
+const Version = "4.99.0"
+const ReleaseDate = "2022-11-09"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
