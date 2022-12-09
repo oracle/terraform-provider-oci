37edc7d99c9cd15b8a329e1bc61c5632319c2437
Terraform Team Automation <no-reply@oracle.com>
2022-12-09 03:16:19 +0000

    Exempted - Preview SDK - DEXREQ-3733

---
+++ vendor/github.com/oracle/oci-go-sdk/v65/queue/put_messages.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2016, 2018, 2022, Oracle and/or its affiliates.  All rights reserved.
+// This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
+// Code generated. DO NOT EDIT.
+
+// Queue API
+//
+// A description of the Queue API
+//
+
+package queue
+
+import (
+	"fmt"
+	"github.com/oracle/oci-go-sdk/v65/common"
+	"strings"
+)
+
+// PutMessages A list of the messages published in a queue.
+type PutMessages struct {
+
+	// The messages that have been published in a queue.
+	Messages []PutMessage `mandatory:"true" json:"messages"`
+}
+
+func (m PutMessages) String() string {
+	return common.PointerString(m)
+}
+
+// ValidateEnumValue returns an error when providing an unsupported enum value
+// This function is being called during constructing API request process
+// Not recommended for calling this function directly
+func (m PutMessages) ValidateEnumValue() (bool, error) {
+	errMessage := []string{}
+
+	if len(errMessage) > 0 {
+		return true, fmt.Errorf(strings.Join(errMessage, "\n"))
+	}
+	return false, nil
+}
