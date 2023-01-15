ea1c18214ec47a81aefafd413134e7dc1a81a50b
Viral Sinha <viral.sinha@oracle.com>
2023-01-15 01:12:06 -0800

    Exempted - Finalize oci.erb and release for version v4.104.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "4.103.0"
-const ReleaseDate = "2023-01-11"
+const Version = "4.104.0"
+const ReleaseDate = "2023-01-20"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
