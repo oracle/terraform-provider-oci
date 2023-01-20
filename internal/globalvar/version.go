18407ef1f7ebc54ac96eaaad58bc610aabc3da23
Sagar Pokale <sagar.pokale@oracle.com>
2023-01-20 15:22:22 -0600

    Finalize changelog and release for version v4.105.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "4.104.0"
-const ReleaseDate = "2023-01-20"
+const Version = "4.105.0"
+const ReleaseDate = "2023-01-25"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
