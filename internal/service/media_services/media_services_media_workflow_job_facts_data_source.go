890845adcbcb74447d19e5570a3ba43c6b977a3b
rashik_bhasin <rashik.bhasin@oracle.com>
2023-02-09 17:15:43 -0800

    Exempted - Breaking change commented in Media Service

--- internal/service/media_services/media_services_media_workflow_job_facts_data_source.go
+++ internal/service/media_services/media_services_media_workflow_job_facts_data_source.go
@@ -4,10 +4,6 @@
 package media_services
 
 import (
-	"context"
-	"fmt"
-	"strconv"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	oci_media_services "github.com/oracle/oci-go-sdk/v65/mediaservices"
 
@@ -88,7 +84,7 @@ func readMediaServicesMediaWorkflowJobFacts(d *schema.ResourceData, m interface{
 type MediaServicesMediaWorkflowJobFactsDataSourceCrud struct {
 	D      *schema.ResourceData
 	Client *oci_media_services.MediaServicesClient
-	Res    *oci_media_services.ListMediaWorkflowJobFactsResponse
+	//Res    *oci_media_services.ListMediaWorkflowJobFactsResponse
 }
 
 func (s *MediaServicesMediaWorkflowJobFactsDataSourceCrud) VoidState() {
@@ -96,93 +92,93 @@ func (s *MediaServicesMediaWorkflowJobFactsDataSourceCrud) VoidState() {
 }
 
 func (s *MediaServicesMediaWorkflowJobFactsDataSourceCrud) Get() error {
-	request := oci_media_services.ListMediaWorkflowJobFactsRequest{}
-
-	if key, ok := s.D.GetOkExists("key"); ok {
-		intValue := 0
-		_, _ = fmt.Sscan(key.(string), &intValue)
-		request.Key = &intValue
-	}
-
-	if mediaWorkflowJobId, ok := s.D.GetOkExists("media_workflow_job_id"); ok {
-		tmp := mediaWorkflowJobId.(string)
-		request.MediaWorkflowJobId = &tmp
-	}
-
-	if type_, ok := s.D.GetOkExists("type"); ok {
-		request.Type = oci_media_services.ListMediaWorkflowJobFactsTypeEnum(type_.(string))
-	}
-
-	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(false, "media_services")
-	request.RequestMetadata.RetryPolicy.MaximumNumberAttempts = 1
-
-	response, err := s.Client.ListMediaWorkflowJobFacts(context.Background(), request)
-	if err != nil && response.RawResponse.StatusCode != 404 {
-		return err
-	}
-
-	s.Res = &response
-	request.Page = s.Res.OpcNextPage
-
-	for request.Page != nil {
-		listResponse, err := s.Client.ListMediaWorkflowJobFacts(context.Background(), request)
-		if err != nil {
-			return err
-		}
-
-		s.Res.Items = append(s.Res.Items, listResponse.Items...)
-		request.Page = listResponse.OpcNextPage
-	}
+	//request := oci_media_services.ListMediaWorkflowJobFactsRequest{}
+	//
+	//if key, ok := s.D.GetOkExists("key"); ok {
+	//	intValue := 0
+	//	_, _ = fmt.Sscan(key.(string), &intValue)
+	//	request.Key = &intValue
+	//}
+	//
+	//if mediaWorkflowJobId, ok := s.D.GetOkExists("media_workflow_job_id"); ok {
+	//	tmp := mediaWorkflowJobId.(string)
+	//	request.MediaWorkflowJobId = &tmp
+	//}
+	//
+	//if type_, ok := s.D.GetOkExists("type"); ok {
+	//	request.Type = oci_media_services.ListMediaWorkflowJobFactsTypeEnum(type_.(string))
+	//}
+	//
+	//request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(false, "media_services")
+	//request.RequestMetadata.RetryPolicy.MaximumNumberAttempts = 1
+	//
+	//response, err := s.Client.ListMediaWorkflowJobFacts(context.Background(), request)
+	//if err != nil && response.RawResponse.StatusCode != 404 {
+	//	return err
+	//}
+	//
+	//s.Res = &response
+	//request.Page = s.Res.OpcNextPage
+	//
+	//for request.Page != nil {
+	//	listResponse, err := s.Client.ListMediaWorkflowJobFacts(context.Background(), request)
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	s.Res.Items = append(s.Res.Items, listResponse.Items...)
+	//	request.Page = listResponse.OpcNextPage
+	//}
 
 	return nil
 }
 
 func (s *MediaServicesMediaWorkflowJobFactsDataSourceCrud) SetData() error {
-	if s.Res == nil {
-		return nil
-	}
-
-	s.D.SetId(tfresource.GenerateDataSourceHashID("MediaServicesMediaWorkflowJobFactsDataSource-", MediaServicesMediaWorkflowJobFactsDataSource(), s.D))
-	resources := []map[string]interface{}{}
-	mediaWorkflowJobFact := map[string]interface{}{}
-
-	items := []interface{}{}
-	for _, item := range s.Res.Items {
-		items = append(items, MediaWorkflowJobFactSummaryToMap(item))
-	}
-	mediaWorkflowJobFact["items"] = items
-
-	if f, fOk := s.D.GetOkExists("filter"); fOk {
-		items = tfresource.ApplyFiltersInCollection(f.(*schema.Set), items, MediaServicesMediaWorkflowJobFactsDataSource().Schema["media_workflow_job_fact_collection"].Elem.(*schema.Resource).Schema)
-		mediaWorkflowJobFact["items"] = items
-	}
-
-	resources = append(resources, mediaWorkflowJobFact)
-	if err := s.D.Set("media_workflow_job_fact_collection", resources); err != nil {
-		return err
-	}
+	//if s.Res == nil {
+	//	return nil
+	//}
+	//
+	//s.D.SetId(tfresource.GenerateDataSourceHashID("MediaServicesMediaWorkflowJobFactsDataSource-", MediaServicesMediaWorkflowJobFactsDataSource(), s.D))
+	//resources := []map[string]interface{}{}
+	//mediaWorkflowJobFact := map[string]interface{}{}
+	//
+	//items := []interface{}{}
+	//for _, item := range s.Res.Items {
+	//	items = append(items, MediaWorkflowJobFactSummaryToMap(item))
+	//}
+	//mediaWorkflowJobFact["items"] = items
+	//
+	//if f, fOk := s.D.GetOkExists("filter"); fOk {
+	//	items = tfresource.ApplyFiltersInCollection(f.(*schema.Set), items, MediaServicesMediaWorkflowJobFactsDataSource().Schema["media_workflow_job_fact_collection"].Elem.(*schema.Resource).Schema)
+	//	mediaWorkflowJobFact["items"] = items
+	//}
+	//
+	//resources = append(resources, mediaWorkflowJobFact)
+	//if err := s.D.Set("media_workflow_job_fact_collection", resources); err != nil {
+	//	return err
+	//}
 
 	return nil
 }
 
-func MediaWorkflowJobFactSummaryToMap(obj oci_media_services.MediaWorkflowJobFactSummary) map[string]interface{} {
-	result := map[string]interface{}{}
-
-	if obj.Key != nil {
-		result["key"] = strconv.FormatInt(*obj.Key, 10)
-	}
-
-	if obj.MediaWorkflowJobId != nil {
-		result["media_workflow_job_id"] = string(*obj.MediaWorkflowJobId)
-	}
-
-	if obj.Name != nil {
-		result["name"] = string(*obj.Name)
-	}
-
-	if obj.Type != nil {
-		result["type"] = string(*obj.Type)
-	}
-
-	return result
-}
+//func MediaWorkflowJobFactSummaryToMap(obj oci_media_services.MediaWorkflowJobFactSummary) map[string]interface{} {
+//	result := map[string]interface{}{}
+//
+//	if obj.Key != nil {
+//		result["key"] = strconv.FormatInt(*obj.Key, 10)
+//	}
+//
+//	if obj.MediaWorkflowJobId != nil {
+//		result["media_workflow_job_id"] = string(*obj.MediaWorkflowJobId)
+//	}
+//
+//	if obj.Name != nil {
+//		result["name"] = string(*obj.Name)
+//	}
+//
+//	if obj.Type != nil {
+//		result["type"] = string(*obj.Type)
+//	}
+//
+//	return result
+//}
