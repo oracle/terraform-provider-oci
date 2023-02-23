27271c1c8be05d9f10b32088b35833cf3e7d1472
Terraform Team Automation <no-reply@oracle.com>
2023-02-23 08:55:50 +0000

    Bug Fix - fix list repositories failure

--- internal/service/devops/devops_repositories_data_source.go
+++ internal/service/devops/devops_repositories_data_source.go
@@ -47,7 +47,166 @@ func DevopsRepositoriesDataSource() *schema.Resource {
 						"items": {
 							Type:     schema.TypeList,
 							Computed: true,
-							Elem:     tfresource.GetDataSourceItemSchema(DevopsRepositoryResource()),
+							Elem: &schema.Resource{
+
+								Schema: map[string]*schema.Schema{
+									// Required
+									"id": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+
+									"name": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"project_id": {
+										Type:     schema.TypeString,
+										Required: true,
+										ForceNew: true,
+									},
+									"repository_type": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+
+									// Optional
+									"default_branch": {
+										Type:     schema.TypeString,
+										Optional: true,
+										Computed: true,
+									},
+									"defined_tags": {
+										Type:             schema.TypeMap,
+										Optional:         true,
+										Computed:         true,
+										DiffSuppressFunc: tfresource.DefinedTagsDiffSuppressFunction,
+										Elem:             schema.TypeString,
+									},
+									"description": {
+										Type:     schema.TypeString,
+										Optional: true,
+										Computed: true,
+									},
+									"freeform_tags": {
+										Type:     schema.TypeMap,
+										Optional: true,
+										Computed: true,
+										Elem:     schema.TypeString,
+									},
+									"mirror_repository_config": {
+										Type:     schema.TypeList,
+										Optional: true,
+										Computed: true,
+										MaxItems: 1,
+										MinItems: 1,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												// Optional
+												"connector_id": {
+													Type:     schema.TypeString,
+													Optional: true,
+													Computed: true,
+												},
+												"repository_url": {
+													Type:     schema.TypeString,
+													Optional: true,
+													Computed: true,
+												},
+												"trigger_schedule": {
+													Type:     schema.TypeList,
+													Optional: true,
+													Computed: true,
+													MaxItems: 1,
+													MinItems: 1,
+													Elem: &schema.Resource{
+														Schema: map[string]*schema.Schema{
+															// Required
+															"schedule_type": {
+																Type:     schema.TypeString,
+																Required: true,
+															},
+
+															// Optional
+															"custom_schedule": {
+																Type:     schema.TypeString,
+																Optional: true,
+																Computed: true,
+															},
+
+															// Computed
+														},
+													},
+												},
+
+												// Computed
+											},
+										},
+									},
+
+									// Computed
+									"branch_count": {
+										Type:     schema.TypeInt,
+										Computed: true,
+									},
+									"commit_count": {
+										Type:     schema.TypeInt,
+										Computed: true,
+									},
+									"compartment_id": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"http_url": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"lifecycle_details": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"namespace": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"project_name": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"size_in_bytes": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"ssh_url": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"state": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"system_tags": {
+										Type:     schema.TypeMap,
+										Computed: true,
+										Elem:     schema.TypeString,
+									},
+									"time_created": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"time_updated": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"trigger_build_events": {
+										Type:     schema.TypeList,
+										Computed: true,
+										Elem: &schema.Schema{
+											Type: schema.TypeString,
+										},
+									},
+								},
+							},
 						},
 					},
 				},
