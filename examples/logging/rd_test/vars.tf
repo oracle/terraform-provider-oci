fb013d97c2d63588dcac0f9f988b4cf7e214fd40
ravbhart <ravi.prakash.bharti@oracle.com>
2023-03-03 17:54:02 +0530

    Exempted - Removed resource discovery output

--- examples/logging/rd_test/vars.tf
+++
@@ -1,3 +0,0 @@
-variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaaxfdo5cmbene2ufapwzjstkh2y4gt7tk4wstx272zntd43fzl6kiq" }
-variable availability_domain--LOil-US-ASHBURN-AD-1 { default = "LOil:US-ASHBURN-AD-1" }
-variable region { default = "us-ashburn-1" }
--- examples/logging/test_db2/vars.tf
+++
@@ -1,3 +0,0 @@
-variable region { default = "us-ashburn-1" }
-variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaasynj7ejhjl3acppihsglkqaaaqii7lpkyer2cnvg2sm2jtv4rtba" }
-variable availability_domain--LOil-US-ASHBURN-AD-3 { default = "LOil:US-ASHBURN-AD-3" }
