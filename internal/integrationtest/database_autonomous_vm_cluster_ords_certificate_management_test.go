8217a44a5d0cd61075b7d1dd39ef1638627f9f7a
Shravan Thatikonda <shravan.thatikonda@oracle.com>
2023-05-19 15:20:22 +0000

    Added - Support for TLS & ORDS BYO Certificates (Phase 2) | ADB-C@C

---
+++ internal/integrationtest/database_autonomous_vm_cluster_ords_certificate_management_test.go
@@ -0,0 +1,102 @@
+// Copyright (c) 2017, 2023, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package integrationtest
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/oracle/terraform-provider-oci/httpreplay"
+	"github.com/oracle/terraform-provider-oci/internal/acctest"
+	"github.com/oracle/terraform-provider-oci/internal/resourcediscovery"
+
+	"github.com/oracle/terraform-provider-oci/internal/utils"
+)
+
+var (
+	certificateId                                                      = utils.GetEnvSettingWithBlankDefault("avm_certificate_id")
+	certificateVariableStr                                             = fmt.Sprintf("variable \"avm_certificate_id\" { default = \"%s\" }\n", certificateId)
+	DatabaseAutonomousVmClusterOrdsCertificateManagementRepresentation = map[string]interface{}{
+		"autonomous_vm_cluster_id":    acctest.Representation{RepType: acctest.Required, Create: `${oci_database_autonomous_vm_cluster.test_autonomous_vm_cluster.id}`},
+		"certificate_generation_type": acctest.Representation{RepType: acctest.Required, Create: `BYOC`},
+		"certificate_id":              acctest.Representation{RepType: acctest.Required, Create: `${var.avm_certificate_id}`},
+	}
+
+	//DatabaseAutonomousVmClusterOrdsSystemCertificateManagementRepresentation = map[string]interface{}{
+	//	"autonomous_vm_cluster_id":    acctest.Representation{RepType: acctest.Required, Create: `${oci_database_autonomous_vm_cluster.test_autonomous_vm_cluster.id}`},
+	//	"certificate_generation_type": acctest.Representation{RepType: acctest.Required, Create: `SYSTEM`},
+	//}
+
+	DatabaseAutonomousVmClusterOrdsManagementResourceDependencies = certificateVariableStr + DatabaseAutonomousVmClusterResourceDependencies +
+		acctest.GenerateResourceFromRepresentationMap("oci_database_autonomous_vm_cluster", "test_autonomous_vm_cluster", acctest.Required, acctest.Create, DatabaseAutonomousVmClusterRepresentation)
+)
+
+// issue-routing-tag: database/ExaCC
+func TestDatabaseAutonomousVmClusterOrdsCertificateManagementResource_basic(t *testing.T) {
+	httpreplay.SetScenario("TestDatabaseAutonomousVmClusterOrdsCertificateManagementResource_basic")
+	defer httpreplay.SaveScenario()
+
+	config := acctest.ProviderTestConfig()
+
+	compartmentId := utils.GetEnvSettingWithBlankDefault("compartment_ocid")
+	compartmentIdVariableStr := fmt.Sprintf("variable \"compartment_id\" { default = \"%s\" }\n", compartmentId)
+
+	resourceName := "oci_database_autonomous_vm_cluster_ords_certificate_management.test_autonomous_vm_cluster_ords_certificate_management"
+	singularDatasourceName := "data.oci_database_autonomous_vm_cluster.test_autonomous_vm_cluster"
+
+	var resId string
+	// Save TF content to Create resource with optional properties. This has to be exactly the same as the config part in the "create with optionals" step in the test.
+	acctest.SaveConfigContent(config+compartmentIdVariableStr+DatabaseAutonomousVmClusterOrdsManagementResourceDependencies+
+		acctest.GenerateResourceFromRepresentationMap("oci_database_autonomous_vm_cluster_ords_certificate_management", "test_autonomous_vm_cluster_ords_certificate_management", acctest.Required, acctest.Create, DatabaseAutonomousVmClusterOrdsCertificateManagementRepresentation), "database", "autonomousVmClusterOrdsCertificateManagement", t)
+
+	acctest.ResourceTest(t, nil, []resource.TestStep{
+		// verify Create
+		{
+			Config: config + compartmentIdVariableStr + DatabaseAutonomousVmClusterOrdsManagementResourceDependencies +
+				acctest.GenerateResourceFromRepresentationMap("oci_database_autonomous_vm_cluster_ords_certificate_management", "test_autonomous_vm_cluster_ords_certificate_management", acctest.Required, acctest.Create, DatabaseAutonomousVmClusterOrdsCertificateManagementRepresentation),
+			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+				resource.TestCheckResourceAttrSet(resourceName, "autonomous_vm_cluster_id"),
+				resource.TestCheckResourceAttr(resourceName, "certificate_generation_type", "BYOC"),
+				resource.TestCheckResourceAttrSet(resourceName, "certificate_id"),
+			),
+		},
+
+		// delete before next Create
+		{
+			Config: config + compartmentIdVariableStr + DatabaseAutonomousVmClusterOrdsManagementResourceDependencies,
+		},
+		// verify Create with optionals
+		{
+			Config: config + compartmentIdVariableStr + DatabaseAutonomousVmClusterOrdsManagementResourceDependencies +
+				acctest.GenerateResourceFromRepresentationMap("oci_database_autonomous_vm_cluster_ords_certificate_management", "test_autonomous_vm_cluster_ords_certificate_management", acctest.Optional, acctest.Create, DatabaseAutonomousVmClusterOrdsCertificateManagementRepresentation),
+			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+				resource.TestCheckResourceAttrSet(resourceName, "autonomous_vm_cluster_id"),
+				resource.TestCheckResourceAttr(resourceName, "certificate_generation_type", "BYOC"),
+				resource.TestCheckResourceAttrSet(resourceName, "certificate_id"),
+
+				func(s *terraform.State) (err error) {
+					resId, err = acctest.FromInstanceState(s, resourceName, "id")
+					if isEnableExportCompartment, _ := strconv.ParseBool(utils.GetEnvSettingWithDefault("enable_export_compartment", "true")); isEnableExportCompartment {
+						if errExport := resourcediscovery.TestExportCompartmentWithResourceName(&resId, &compartmentId, resourceName); errExport != nil {
+							return errExport
+						}
+					}
+					return err
+				},
+			),
+		},
+		{
+			Config: config + compartmentIdVariableStr + DatabaseAutonomousVmClusterOrdsManagementResourceDependencies +
+				acctest.GenerateDataSourceFromRepresentationMap("oci_database_autonomous_vm_cluster", "test_autonomous_vm_cluster", acctest.Required, acctest.Create, DatabaseDatabaseAutonomousVmClusterSingularDataSourceRepresentation),
+			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+				resource.TestCheckResourceAttrSet(singularDatasourceName, "autonomous_vm_cluster_id"),
+				resource.TestCheckResourceAttrSet(singularDatasourceName, "time_ords_certificate_expires"),
+			),
+		},
+	})
+}
---
+++ internal/service/database/database_autonomous_vm_cluster_ssl_certificate_management_resource.go
@@ -0,0 +1,144 @@
+// Copyright (c) 2017, 2023, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package database
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	oci_database "github.com/oracle/oci-go-sdk/v65/database"
+	oci_work_requests "github.com/oracle/oci-go-sdk/v65/workrequests"
+
+	"github.com/oracle/terraform-provider-oci/internal/client"
+	"github.com/oracle/terraform-provider-oci/internal/tfresource"
+)
+
+func DatabaseAutonomousVmClusterSslCertificateManagementResource() *schema.Resource {
+	return &schema.Resource{
+		Timeouts: tfresource.DefaultTimeout,
+		Create:   createDatabaseAutonomousVmClusterSslCertificateManagement,
+		Read:     readDatabaseAutonomousVmClusterSslCertificateManagement,
+		Delete:   deleteDatabaseAutonomousVmClusterSslCertificateManagement,
+		Schema: map[string]*schema.Schema{
+			// Required
+			"autonomous_vm_cluster_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"certificate_generation_type": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			// Optional
+			"ca_bundle_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"certificate_authority_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"certificate_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+
+			// Computed
+		},
+	}
+}
+
+func createDatabaseAutonomousVmClusterSslCertificateManagement(d *schema.ResourceData, m interface{}) error {
+	sync := &DatabaseAutonomousVmClusterSslCertificateManagementResourceCrud{}
+	sync.D = d
+	sync.Client = m.(*client.OracleClients).DatabaseClient()
+	sync.WorkRequestClient = m.(*client.OracleClients).WorkRequestClient
+
+	return tfresource.CreateResource(d, sync)
+}
+
+func readDatabaseAutonomousVmClusterSslCertificateManagement(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func deleteDatabaseAutonomousVmClusterSslCertificateManagement(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+type DatabaseAutonomousVmClusterSslCertificateManagementResourceCrud struct {
+	tfresource.BaseCrud
+	Client                 *oci_database.DatabaseClient
+	Res                    *oci_database.RotateAutonomousVmClusterSslCertsResponse
+	DisableNotFoundRetries bool
+	WorkRequestClient      *oci_work_requests.WorkRequestClient
+}
+
+func (s *DatabaseAutonomousVmClusterSslCertificateManagementResourceCrud) ID() string {
+	return tfresource.GenerateDataSourceHashID("DatabaseAutonomousVmClusterSslCertificateManagementResource-", DatabaseAutonomousVmClusterSslCertificateManagementResource(), s.D)
+}
+
+func (s *DatabaseAutonomousVmClusterSslCertificateManagementResourceCrud) Create() error {
+	request := oci_database.RotateAutonomousVmClusterSslCertsRequest{}
+
+	if autonomousVmClusterId, ok := s.D.GetOkExists("autonomous_vm_cluster_id"); ok {
+		tmp := autonomousVmClusterId.(string)
+		request.AutonomousVmClusterId = &tmp
+	}
+
+	if caBundleId, ok := s.D.GetOkExists("ca_bundle_id"); ok {
+		tmp := caBundleId.(string)
+		request.CaBundleId = &tmp
+	}
+
+	if certificateAuthorityId, ok := s.D.GetOkExists("certificate_authority_id"); ok {
+		tmp := certificateAuthorityId.(string)
+		request.CertificateAuthorityId = &tmp
+	}
+
+	if certificateGenerationType, ok := s.D.GetOkExists("certificate_generation_type"); ok {
+		request.CertificateGenerationType = oci_database.RotateAutonomousVmClusterSslCertsDetailsCertificateGenerationTypeEnum(certificateGenerationType.(string))
+	}
+
+	if certificateId, ok := s.D.GetOkExists("certificate_id"); ok {
+		tmp := certificateId.(string)
+		request.CertificateId = &tmp
+	}
+
+	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(s.DisableNotFoundRetries, "database")
+
+	response, err := s.Client.RotateAutonomousVmClusterSslCerts(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	workId := response.OpcWorkRequestId
+	s.Res = &response
+
+	if workId != nil {
+		var identifier *string
+		var err error
+		identifier, err = tfresource.WaitForWorkRequestWithErrorHandling(s.WorkRequestClient, workId, "autonomousvmcluster", oci_work_requests.WorkRequestResourceActionTypeUpdated, s.D.Timeout(schema.TimeoutUpdate), s.DisableNotFoundRetries)
+		if identifier != nil {
+			s.D.SetId(*identifier)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *DatabaseAutonomousVmClusterSslCertificateManagementResourceCrud) SetData() error {
+	return nil
+}
