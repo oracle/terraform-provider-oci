584c1695790e26b2e1a6365f5aaac72920d9e20b
Terraform Team Automation <no-reply@oracle.com>
2023-07-28 00:40:26 +0000

    Added - Support for OPSI News Reports

---
+++ internal/service/opsi/opsi_news_reports_data_source.go
@@ -0,0 +1,176 @@
+// Copyright (c) 2017, 2023, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package opsi
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	oci_opsi "github.com/oracle/oci-go-sdk/v65/operationsinsights"
+
+	"github.com/oracle/terraform-provider-oci/internal/client"
+	"github.com/oracle/terraform-provider-oci/internal/tfresource"
+)
+
+func OpsiNewsReportsDataSource() *schema.Resource {
+	return &schema.Resource{
+		Read: readOpsiNewsReports,
+		Schema: map[string]*schema.Schema{
+			"filter": tfresource.DataSourceFiltersSchema(),
+			"compartment_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"compartment_id_in_subtree": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"news_report_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"state": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"status": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"news_report_collection": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+
+						"items": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem:     tfresource.GetDataSourceItemSchema(OpsiNewsReportResource()),
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func readOpsiNewsReports(d *schema.ResourceData, m interface{}) error {
+	sync := &OpsiNewsReportsDataSourceCrud{}
+	sync.D = d
+	sync.Client = m.(*client.OracleClients).OperationsInsightsClient()
+
+	return tfresource.ReadResource(sync)
+}
+
+type OpsiNewsReportsDataSourceCrud struct {
+	D      *schema.ResourceData
+	Client *oci_opsi.OperationsInsightsClient
+	Res    *oci_opsi.ListNewsReportsResponse
+}
+
+func (s *OpsiNewsReportsDataSourceCrud) VoidState() {
+	s.D.SetId("")
+}
+
+func (s *OpsiNewsReportsDataSourceCrud) Get() error {
+	request := oci_opsi.ListNewsReportsRequest{}
+
+	if compartmentId, ok := s.D.GetOkExists("compartment_id"); ok {
+		tmp := compartmentId.(string)
+		request.CompartmentId = &tmp
+	}
+
+	if compartmentIdInSubtree, ok := s.D.GetOkExists("compartment_id_in_subtree"); ok {
+		tmp := compartmentIdInSubtree.(bool)
+		request.CompartmentIdInSubtree = &tmp
+	}
+
+	if newsReportId, ok := s.D.GetOkExists("id"); ok {
+		tmp := newsReportId.(string)
+		request.NewsReportId = &tmp
+	}
+
+	if state, ok := s.D.GetOkExists("state"); ok {
+		interfaces := state.([]interface{})
+		tmp := make([]oci_opsi.LifecycleStateEnum, len(interfaces))
+		for i := range interfaces {
+			if interfaces[i] != nil {
+				tmp[i] = oci_opsi.LifecycleStateEnum(interfaces[i].(string))
+			}
+		}
+		if len(tmp) != 0 || s.D.HasChange("state") {
+			request.LifecycleState = tmp
+		}
+	}
+
+	if status, ok := s.D.GetOkExists("status"); ok {
+		interfaces := status.([]interface{})
+		tmp := make([]oci_opsi.ResourceStatusEnum, len(interfaces))
+		for i := range interfaces {
+			if interfaces[i] != nil {
+				tmp[i] = oci_opsi.ResourceStatusEnum(interfaces[i].(string))
+			}
+		}
+		if len(tmp) != 0 || s.D.HasChange("status") {
+			request.Status = tmp
+		}
+	}
+
+	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(false, "opsi")
+
+	response, err := s.Client.ListNewsReports(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	s.Res = &response
+	request.Page = s.Res.OpcNextPage
+
+	for request.Page != nil {
+		listResponse, err := s.Client.ListNewsReports(context.Background(), request)
+		if err != nil {
+			return err
+		}
+
+		s.Res.Items = append(s.Res.Items, listResponse.Items...)
+		request.Page = listResponse.OpcNextPage
+	}
+
+	return nil
+}
+
+func (s *OpsiNewsReportsDataSourceCrud) SetData() error {
+	if s.Res == nil {
+		return nil
+	}
+
+	s.D.SetId(tfresource.GenerateDataSourceHashID("OpsiNewsReportsDataSource-", OpsiNewsReportsDataSource(), s.D))
+	resources := []map[string]interface{}{}
+	newsReport := map[string]interface{}{}
+
+	items := []interface{}{}
+	for _, item := range s.Res.Items {
+		items = append(items, NewsReportSummaryToMap(item))
+	}
+	newsReport["items"] = items
+
+	if f, fOk := s.D.GetOkExists("filter"); fOk {
+		items = tfresource.ApplyFiltersInCollection(f.(*schema.Set), items, OpsiNewsReportsDataSource().Schema["news_report_collection"].Elem.(*schema.Resource).Schema)
+		newsReport["items"] = items
+	}
+
+	resources = append(resources, newsReport)
+	if err := s.D.Set("news_report_collection", resources); err != nil {
+		return err
+	}
+
+	return nil
+}
