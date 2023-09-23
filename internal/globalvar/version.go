9d95ce622a1795ef32e6a9ac4c9c758ce3062861
Rakshith Siddanahalli Murukannappa <rakshith.siddanahalli.murukan@oracle.com>
2023-09-22 17:13:20 -0700

    Finalize changelog and release for version v5.14.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "5.13.0"
-const ReleaseDate = "2023-09-13"
+const Version = "5.14.0"
+const ReleaseDate = "2023-09-27"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
