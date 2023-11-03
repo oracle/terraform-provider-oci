3ef07ef1872a1f158f59c29e628a334ed954596e
ravbhart <ravi.prakash.bharti@oracle.com>
2023-11-03 13:53:27 +0530

    Finalize changelog and release for version v5.20.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "5.19.0"
-const ReleaseDate = "2023-11-06"
+const Version = "5.20.0"
+const ReleaseDate = "2023-11-08"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
