69f4f503a2a10ef15bc5dba908a04d66c0ffce76
ravbhart <ravi.prakash.bharti@oracle.com>
2023-11-03 13:53:27 +0530

    Finalize changelog and release for version v5.20.0

--- examples/apm/apm_domains/main.tf
+++ examples/apm/apm_domains/main.tf
@@ -4,7 +4,7 @@
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
+#variable "private_key_path" {}
 variable "region" {}
 variable "compartment_ocid" {}
 
@@ -37,11 +37,11 @@ variable "data_key_data_key_type" {
 }
 
 provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
+#  tenancy_ocid     = var.tenancy_ocid
+#  user_ocid        = var.user_ocid
+#  fingerprint      = var.fingerprint
+#  private_key_path = var.private_key_path
+#  region           = var.region
 }
 
 resource "oci_apm_apm_domain" "test_apm_domain" {
