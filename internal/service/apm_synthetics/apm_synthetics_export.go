bc7f5d466be74c63d098cc8bfbdb848bb4b74fbe
Terraform Team Automation <no-reply@oracle.com>
2024-01-04 16:15:36 +0000

    Added - Support for APM - Synthetics - Support for On Premise Vantage Point

--- internal/service/apm_synthetics/apm_synthetics_export.go
+++ internal/service/apm_synthetics/apm_synthetics_export.go
@@ -9,6 +9,9 @@ import (
 func init() {
 	exportApmSyntheticsScriptHints.GetIdFn = getApmSyntheticsScriptId
 	exportApmSyntheticsMonitorHints.GetIdFn = getApmSyntheticsMonitorId
+	//exportApmSyntheticsDedicatedVantagePointHints.GetIdFn = getApmSyntheticsDedicatedVantagePointId
+	exportApmSyntheticsOnPremiseVantagePointHints.GetIdFn = getApmSyntheticsOnPremiseVantagePointId
+	exportApmSyntheticsOnPremiseVantagePointWorkerHints.GetIdFn = getApmSyntheticsOnPremiseVantagePointWorkerId
 	tf_export.RegisterCompartmentGraphs("apm_synthetics", apmSyntheticsResourceGraph)
 }
 
@@ -16,28 +19,48 @@ func init() {
 
 func getApmSyntheticsScriptId(resource *tf_export.OCIResource) (string, error) {
 
+	apmDomainId := resource.Parent.Id
 	scriptId, ok := resource.SourceAttributes["id"].(string)
 	if !ok {
 		return "", fmt.Errorf("[ERROR] unable to find scriptId for ApmSynthetics Script")
 	}
-	apmDomainId, ok := resource.SourceAttributes["apm_domain_id"].(string)
-	if !ok {
-		return "", fmt.Errorf("[ERROR] unable to find apmDomainId for ApmSynthetics Script")
-	}
+
 	return GetScriptCompositeId(scriptId, apmDomainId), nil
 }
 
 func getApmSyntheticsMonitorId(resource *tf_export.OCIResource) (string, error) {
 
+	apmDomainId := resource.Parent.Id
 	monitorId, ok := resource.SourceAttributes["id"].(string)
 	if !ok {
 		return "", fmt.Errorf("[ERROR] unable to find monitorId for ApmSynthetics Monitor")
 	}
-	apmDomainId, ok := resource.SourceAttributes["apm_domain_id"].(string)
-	if !ok {
-		return "", fmt.Errorf("[ERROR] unable to find apmDomainId for ApmSynthetics Monitor")
-	}
+
 	return GetMonitorCompositeId(monitorId, apmDomainId), nil
+}
+
+func getApmSyntheticsOnPremiseVantagePointWorkerId(resource *tf_export.OCIResource) (string, error) {
+
+	onPremiseVantagePointId := resource.Parent.Id
+	workerId, ok := resource.SourceAttributes["id"].(string)
+	if !ok {
+		return "", fmt.Errorf("[ERROR] unable to find id for ApmSynthetics OnPremiseVantagePointWorker")
+	}
+
+	apmDomainId := resource.Parent.Parent.Id
+
+	return GetOnPremiseVantagePointWorkerCompositeId(onPremiseVantagePointId, workerId, apmDomainId), nil
+}
+
+func getApmSyntheticsOnPremiseVantagePointId(resource *tf_export.OCIResource) (string, error) {
+
+	onPremiseVantagePointId, ok := resource.SourceAttributes["id"].(string)
+	if !ok {
+		return "", fmt.Errorf("[ERROR] unable to find id for ApmSynthetics OnPremiseVantagePoint")
+	}
+	apmDomainId := resource.Parent.Id
+
+	return GetOnPremiseVantagePointCompositeId(onPremiseVantagePointId, apmDomainId), nil
 }
 
 // Hints for discovering and exporting this resource to configuration and state files
@@ -45,6 +68,7 @@ var exportApmSyntheticsScriptHints = &tf_export.TerraformResourceHints{
 	ResourceClass:          "oci_apm_synthetics_script",
 	DatasourceClass:        "oci_apm_synthetics_scripts",
 	DatasourceItemsAttr:    "script_collection",
+	IsDatasourceCollection: true,
 	ResourceAbbreviation:   "script",
 	RequireResourceRefresh: true,
 }
@@ -53,6 +77,7 @@ var exportApmSyntheticsMonitorHints = &tf_export.TerraformResourceHints{
 	ResourceClass:          "oci_apm_synthetics_monitor",
 	DatasourceClass:        "oci_apm_synthetics_monitors",
 	DatasourceItemsAttr:    "monitor_collection",
+	IsDatasourceCollection: true,
 	ResourceAbbreviation:   "monitor",
 	RequireResourceRefresh: true,
 }
@@ -63,6 +88,24 @@ var exportApmSyntheticsDedicatedVantagePointHints = &tf_export.TerraformResource
 	DatasourceItemsAttr:    "dedicated_vantage_point_collection",
 	IsDatasourceCollection: true,
 	ResourceAbbreviation:   "dedicated_vantage_point",
+	RequireResourceRefresh: true,
+}
+
+var exportApmSyntheticsOnPremiseVantagePointWorkerHints = &tf_export.TerraformResourceHints{
+	ResourceClass:          "oci_apm_synthetics_on_premise_vantage_point_worker",
+	DatasourceClass:        "oci_apm_synthetics_on_premise_vantage_point_workers",
+	DatasourceItemsAttr:    "worker_collection",
+	IsDatasourceCollection: true,
+	ResourceAbbreviation:   "on_premise_vantage_point_worker",
+	RequireResourceRefresh: true,
+}
+
+var exportApmSyntheticsOnPremiseVantagePointHints = &tf_export.TerraformResourceHints{
+	ResourceClass:          "oci_apm_synthetics_on_premise_vantage_point",
+	DatasourceClass:        "oci_apm_synthetics_on_premise_vantage_points",
+	DatasourceItemsAttr:    "on_premise_vantage_point_collection",
+	IsDatasourceCollection: true,
+	ResourceAbbreviation:   "on_premise_vantage_point",
 	RequireResourceRefresh: true,
 }
 
--- website/docs/d/apm_synthetics_monitor.html.markdown
+++ website/docs/d/apm_synthetics_monitor.html.markdown
@@ -53,13 +53,17 @@ The following attributes are exported:
 	* `is_certificate_validation_enabled` - If certificate validation is enabled, then the call will fail in case of certification errors.
 	* `is_default_snapshot_enabled` - If disabled, auto snapshots are not collected.
 	* `is_failure_retried` - If isFailureRetried is enabled, then a failed call will be retried.
+	* `is_query_recursive` - If isQueryRecursive is enabled, then queries will be sent recursively to the target server.
 	* `is_redirection_enabled` - If redirection is enabled, then redirects will be allowed while accessing target URL.
-	* `network_configuration` - Details of the network configuration.
+	* `name_server` - Name of the server that will be used to perform DNS lookup.
+	* `network_configuration` - Details of the network configuration. For NETWORK monitor type, NetworkConfiguration is mandatory.
 		* `number_of_hops` - Number of hops.
 		* `probe_mode` - Type of probe mode when TCP protocol is selected.
 		* `probe_per_hop` - Number of probes per hop.
 		* `protocol` - Type of protocol.
 		* `transmission_rate` - Number of probe packets sent out simultaneously.
+	* `protocol` - Type of protocol.
+	* `record_type` - DNS record type.
 	* `req_authentication_details` - Details for request HTTP authentication.
 		* `auth_headers` - List of authentication headers. Example: `[{"headerName": "content-type", "headerValue":"json"}]` 
 			* `header_name` - Name of the header.
@@ -105,7 +109,7 @@ The following attributes are exported:
 		* `param_name` - Name of the parameter.
 		* `param_value` - Value of the parameter.
 * `status` - Enables or disables the monitor.
-* `target` - Specify the endpoint on which to run the monitor. For BROWSER and REST monitor types, target is mandatory. If target is specified in the SCRIPTED_BROWSER monitor type, then the monitor will run the selected script (specified by scriptId in monitor) against the specified target endpoint. If target is not specified in the SCRIPTED_BROWSER monitor type, then the monitor will run the selected script as it is. For NETWORK monitor with TCP protocol, a port needs to be provided along with target. Example: 192.168.0.1:80 
+* `target` - Specify the endpoint on which to run the monitor. For BROWSER, REST and NETWORK monitor types, target is mandatory. If target is specified in the SCRIPTED_BROWSER monitor type, then the monitor will run the selected script (specified by scriptId in monitor) against the specified target endpoint. If target is not specified in the SCRIPTED_BROWSER monitor type, then the monitor will run the selected script as it is. For NETWORK monitor with TCP protocol, a port needs to be provided along with target. Example: 192.168.0.1:80 
 * `time_created` - The time the resource was created, expressed in [RFC 3339](https://tools.ietf.org/html/rfc3339) timestamp format. Example: `2020-02-12T22:47:12.613Z` 
 * `time_updated` - The time the resource was updated, expressed in [RFC 3339](https://tools.ietf.org/html/rfc3339) timestamp format. Example: `2020-02-13T22:47:12.613Z` 
 * `timeout_in_seconds` - Timeout in seconds. If isFailureRetried is true, then timeout cannot be more than 30% of repeatIntervalInSeconds time for monitors. If isFailureRetried is false, then timeout cannot be more than 50% of repeatIntervalInSeconds time for monitors. Also, timeoutInSeconds should be a multiple of 60 for Scripted REST, Scripted Browser and Browser monitors. Monitor will be allowed to run only for timeoutInSeconds time. It would be terminated after that. 
