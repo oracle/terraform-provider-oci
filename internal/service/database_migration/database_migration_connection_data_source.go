c7884681eedf8d662436f6f4a5a58eb740804884
Gaurav Taneja <gaurav.taneja@oracle.com>
2024-05-15 18:58:09 -0700

    Added - Support for MySQL <> MySQL Homogenous Migration support

--- internal/service/database_migration/database_migration_connection_data_source.go
+++ internal/service/database_migration/database_migration_connection_data_source.go
@@ -1,16 +1,16 @@
-// Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
-// Licensed under the Mozilla Public License v2.0
-
+// // Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
+// // Licensed under the Mozilla Public License v2.0
 package database_migration
 
 import (
 	"context"
-
-	"github.com/oracle/terraform-provider-oci/internal/client"
-	"github.com/oracle/terraform-provider-oci/internal/tfresource"
+	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	oci_database_migration "github.com/oracle/oci-go-sdk/v65/databasemigration"
+
+	"github.com/oracle/terraform-provider-oci/internal/client"
+	"github.com/oracle/terraform-provider-oci/internal/tfresource"
 )
 
 func DatabaseMigrationConnectionDataSource() *schema.Resource {
@@ -64,90 +64,241 @@ func (s *DatabaseMigrationConnectionDataSourceCrud) SetData() error {
 		return nil
 	}
 
-	s.D.SetId(*s.Res.Id)
+	s.D.SetId(*s.Res.GetId())
+	switch v := (s.Res.Connection).(type) {
+	case oci_database_migration.MysqlConnection:
+		s.D.Set("connection_type", "MYSQL")
 
-	if s.Res.AdminCredentials != nil {
-		s.D.Set("admin_credentials", []interface{}{AdminCredentialsToMap(s.Res.AdminCredentials)})
-	} else {
-		s.D.Set("admin_credentials", nil)
-	}
+		additionalAttributes := []interface{}{}
+		for _, item := range v.AdditionalAttributes {
+			additionalAttributes = append(additionalAttributes, NameValuePairToMap(item))
+		}
+		s.D.Set("additional_attributes", additionalAttributes)
 
-	if s.Res.CertificateTdn != nil {
-		s.D.Set("certificate_tdn", *s.Res.CertificateTdn)
-	}
+		if v.DatabaseName != nil {
+			s.D.Set("database_name", *v.DatabaseName)
+		}
 
-	if s.Res.CompartmentId != nil {
-		s.D.Set("compartment_id", *s.Res.CompartmentId)
-	}
+		if v.DbSystemId != nil {
+			s.D.Set("db_system_id", *v.DbSystemId)
+		}
 
-	if s.Res.ConnectDescriptor != nil {
-		s.D.Set("connect_descriptor", []interface{}{ConnectDescriptorToMap(s.Res.ConnectDescriptor)})
-	} else {
-		s.D.Set("connect_descriptor", nil)
-	}
+		if v.Host != nil {
+			s.D.Set("host", *v.Host)
+		}
 
-	if s.Res.CredentialsSecretId != nil {
-		s.D.Set("credentials_secret_id", *s.Res.CredentialsSecretId)
-	}
+		if v.Port != nil {
+			s.D.Set("port", *v.Port)
+		}
 
-	if s.Res.DatabaseId != nil {
-		s.D.Set("database_id", *s.Res.DatabaseId)
-	}
+		s.D.Set("security_protocol", v.SecurityProtocol)
 
-	s.D.Set("database_type", s.Res.DatabaseType)
+		s.D.Set("ssl_mode", v.SslMode)
 
-	if s.Res.DefinedTags != nil {
-		s.D.Set("defined_tags", tfresource.DefinedTagsToMap(s.Res.DefinedTags))
-	}
+		s.D.Set("technology_type", v.TechnologyType)
 
-	if s.Res.DisplayName != nil {
-		s.D.Set("display_name", *s.Res.DisplayName)
-	}
+		if v.CompartmentId != nil {
+			s.D.Set("compartment_id", *v.CompartmentId)
+		}
 
-	s.D.Set("freeform_tags", s.Res.FreeformTags)
+		if v.DefinedTags != nil {
+			s.D.Set("defined_tags", tfresource.DefinedTagsToMap(v.DefinedTags))
+		}
 
-	if s.Res.LifecycleDetails != nil {
-		s.D.Set("lifecycle_details", *s.Res.LifecycleDetails)
-	}
+		if v.Description != nil {
+			s.D.Set("description", *v.Description)
+		}
 
-	s.D.Set("manual_database_sub_type", s.Res.ManualDatabaseSubType)
+		if v.DisplayName != nil {
+			s.D.Set("display_name", *v.DisplayName)
+		}
 
-	if s.Res.PrivateEndpoint != nil {
-		s.D.Set("private_endpoint", []interface{}{PrivateEndpointDetailsToMap(s.Res.PrivateEndpoint)})
-	} else {
-		s.D.Set("private_endpoint", nil)
-	}
+		s.D.Set("freeform_tags", v.FreeformTags)
 
-	if s.Res.ReplicationCredentials != nil {
-		s.D.Set("replication_credentials", []interface{}{AdminCredentialsToMapPassword(s.Res.ReplicationCredentials, s.D)})
-	} else {
-		s.D.Set("replication_credentials", nil)
-	}
+		ingressIps := []interface{}{}
+		for _, item := range v.IngressIps {
+			ingressIps = append(ingressIps, IngressIpDetailsToMap(item))
+		}
+		s.D.Set("ingress_ips", ingressIps)
 
-	if s.Res.SshDetails != nil {
-		s.D.Set("ssh_details", []interface{}{SshDetailsToMap(s.Res.SshDetails)})
-	} else {
-		s.D.Set("ssh_details", nil)
-	}
+		if v.KeyId != nil {
+			s.D.Set("key_id", *v.KeyId)
+		}
 
-	s.D.Set("state", s.Res.LifecycleState)
+		if v.LifecycleDetails != nil {
+			s.D.Set("lifecycle_details", *v.LifecycleDetails)
+		}
 
-	if s.Res.SystemTags != nil {
-		s.D.Set("system_tags", tfresource.SystemTagsToMap(s.Res.SystemTags))
-	}
+		nsgIds := []interface{}{}
+		for _, item := range v.NsgIds {
+			nsgIds = append(nsgIds, item)
+		}
+		s.D.Set("nsg_ids", v.NsgIds)
 
-	if s.Res.TimeCreated != nil {
-		s.D.Set("time_created", s.Res.TimeCreated.String())
-	}
+		if v.Password != nil {
+			s.D.Set("password", *v.Password)
+		}
 
-	if s.Res.TimeUpdated != nil {
-		s.D.Set("time_updated", s.Res.TimeUpdated.String())
-	}
+		if v.PrivateEndpointId != nil {
+			s.D.Set("private_endpoint_id", *v.PrivateEndpointId)
+		}
 
-	if s.Res.VaultDetails != nil {
-		s.D.Set("vault_details", []interface{}{VaultDetailsToMap(s.Res.VaultDetails)})
-	} else {
-		s.D.Set("vault_details", nil)
+		if v.ReplicationPassword != nil {
+			s.D.Set("replication_password", *v.ReplicationPassword)
+		}
+
+		if v.ReplicationUsername != nil {
+			s.D.Set("replication_username", *v.ReplicationUsername)
+		}
+
+		if v.SecretId != nil {
+			s.D.Set("secret_id", *v.SecretId)
+		}
+
+		s.D.Set("state", v.LifecycleState)
+
+		if v.SubnetId != nil {
+			s.D.Set("subnet_id", *v.SubnetId)
+		}
+
+		if v.SystemTags != nil {
+			s.D.Set("system_tags", tfresource.SystemTagsToMap(v.SystemTags))
+		}
+
+		s.D.Set("technology_type", v.TechnologyType)
+
+		if v.TimeCreated != nil {
+			s.D.Set("time_created", v.TimeCreated.String())
+		}
+
+		if v.TimeUpdated != nil {
+			s.D.Set("time_updated", v.TimeUpdated.String())
+		}
+
+		if v.Username != nil {
+			s.D.Set("username", *v.Username)
+		}
+
+		if v.VaultId != nil {
+			s.D.Set("vault_id", *v.VaultId)
+		}
+	case oci_database_migration.OracleConnection:
+		s.D.Set("connection_type", "ORACLE")
+
+		if v.ConnectionString != nil {
+			s.D.Set("connection_string", *v.ConnectionString)
+		}
+
+		if v.DatabaseId != nil {
+			s.D.Set("database_id", *v.DatabaseId)
+		}
+
+		if v.SshHost != nil {
+			s.D.Set("ssh_host", *v.SshHost)
+		}
+
+		if v.SshKey != nil {
+			s.D.Set("ssh_key", *v.SshKey)
+		}
+
+		if v.SshSudoLocation != nil {
+			s.D.Set("ssh_sudo_location", *v.SshSudoLocation)
+		}
+
+		if v.SshUser != nil {
+			s.D.Set("ssh_user", *v.SshUser)
+		}
+
+		s.D.Set("technology_type", v.TechnologyType)
+
+		if v.CompartmentId != nil {
+			s.D.Set("compartment_id", *v.CompartmentId)
+		}
+
+		if v.DefinedTags != nil {
+			s.D.Set("defined_tags", tfresource.DefinedTagsToMap(v.DefinedTags))
+		}
+
+		if v.Description != nil {
+			s.D.Set("description", *v.Description)
+		}
+
+		if v.DisplayName != nil {
+			s.D.Set("display_name", *v.DisplayName)
+		}
+
+		s.D.Set("freeform_tags", v.FreeformTags)
+
+		ingressIps := []interface{}{}
+		for _, item := range v.IngressIps {
+			ingressIps = append(ingressIps, IngressIpDetailsToMap(item))
+		}
+		s.D.Set("ingress_ips", ingressIps)
+
+		if v.KeyId != nil {
+			s.D.Set("key_id", *v.KeyId)
+		}
+
+		if v.LifecycleDetails != nil {
+			s.D.Set("lifecycle_details", *v.LifecycleDetails)
+		}
+
+		nsgIds := []interface{}{}
+		for _, item := range v.NsgIds {
+			nsgIds = append(nsgIds, item)
+		}
+		s.D.Set("nsg_ids", v.NsgIds)
+
+		if v.Password != nil {
+			s.D.Set("password", *v.Password)
+		}
+
+		if v.PrivateEndpointId != nil {
+			s.D.Set("private_endpoint_id", *v.PrivateEndpointId)
+		}
+
+		if v.ReplicationPassword != nil {
+			s.D.Set("replication_password", *v.ReplicationPassword)
+		}
+
+		if v.ReplicationUsername != nil {
+			s.D.Set("replication_username", *v.ReplicationUsername)
+		}
+
+		if v.SecretId != nil {
+			s.D.Set("secret_id", *v.SecretId)
+		}
+
+		s.D.Set("state", v.LifecycleState)
+
+		if v.SubnetId != nil {
+			s.D.Set("subnet_id", *v.SubnetId)
+		}
+
+		if v.SystemTags != nil {
+			s.D.Set("system_tags", tfresource.SystemTagsToMap(v.SystemTags))
+		}
+
+		s.D.Set("technology_type", v.TechnologyType)
+
+		if v.TimeCreated != nil {
+			s.D.Set("time_created", v.TimeCreated.String())
+		}
+
+		if v.TimeUpdated != nil {
+			s.D.Set("time_updated", v.TimeUpdated.String())
+		}
+
+		if v.Username != nil {
+			s.D.Set("username", *v.Username)
+		}
+
+		if v.VaultId != nil {
+			s.D.Set("vault_id", *v.VaultId)
+		}
+	default:
+		log.Printf("[WARN] Received 'connection_type' of unknown type %v", s.Res.Connection)
+		return nil
 	}
 
 	return nil
--- internal/service/database_migration/database_migration_connections_data_source.go
+++ internal/service/database_migration/database_migration_connections_data_source.go
@@ -1,16 +1,15 @@
-// Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
-// Licensed under the Mozilla Public License v2.0
-
+// // Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
+// // Licensed under the Mozilla Public License v2.0
 package database_migration
 
 import (
 	"context"
 
-	"github.com/oracle/terraform-provider-oci/internal/client"
-	"github.com/oracle/terraform-provider-oci/internal/tfresource"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	oci_database_migration "github.com/oracle/oci-go-sdk/v65/databasemigration"
+
+	"github.com/oracle/terraform-provider-oci/internal/client"
+	"github.com/oracle/terraform-provider-oci/internal/tfresource"
 )
 
 func DatabaseMigrationConnectionsDataSource() *schema.Resource {
@@ -22,13 +21,31 @@ func DatabaseMigrationConnectionsDataSource() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+			"connection_type": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
 			"display_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"source_connection_id": {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
 			"state": {
 				Type:     schema.TypeString,
 				Optional: true,
+			},
+			"technology_type": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
 			},
 			"connection_collection": {
 				Type:     schema.TypeList,
@@ -74,13 +91,44 @@ func (s *DatabaseMigrationConnectionsDataSourceCrud) Get() error {
 		request.CompartmentId = &tmp
 	}
 
+	if connectionType, ok := s.D.GetOkExists("connection_type"); ok {
+		interfaces := connectionType.([]interface{})
+		tmp := make([]oci_database_migration.ConnectionTypeEnum, len(interfaces))
+		for i := range interfaces {
+			if interfaces[i] != nil {
+				tmp[i] = oci_database_migration.ConnectionTypeEnum(interfaces[i].(string))
+			}
+		}
+		if len(tmp) != 0 || s.D.HasChange("connection_type") {
+			request.ConnectionType = tmp
+		}
+	}
+
 	if displayName, ok := s.D.GetOkExists("display_name"); ok {
 		tmp := displayName.(string)
 		request.DisplayName = &tmp
 	}
 
+	if sourceConnectionId, ok := s.D.GetOkExists("source_connection_id"); ok {
+		tmp := sourceConnectionId.(string)
+		request.SourceConnectionId = &tmp
+	}
+
 	if state, ok := s.D.GetOkExists("state"); ok {
 		request.LifecycleState = oci_database_migration.ListConnectionsLifecycleStateEnum(state.(string))
+	}
+
+	if technologyType, ok := s.D.GetOkExists("technology_type"); ok {
+		interfaces := technologyType.([]interface{})
+		tmp := make([]oci_database_migration.TechnologyTypeEnum, len(interfaces))
+		for i := range interfaces {
+			if interfaces[i] != nil {
+				tmp[i] = oci_database_migration.TechnologyTypeEnum(interfaces[i].(string))
+			}
+		}
+		if len(tmp) != 0 || s.D.HasChange("technology_type") {
+			request.TechnologyType = tmp
+		}
 	}
 
 	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(false, "database_migration")
@@ -117,7 +165,7 @@ func (s *DatabaseMigrationConnectionsDataSourceCrud) SetData() error {
 
 	items := []interface{}{}
 	for _, item := range s.Res.Items {
-		items = append(items, ConnectionSummaryToMap(item))
+		items = append(items, ConnectionSummaryToMap(item, true))
 	}
 	connection["items"] = items
 
