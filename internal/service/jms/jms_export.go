78d29c735bd3f5d014862d5cc53baa6a186d6efc
Terraform Team Automation <no-reply@oracle.com>
2024-06-20 07:39:24 +0000

    Added - Support for JMS Implement management resource for advancedFeatureConfiguration

--- internal/service/jms/jms_export.go
+++ internal/service/jms/jms_export.go
@@ -7,10 +7,17 @@ import (
 )
 
 func init() {
+	exportJmsFleetAdvancedFeatureConfigurationHints.GetIdFn = getJmsFleetAdvancedFeatureConfigurationId
 	tf_export.RegisterCompartmentGraphs("jms", jmsResourceGraph)
 }
 
 // Custom overrides for generating composite IDs within the resource discovery framework
+
+func getJmsFleetAdvancedFeatureConfigurationId(resource *tf_export.OCIResource) (string, error) {
+
+	fleetId := resource.Parent.Id
+	return GetFleetAdvancedFeatureConfigurationCompositeId(fleetId), nil
+}
 
 // Hints for discovering and exporting this resource to configuration and state files
 var exportJmsFleetHints = &tf_export.TerraformResourceHints{
@@ -26,8 +33,22 @@ var exportJmsFleetHints = &tf_export.TerraformResourceHints{
 	},
 }
 
+var exportJmsFleetAdvancedFeatureConfigurationHints = &tf_export.TerraformResourceHints{
+	ResourceClass:        "oci_jms_fleet_advanced_feature_configuration",
+	DatasourceClass:      "oci_jms_fleet_advanced_feature_configuration",
+	ResourceAbbreviation: "fleet_advanced_feature_configuration",
+}
+
 var jmsResourceGraph = tf_export.TerraformResourceGraph{
 	"oci_identity_compartment": {
 		{TerraformResourceHints: exportJmsFleetHints},
+	},
+	"oci_jms_fleet": {
+		{
+			TerraformResourceHints: exportJmsFleetAdvancedFeatureConfigurationHints,
+			DatasourceQueryParams: map[string]string{
+				"fleet_id": "id",
+			},
+		},
 	},
 }
