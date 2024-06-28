56d6edf568b09ade5028c1ffed4adcff00ce6b05
Sagar Pokale <sagar.pokale@oracle.com>
2024-06-28 15:48:45 -0500

    Added - Support for MySQL Homogenous Migration support

--- internal/service/database_migration/database_migration_connections_data_source.go
+++ internal/service/database_migration/database_migration_connections_data_source.go
@@ -2,134 +2,182 @@
 // // Licensed under the Mozilla Public License v2.0
 package database_migration
 
-//
-//import (
-//	"context"
-//
-//	"github.com/oracle/terraform-provider-oci/internal/client"
-//	"github.com/oracle/terraform-provider-oci/internal/tfresource"
-//
-//	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-//	oci_database_migration "github.com/oracle/oci-go-sdk/v65/databasemigration"
-//)
-//
-//func DatabaseMigrationConnectionsDataSource() *schema.Resource {
-//	return &schema.Resource{
-//		Read: readDatabaseMigrationConnections,
-//		Schema: map[string]*schema.Schema{
-//			"filter": tfresource.DataSourceFiltersSchema(),
-//			"compartment_id": {
-//				Type:     schema.TypeString,
-//				Required: true,
-//			},
-//			"display_name": {
-//				Type:     schema.TypeString,
-//				Optional: true,
-//			},
-//			"state": {
-//				Type:     schema.TypeString,
-//				Optional: true,
-//			},
-//			"connection_collection": {
-//				Type:     schema.TypeList,
-//				Computed: true,
-//				Elem: &schema.Resource{
-//					Schema: map[string]*schema.Schema{
-//
-//						"items": {
-//							Type:     schema.TypeList,
-//							Computed: true,
-//							Elem:     tfresource.GetDataSourceItemSchema(DatabaseMigrationConnectionResource()),
-//						},
-//					},
-//				},
-//			},
-//		},
-//	}
-//}
-//
-//func readDatabaseMigrationConnections(d *schema.ResourceData, m interface{}) error {
-//	sync := &DatabaseMigrationConnectionsDataSourceCrud{}
-//	sync.D = d
-//	sync.Client = m.(*client.OracleClients).DatabaseMigrationClient()
-//
-//	return tfresource.ReadResource(sync)
-//}
-//
-//type DatabaseMigrationConnectionsDataSourceCrud struct {
-//	D      *schema.ResourceData
-//	Client *oci_database_migration.DatabaseMigrationClient
-//	Res    *oci_database_migration.ListConnectionsResponse
-//}
-//
-//func (s *DatabaseMigrationConnectionsDataSourceCrud) VoidState() {
-//	s.D.SetId("")
-//}
-//
-//func (s *DatabaseMigrationConnectionsDataSourceCrud) Get() error {
-//	request := oci_database_migration.ListConnectionsRequest{}
-//
-//	if compartmentId, ok := s.D.GetOkExists("compartment_id"); ok {
-//		tmp := compartmentId.(string)
-//		request.CompartmentId = &tmp
-//	}
-//
-//	if displayName, ok := s.D.GetOkExists("display_name"); ok {
-//		tmp := displayName.(string)
-//		request.DisplayName = &tmp
-//	}
-//
-//	if state, ok := s.D.GetOkExists("state"); ok {
-//		request.LifecycleState = oci_database_migration.ListConnectionsLifecycleStateEnum(state.(string))
-//	}
-//
-//	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(false, "database_migration")
-//
-//	response, err := s.Client.ListConnections(context.Background(), request)
-//	if err != nil {
-//		return err
-//	}
-//
-//	s.Res = &response
-//	request.Page = s.Res.OpcNextPage
-//
-//	for request.Page != nil {
-//		listResponse, err := s.Client.ListConnections(context.Background(), request)
-//		if err != nil {
-//			return err
-//		}
-//
-//		s.Res.Items = append(s.Res.Items, listResponse.Items...)
-//		request.Page = listResponse.OpcNextPage
-//	}
-//
-//	return nil
-//}
-//
-//func (s *DatabaseMigrationConnectionsDataSourceCrud) SetData() error {
-//	if s.Res == nil {
-//		return nil
-//	}
-//
-//	s.D.SetId(tfresource.GenerateDataSourceHashID("DatabaseMigrationConnectionsDataSource-", DatabaseMigrationConnectionsDataSource(), s.D))
-//	resources := []map[string]interface{}{}
-//	connection := map[string]interface{}{}
-//
-//	items := []interface{}{}
-//	for _, item := range s.Res.Items {
-//		items = append(items, ConnectionSummaryToMap(item))
-//	}
-//	connection["items"] = items
-//
-//	if f, fOk := s.D.GetOkExists("filter"); fOk {
-//		items = tfresource.ApplyFiltersInCollection(f.(*schema.Set), items, DatabaseMigrationConnectionsDataSource().Schema["connection_collection"].Elem.(*schema.Resource).Schema)
-//		connection["items"] = items
-//	}
-//
-//	resources = append(resources, connection)
-//	if err := s.D.Set("connection_collection", resources); err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	oci_database_migration "github.com/oracle/oci-go-sdk/v65/databasemigration"
+
+	"github.com/oracle/terraform-provider-oci/internal/client"
+	"github.com/oracle/terraform-provider-oci/internal/tfresource"
+)
+
+func DatabaseMigrationConnectionsDataSource() *schema.Resource {
+	return &schema.Resource{
+		Read: readDatabaseMigrationConnections,
+		Schema: map[string]*schema.Schema{
+			"filter": tfresource.DataSourceFiltersSchema(),
+			"compartment_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"connection_type": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"display_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"source_connection_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"state": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"technology_type": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"connection_collection": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+
+						"items": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem:     tfresource.GetDataSourceItemSchema(DatabaseMigrationConnectionResource()),
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func readDatabaseMigrationConnections(d *schema.ResourceData, m interface{}) error {
+	sync := &DatabaseMigrationConnectionsDataSourceCrud{}
+	sync.D = d
+	sync.Client = m.(*client.OracleClients).DatabaseMigrationClient()
+
+	return tfresource.ReadResource(sync)
+}
+
+type DatabaseMigrationConnectionsDataSourceCrud struct {
+	D      *schema.ResourceData
+	Client *oci_database_migration.DatabaseMigrationClient
+	Res    *oci_database_migration.ListConnectionsResponse
+}
+
+func (s *DatabaseMigrationConnectionsDataSourceCrud) VoidState() {
+	s.D.SetId("")
+}
+
+func (s *DatabaseMigrationConnectionsDataSourceCrud) Get() error {
+	request := oci_database_migration.ListConnectionsRequest{}
+
+	if compartmentId, ok := s.D.GetOkExists("compartment_id"); ok {
+		tmp := compartmentId.(string)
+		request.CompartmentId = &tmp
+	}
+
+	if connectionType, ok := s.D.GetOkExists("connection_type"); ok {
+		interfaces := connectionType.([]interface{})
+		tmp := make([]oci_database_migration.ConnectionTypeEnum, len(interfaces))
+		for i := range interfaces {
+			if interfaces[i] != nil {
+				tmp[i] = oci_database_migration.ConnectionTypeEnum(interfaces[i].(string))
+			}
+		}
+		if len(tmp) != 0 || s.D.HasChange("connection_type") {
+			request.ConnectionType = tmp
+		}
+	}
+
+	if displayName, ok := s.D.GetOkExists("display_name"); ok {
+		tmp := displayName.(string)
+		request.DisplayName = &tmp
+	}
+
+	if sourceConnectionId, ok := s.D.GetOkExists("source_connection_id"); ok {
+		tmp := sourceConnectionId.(string)
+		request.SourceConnectionId = &tmp
+	}
+
+	if state, ok := s.D.GetOkExists("state"); ok {
+		request.LifecycleState = oci_database_migration.ListConnectionsLifecycleStateEnum(state.(string))
+	}
+
+	if technologyType, ok := s.D.GetOkExists("technology_type"); ok {
+		interfaces := technologyType.([]interface{})
+		tmp := make([]oci_database_migration.TechnologyTypeEnum, len(interfaces))
+		for i := range interfaces {
+			if interfaces[i] != nil {
+				tmp[i] = oci_database_migration.TechnologyTypeEnum(interfaces[i].(string))
+			}
+		}
+		if len(tmp) != 0 || s.D.HasChange("technology_type") {
+			request.TechnologyType = tmp
+		}
+	}
+
+	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(false, "database_migration")
+
+	response, err := s.Client.ListConnections(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	s.Res = &response
+	request.Page = s.Res.OpcNextPage
+
+	for request.Page != nil {
+		listResponse, err := s.Client.ListConnections(context.Background(), request)
+		if err != nil {
+			return err
+		}
+
+		s.Res.Items = append(s.Res.Items, listResponse.Items...)
+		request.Page = listResponse.OpcNextPage
+	}
+
+	return nil
+}
+
+func (s *DatabaseMigrationConnectionsDataSourceCrud) SetData() error {
+	if s.Res == nil {
+		return nil
+	}
+
+	s.D.SetId(tfresource.GenerateDataSourceHashID("DatabaseMigrationConnectionsDataSource-", DatabaseMigrationConnectionsDataSource(), s.D))
+	resources := []map[string]interface{}{}
+	connection := map[string]interface{}{}
+
+	items := []interface{}{}
+	for _, item := range s.Res.Items {
+		items = append(items, ConnectionSummaryToMap(item, true))
+	}
+	connection["items"] = items
+
+	if f, fOk := s.D.GetOkExists("filter"); fOk {
+		items = tfresource.ApplyFiltersInCollection(f.(*schema.Set), items, DatabaseMigrationConnectionsDataSource().Schema["connection_collection"].Elem.(*schema.Resource).Schema)
+		connection["items"] = items
+	}
+
+	resources = append(resources, connection)
+	if err := s.D.Set("connection_collection", resources); err != nil {
+		return err
+	}
+
+	return nil
+}
--- website/docs/d/database_migration_job.html.markdown
+++ website/docs/d/database_migration_job.html.markdown
@@ -4,13 +4,15 @@ layout: "oci"
 page_title: "Oracle Cloud Infrastructure: oci_database_migration_job"
 sidebar_current: "docs-oci-datasource-database_migration-job"
 description: |-
-  Provides details about a specific Job in Oracle Cloud Infrastructure Database Migration service
+Provides details about a specific Job in Oracle Cloud Infrastructure Database Migration service
 ---
 
 # Data Source: oci_database_migration_job
 This data source provides details about a specific Job resource in Oracle Cloud Infrastructure Database Migration service.
 
 Get a migration job.
+
+Note: If you wish to use the DMS deprecated API version /20210929 it is necessary to pin the Terraform Provider version to v5.46.0. Newer Terraform provider versions will not support the DMS deprecated API version /20210929
 
 ## Example Usage
 
@@ -25,44 +27,43 @@ data "oci_database_migration_job" "test_job" {
 
 The following arguments are supported:
 
-* `job_id` - (Required) The OCID of the job 
+* `job_id` - (Required) The OCID of the job
 
 
 ## Attributes Reference
 
 The following attributes are exported:
 
-* `defined_tags` - Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{"foo-namespace.bar-key": "value"}` 
-* `display_name` - Name of the job. 
-* `freeform_tags` - Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{"bar-key": "value"}` 
-* `id` - The OCID of the Migration Job. 
-* `lifecycle_details` - A message describing the current state in more detail. For example, can be used to provide actionable information for a resource in Failed state. 
-* `migration_id` - The OCID of the Migration that this job belongs to. 
-* `progress` - Progress details of a Migration Job. 
-	* `current_phase` - Current phase of the job. 
-	* `current_status` - Current status of the job. 
-	* `phases` - List of phase status for the job. 
-		* `action` - The text describing the action required to fix the issue 
-		* `duration_in_ms` - Duration of the phase in milliseconds 
-		* `extract` - Summary of phase status results. 
-			* `message` - Message in entry. 
-			* `type` - Type of extract. 
-		* `is_advisor_report_available` - True if a Pre-Migration Advisor report is available for this phase. False or null if no report is available. 
-		* `issue` - The text describing the root cause of the reported issue 
-		* `log_location` - Details to access log file in the specified Object Storage bucket, if any. 
-			* `bucket` - Name of the bucket containing the log file. 
-			* `namespace` - Object Storage namespace. 
-			* `object` - Log object name. 
-		* `name` - Phase name 
-		* `progress` - Percent progress of job phase. 
-		* `status` - Phase status 
-* `state` - The current state of the migration job. 
-* `system_tags` - Usage of system tag keys. These predefined keys are scoped to namespaces. Example: `{"orcl-cloud.free-tier-retained": "true"}` 
-* `time_created` - The time the Migration Job was created. An RFC3339 formatted datetime string 
-* `time_updated` - The time the Migration Job was last updated. An RFC3339 formatted datetime string 
-* `type` - The job type. 
-* `unsupported_objects` - Database objects not supported. 
-	* `object` - Name of the object (regular expression is allowed) 
-	* `owner` - Owner of the object (regular expression is allowed) 
+* `defined_tags` - Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{"foo-namespace.bar-key": "value"}`
+* `display_name` - Name of the job.
+* `freeform_tags` - Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace.  For more information, see Resource Tags. Example: {"Department": "Finance"}
+* `id` - The OCID of the Migration Job.
+* `lifecycle_details` - A message describing the current state in more detail. For example, can be used to provide actionable information for a resource in Failed state.
+* `migration_id` - The OCID of the Migration that this job belongs to.
+* `progress` - Progress details of a Migration Job.
+	* `current_phase` - Current phase of the job.
+	* `current_status` - Current status of the job.
+	* `phases` - List of phase status for the job.
+		* `action` - The text describing the action required to fix the issue
+		* `duration_in_ms` - Duration of the phase in milliseconds
+		* `extract` - Summary of phase status results.
+			* `message` - Message in entry.
+			* `type` - Type of extract.
+		* `is_advisor_report_available` - True if a Pre-Migration Advisor report is available for this phase. False or null if no report is available.
+		* `issue` - The text describing the root cause of the reported issue
+		* `log_location` - Details to access log file in the specified Object Storage bucket, if any.
+			* `bucket` - Name of the bucket containing the log file.
+			* `namespace` - Object Storage namespace.
+			* `object` - Log object name.
+		* `name` - Phase name
+		* `progress` - Percent progress of job phase.
+		* `status` - Phase status
+* `state` - The current state of the migration job.
+* `system_tags` - Usage of system tag keys. These predefined keys are scoped to namespaces. Example: `{"orcl-cloud.free-tier-retained": "true"}`
+* `time_created` - The time the Migration Job was created. An RFC3339 formatted datetime string
+* `time_updated` - The time the Migration Job was last updated. An RFC3339 formatted datetime string
+* `type` - The job type.
+* `unsupported_objects` - Database objects not supported.
+	* `object` - Name of the object (regular expression is allowed)
+	* `owner` - Owner of the object (regular expression is allowed)
 	* `type` - Type of unsupported object 
-
