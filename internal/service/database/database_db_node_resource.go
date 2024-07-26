009d2ee5eca02fecea362ea00fa380c43a8d5200
Terraform Team Automation <no-reply@oracle.com>
2024-07-26 21:42:22 +0000

    Bug Fix - Db_Node id setting causing an issue with backward compatability

--- internal/service/database/database_db_node_resource.go
+++ internal/service/database/database_db_node_resource.go
@@ -314,7 +314,7 @@ func (s *DatabaseDbNodeResourceCrud) Update() error {
 
 func (s *DatabaseDbNodeResourceCrud) SetData() error {
 	if s.Res.Id != nil {
-		s.D.Set("id", *s.Res.Id)
+		s.D.Set("db_node_id", *s.Res.Id)
 	}
 
 	if s.Res.AdditionalDetails != nil {
