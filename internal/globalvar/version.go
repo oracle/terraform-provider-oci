cec3fb240b3f690e658c8b3d4c5f79517a48b430
Vivek Arakali Nagaraja Rao <vivek.arakali.nagaraja.rao@oracle.com>
2024-07-29 19:56:10 -0500

    Exempted - Finalize oci.erb and release for version v6.5.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "6.3.1"
-const ReleaseDate = "2024-07-24"
+const Version = "6.5.0"
+const ReleaseDate = "2024-08-03"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
