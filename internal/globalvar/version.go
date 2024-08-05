12b786e0a578b07e755540b7361724580f88d79e
Monica Joshi <monica.joshi@oracle.com>
2024-08-05 15:24:08 +0530

    Exempted - Finalize oci.erb and release for version v6.6.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "6.5.0"
-const ReleaseDate = "2024-08-03"
+const Version = "6.6.0"
+const ReleaseDate = "2024-08-10"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
