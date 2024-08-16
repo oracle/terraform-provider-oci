7709e63b1a5f9faa1efd6e00cdea03d584dd8240
Ravi Prakash Bharti <ravi.prakash.bharti@oracle.com>
2024-08-16 17:41:40 +0530

    Exempted - Finalize oci.erb and release for version v6.8.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "6.7.0"
-const ReleaseDate = "2024-08-17"
+const Version = "6.8.0"
+const ReleaseDate = "2024-08-21"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
