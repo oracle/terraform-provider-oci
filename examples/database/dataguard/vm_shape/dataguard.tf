b6090487c096e1ec9a62a22a889713595bc3d8a9
Terraform Team Automation <no-reply@oracle.com>
2024-08-23 00:44:35 +0000

    Added - Support for Adding domain parameter in DG API

--- examples/database/dataguard/vm_shape/dataguard.tf
+++ examples/database/dataguard/vm_shape/dataguard.tf
@@ -34,17 +34,66 @@ data "oci_identity_availability_domain" "ad" {
   ad_number      = 1
 }
 
-resource "oci_core_vcn" "vcn1" {
+data "oci_core_services" "test_services" {
+  filter {
+    name   = "name"
+    values = [".*Oracle.*Services.*Network"]
+    regex  = true
+  }
+}
+
+data "oci_identity_availability_domains" "test_availability_domains" {
+  compartment_id = var.tenancy_ocid
+}
+
+resource "oci_core_vcn" "test_vcn" {
   cidr_block     = "10.0.0.0/16"
   compartment_id = var.compartment_ocid
   display_name   = "TFExampleVCN"
   dns_label      = "tfexamplevcn"
 }
 
+resource "oci_core_service_gateway" "test_service_gateway" {
+  compartment_id = var.compartment_ocid
+  display_name   = "test_service_gateway"
+  vcn_id         = oci_core_vcn.test_vcn.id
+
+  services {
+    service_id = data.oci_core_services.test_services.services.0.id
+  }
+
+}
+
+resource "oci_core_default_route_table" "test_vcn__default_route_table" {
+  manage_default_resource_id = oci_core_vcn.test_vcn.default_route_table_id
+
+  route_rules {
+    network_entity_id = oci_core_service_gateway.test_service_gateway.id
+
+    description 	  = "Internal traffic for OCI Services"
+    destination       = data.oci_core_services.test_services.services[0].cidr_block
+    destination_type  = "SERVICE_CIDR_BLOCK"
+  }
+}
+
+resource "oci_core_route_table" "test_route_table" {
+  compartment_id = var.compartment_ocid
+  display_name   = "test_subnet_rt"
+  vcn_id         = oci_core_vcn.test_vcn.id
+
+  route_rules {
+    network_entity_id = oci_core_service_gateway.test_service_gateway.id
+
+    description 	  = "Internal traffic for OCI Services"
+    destination       = data.oci_core_services.test_services.services[0].cidr_block
+    destination_type  = "SERVICE_CIDR_BLOCK"
+  }
+}
+
 resource "oci_core_security_list" "test_security_list" {
   compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_vcn.vcn1.id
-  display_name   = "TFExampleSecurityList"
+  vcn_id         = oci_core_vcn.test_vcn.id
+  display_name   = "test_security_list"
 
   // allow outbound tcp traffic on all ports
   egress_security_rules {
@@ -58,21 +107,21 @@ resource "oci_core_security_list" "test_security_list" {
   }
 }
 
-// An AD based subnet will supply an Availability Domain
 resource "oci_core_subnet" "test_subnet" {
-  availability_domain = data.oci_identity_availability_domain.ad.name
-  cidr_block          = "10.0.2.0/24"
-  display_name        = "TFADSubnet"
-  dns_label           = "adsubnet"
-  compartment_id      = var.compartment_ocid
-  vcn_id              = oci_core_vcn.vcn1.id
-  security_list_ids   = [oci_core_security_list.test_security_list.id]
-  route_table_id      = oci_core_vcn.vcn1.default_route_table_id
-  dhcp_options_id     = oci_core_vcn.vcn1.default_dhcp_options_id
+  cidr_block          	   = "10.0.2.0/24"
+  compartment_id      	   = var.compartment_ocid
+  vcn_id              	   = oci_core_vcn.test_vcn.id
+  display_name        	   = "test_subnet"
+  security_list_ids   	   = [oci_core_security_list.test_security_list.id]
+  route_table_id      	   = oci_core_route_table.test_route_table.id
+  dhcp_options_id     	   = oci_core_vcn.test_vcn.default_dhcp_options_id
+  dns_label           	   = "tftestsubnet"
+  prohibit_public_ip_on_vnic = "true"
 }
 
+
 resource "oci_database_db_system" "test_db_system" {
-  availability_domain     = oci_core_subnet.test_subnet.availability_domain
+  availability_domain     = data.oci_identity_availability_domains.test_availability_domains.availability_domains.0.name
   compartment_id          = var.compartment_ocid
   subnet_id               = oci_core_subnet.test_subnet.id
   database_edition        = "ENTERPRISE_EDITION"
@@ -125,12 +174,12 @@ resource "oci_database_data_guard_association" "test_data_guard_association" {
   display_name              = "TFExampleDataGuardAssociationVM"
   shape                     = "VM.Standard2.1"
   subnet_id                 = oci_core_subnet.test_subnet.id
-  availability_domain       = oci_core_subnet.test_subnet.availability_domain
+  availability_domain       = data.oci_identity_availability_domains.test_availability_domains.availability_domains.0.name
   nsg_ids                   = [oci_core_network_security_group.test_network_security_group.id]
   hostname                  = "ocidb"
-  db_system_defined_tags    = "${map("example-tag-namespace-all.example-tag", "dbSystemDefinedTags1")}"
+  db_system_defined_tags    = map("example-tag-namespace-all.example-tag", "dbSystemDefinedTags1")
   db_system_freeform_tags   = {"dbSystemFreeformTagsK" = "dbSystemFreeformTagsV"}
-  database_defined_tags     = "${map("example-tag-namespace-all.example-tag", "databaseDefinedTags1")}"
+  database_defined_tags     = map("example-tag-namespace-all.example-tag", "databaseDefinedTags1")
   database_freeform_tags    = {"databaseFreeformTagsK" = "databaseFreeformTagsV"}
   fault_domains             = ["FAULT-DOMAIN-3"]
   license_model             = "LICENSE_INCLUDED"
@@ -142,7 +191,7 @@ resource "oci_database_data_guard_association" "test_data_guard_association" {
 
 resource "oci_core_network_security_group" "test_network_security_group" {
   compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_vcn.vcn1.id
+  vcn_id         = oci_core_vcn.test_vcn.id
   display_name   = "tf-example-nsg"
 }
 
