594289ad0c8776026f416c99c9e247db584e8a7f
jiaqchenO <jiaqi.cheng@oracle.com>
2024-08-23 19:32:09 -0700

    Exempted - Finalize oci.erb and release for version v6.9.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "6.8.0"
-const ReleaseDate = "2024-08-21"
+const Version = "6.9.0"
+const ReleaseDate = "2024-08-28"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
