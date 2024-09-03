e5b75ef1e7cee8a1ceec22f4b2984cd7e3719409
Girish Jambagi <girish.jambagi@oracle.com>
2024-09-03 12:00:43 -0500

    Added - Support for Security Attributes

--- internal/integrationtest/security_attribute_security_attribute_namespace_test.go
+++ internal/integrationtest/security_attribute_security_attribute_namespace_test.go
@@ -6,11 +6,10 @@ package integrationtest
 import (
 	"context"
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"strconv"
 	"testing"
 	"time"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
