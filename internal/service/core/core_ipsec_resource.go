b3a9a77f2b23ea6a232268ba693a694303e2464f
Kartiki Bhat <kartiki.bhat@oracle.com>
2024-09-05 10:50:10 -0700

    Bug Fix - add support for tunnel configurations in create private ipsec over fastconnect

--- internal/service/core/core_ipsec_resource.go
+++ internal/service/core/core_ipsec_resource.go
@@ -5,9 +5,11 @@ package core
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"reflect"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
 	"github.com/oracle/terraform-provider-oci/internal/client"
 	"github.com/oracle/terraform-provider-oci/internal/tfresource"
 
@@ -77,6 +79,35 @@ func CoreIpSecConnectionResource() *schema.Resource {
 				Computed: true,
 				Elem:     schema.TypeString,
 			},
+			"tunnel_configuration": { // used for private ipsec connection on CREATE only. Will not be updated.
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				MinItems: 0,
+				MaxItems: 2,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"oracle_tunnel_ip": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"associated_virtual_circuits": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Computed: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"drg_route_table_id": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
 
 			// Computed
 			"state": {
@@ -128,10 +159,17 @@ func deleteCoreIpSecConnection(d *schema.ResourceData, m interface{}) error {
 	return tfresource.DeleteResource(d, sync)
 }
 
+type PrivateIpSecConnectionTunnelResourceCrud struct {
+	OracleTunnelIp            *string
+	AssociatedVirtualCircuits []string
+	DrgRouteTableId           *string
+}
+
 type CoreIpSecConnectionResourceCrud struct {
 	tfresource.BaseCrud
 	Client                 *oci_core.VirtualNetworkClient
 	Res                    *oci_core.IpSecConnection
+	ResTunnels             []PrivateIpSecConnectionTunnelResourceCrud
 	DisableNotFoundRetries bool
 }
 
@@ -231,6 +269,22 @@ func (s *CoreIpSecConnectionResourceCrud) Create() error {
 		request.StaticRoutes = tmp
 	}
 
+	if tunnelConfigs, ok := s.D.GetOkExists("tunnel_configuration"); ok {
+		tmpList := tunnelConfigs.([]interface{})
+		if len(tmpList) > 0 {
+			log.Printf("mapping first tunnel information")
+			fieldKeyFormat := fmt.Sprintf("%s.%d.%%s", "tunnel_configuration", 0)
+			tmp := s.mapToTunnelConfiguration(fieldKeyFormat)
+			request.TunnelConfiguration = append(request.TunnelConfiguration, tmp)
+		}
+		if len(tmpList) > 1 {
+			log.Printf("mapping second tunnel information")
+			fieldKeyFormat := fmt.Sprintf("%s.%d.%%s", "tunnel_configuration", 1)
+			tmp := s.mapToTunnelConfiguration(fieldKeyFormat)
+			request.TunnelConfiguration = append(request.TunnelConfiguration, tmp)
+		}
+	}
+
 	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(s.DisableNotFoundRetries, "core")
 
 	response, err := s.Client.CreateIPSecConnection(context.Background(), request)
@@ -256,6 +310,15 @@ func (s *CoreIpSecConnectionResourceCrud) Get() error {
 	}
 
 	s.Res = &response.IpSecConnection
+
+	// only retrieve tunnels for tunnel config info if ipsec over fastconnect
+	if s.Res.TransportType == oci_core.IpSecConnectionTransportTypeFastconnect {
+		err = s.GetTunnels()
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
@@ -295,6 +358,29 @@ func (s *CoreIpSecConnectionResourceCrud) Update() error {
 
 	if freeformTags, ok := s.D.GetOkExists("freeform_tags"); ok {
 		request.FreeformTags = tfresource.ObjectMapToStringMap(freeformTags.(map[string]interface{}))
+	}
+	if _, ok := s.D.GetOk("tunnel_configuration"); ok && s.D.HasChange("tunnel_configuration") {
+		oldValue, newValue := s.D.GetChange("tunnel_configuration")
+
+		oldTunnels, ok1 := oldValue.([]interface{})
+		newTunnels, ok2 := newValue.([]interface{})
+		if !ok1 || !ok2 {
+			return fmt.Errorf("Type conversion failed for tunnel, data type of tunnel is %s", reflect.TypeOf(newValue))
+		}
+		for index, _ := range newTunnels {
+			oldTunnel := oldTunnels[index].(map[string]interface{})
+			newTunnel := newTunnels[index].(map[string]interface{})
+
+			if oldTunnel["oracle_tunnel_ip"] != newTunnel["oracle_tunnel_ip"] {
+				return fmt.Errorf("oracle_tunnel_ip field cannot be updated after create ipsec connection")
+			}
+			if notequal := !reflect.DeepEqual(oldTunnel["associated_virtual_circuits"], newTunnel["associated_virtual_circuits"]); notequal {
+				return fmt.Errorf("associated_virtual_circuits field cannot be updated after create ipsec connection")
+			}
+			if oldTunnel["drg_route_table_id"] != newTunnel["drg_route_table_id"] {
+				return fmt.Errorf("drg_route_table_id cannot be updated through oci_core_ipsec, use oci_core_drg_attachment_management resource instead to update")
+			}
+		}
 	}
 
 	tmp := s.D.Id()
@@ -374,6 +460,32 @@ func (s *CoreIpSecConnectionResourceCrud) SetData() error {
 
 	s.D.Set("transport_type", s.Res.TransportType)
 
+	// set tunnel_configurations if ipsec over fast connect tunnel
+	if s.Res.TransportType == oci_core.IpSecConnectionTransportTypeFastconnect {
+		if s.ResTunnels != nil && len(s.ResTunnels) > 0 {
+			tmpList := []interface{}{}
+			for _, value := range s.ResTunnels {
+				t := make(map[string]interface{})
+				if value.OracleTunnelIp != nil {
+					t["oracle_tunnel_ip"] = *value.OracleTunnelIp
+				} else {
+					t["oracle_tunnel_ip"] = ""
+				}
+				t["associated_virtual_circuits"] = value.AssociatedVirtualCircuits
+				if value.DrgRouteTableId != nil {
+					t["drg_route_table_id"] = *value.DrgRouteTableId
+				} else {
+					t["drg_route_table_id"] = ""
+				}
+				tmpList = append(tmpList, t)
+			}
+			err := s.D.Set("tunnel_configuration", tmpList)
+			if err != nil {
+				log.Printf("[WARN] create_ipsec_connection tunnel configuration could not be set: %q", err)
+			}
+		}
+	}
+
 	return nil
 }
 
@@ -397,5 +509,114 @@ func (s *CoreIpSecConnectionResourceCrud) updateCompartment(compartment interfac
 		return waitErr
 	}
 
+	return nil
+}
+
+func (s *CoreIpSecConnectionResourceCrud) mapToTunnelConfiguration(fieldKeyFormat string) oci_core.CreateIpSecConnectionTunnelDetails {
+	result := oci_core.CreateIpSecConnectionTunnelDetails{}
+
+	if oracleTunnelIp, ok := s.D.GetOkExists(fmt.Sprintf(fieldKeyFormat, "oracle_tunnel_ip")); ok {
+		tmp := oracleTunnelIp.(string)
+		result.OracleTunnelIp = &tmp
+	}
+	if drgRouteTableId, ok := s.D.GetOkExists(fmt.Sprintf(fieldKeyFormat, "drg_route_table_id")); ok {
+		tmp := drgRouteTableId.(string)
+		result.DrgRouteTableId = &tmp
+	}
+	if virtualCircuits, ok := s.D.GetOkExists(fmt.Sprintf(fieldKeyFormat, "associated_virtual_circuits")); ok {
+		interfaces := virtualCircuits.([]interface{})
+		tmp := make([]string, len(interfaces))
+		for i := range interfaces {
+			if interfaces[i] != nil {
+				tmp[i] = interfaces[i].(string)
+			}
+		}
+		result.AssociatedVirtualCircuits = tmp
+	}
+	return result
+}
+
+func (s *CoreIpSecConnectionResourceCrud) GetTunnels() error {
+	if inputTunnelConfigs, ok := s.D.GetOk("tunnel_configuration"); ok {
+		tmpList := inputTunnelConfigs.([]interface{})
+		tunnelConfig := make([]PrivateIpSecConnectionTunnelResourceCrud, len(tmpList))
+		for index, _ := range tmpList {
+			tunnel := tmpList[index].(map[string]interface{})
+			var tmp PrivateIpSecConnectionTunnelResourceCrud
+			oracleIp := tunnel["oracle_tunnel_ip"].(string)
+			drgId := tunnel["drg_route_table_id"].(string)
+			tmp.OracleTunnelIp = &oracleIp
+			tmp.AssociatedVirtualCircuits = make([]string, len(tunnel["associated_virtual_circuits"].([]interface{})))
+			for k, v := range tunnel["associated_virtual_circuits"].([]interface{}) {
+				tmp.AssociatedVirtualCircuits[k] = v.(string)
+			}
+			tmp.DrgRouteTableId = &drgId
+			tunnelConfig[index] = tmp
+		}
+		s.ResTunnels = tunnelConfig
+	} else {
+		request := oci_core.ListIPSecConnectionTunnelsRequest{}
+
+		tmp := s.D.Id()
+		request.IpscId = &tmp
+
+		request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(false, "core")
+
+		response, err := s.Client.ListIPSecConnectionTunnels(context.Background(), request)
+		if err != nil {
+			return err
+		}
+
+		resTunnels := &response
+		request.Page = resTunnels.OpcNextPage
+
+		for request.Page != nil {
+			listResponse, err := s.Client.ListIPSecConnectionTunnels(context.Background(), request)
+			if err != nil {
+				return err
+			}
+
+			resTunnels.Items = append(resTunnels.Items, listResponse.Items...)
+			request.Page = listResponse.OpcNextPage
+		}
+		for _, value := range resTunnels.Items {
+			var tmp PrivateIpSecConnectionTunnelResourceCrud
+			tmp.OracleTunnelIp = value.VpnIp
+			tmp.AssociatedVirtualCircuits = value.AssociatedVirtualCircuits
+			err := s.GetDrgRouteTableId(&value, &tmp)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (s *CoreIpSecConnectionResourceCrud) GetDrgRouteTableId(tunnel *oci_core.IpSecConnectionTunnel, t *PrivateIpSecConnectionTunnelResourceCrud) error {
+	request := oci_core.ListDrgAttachmentsRequest{}
+
+	request.NetworkId = tunnel.Id
+	request.CompartmentId = tunnel.CompartmentId
+	request.AttachmentType = oci_core.ListDrgAttachmentsAttachmentTypeIpsecTunnel
+
+	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(false, "core")
+
+	response, err := s.Client.ListDrgAttachments(context.Background(), request)
+	if err != nil {
+		return err
+	}
+	resAtt := response
+	for request.Page != nil {
+		listResponse, err := s.Client.ListDrgAttachments(context.Background(), request)
+		if err != nil {
+			return err
+		}
+
+		resAtt.Items = append(resAtt.Items, listResponse.Items...)
+		request.Page = listResponse.OpcNextPage
+	}
+	if len(resAtt.Items) == 1 {
+		t.DrgRouteTableId = resAtt.Items[0].DrgRouteTableId
+	}
 	return nil
 }
