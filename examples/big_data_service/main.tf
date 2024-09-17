784b38c4c2b48f91ac45157c550c54992cea38ba
Shrey Kapoor <shrey.kapoor@oracle.com>
2024-09-17 13:14:54 -0700

    Bug Fix - Addnode failing for dense shapes

--- examples/big_data_service/main.tf
+++ examples/big_data_service/main.tf
@@ -5,7 +5,7 @@ variable "tenancy_ocid" {
 }
 
 variable "cluster_profile" {
-default = "HADOOP"
+  default = "HADOOP"
 }
 
 variable "user_ocid" {
@@ -85,7 +85,7 @@ variable "bds_instance_nodes_shape" {
 }
 
 variable "bds_instance_worker_node_shape" {
-  default = "VM.Standard.Generic"
+  default = "VM.DenseIO.E5.Flex"
 }
 
 variable "bds_instance_compute_only_worker_node_shape" {
@@ -148,7 +148,7 @@ resource "oci_bds_bds_instance" "test_bds_instance" {
   is_secure              = var.bds_instance_is_secure
   kms_key_id             = var.kms_key_id
   cluster_profile        = var.cluster_profile
-  bootstrap_script_url = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/Lk5JT9tnUIOG4yLm6S21QVR7m3Rm2uj1RAS2Olx5v14onLU2Y-b0lIc_N0RuUIge/n/idpbwtq1b3ta/b/bucket-20230214-1316/o/execute_bootstrap_script.sh"
+  bootstrap_script_url   = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/Lk5JT9tnUIOG4yLm6S21QVR7m3Rm2uj1RAS2Olx5v14onLU2Y-b0lIc_N0RuUIge/n/idpbwtq1b3ta/b/bucket-20230214-1316/o/execute_bootstrap_script.sh"
 
   master_node {
     #Required
@@ -158,9 +158,9 @@ resource "oci_bds_bds_instance" "test_bds_instance" {
     block_volume_size_in_gbs = var.bds_instance_nodes_block_volume_size_in_gbs
     number_of_nodes          = 1
     shape_config {
-            memory_in_gbs = 120
-           ocpus         = 8
-        }
+      memory_in_gbs = 120
+      ocpus         = 8
+    }
   }
 
   util_node {
@@ -171,50 +171,50 @@ resource "oci_bds_bds_instance" "test_bds_instance" {
     block_volume_size_in_gbs = var.bds_instance_nodes_block_volume_size_in_gbs
     number_of_nodes          = 1
     shape_config {
-            memory_in_gbs = 120
-            ocpus         = 8
-        }
+      memory_in_gbs = 120
+      ocpus         = 8
+    }
   }
 
   worker_node {
     #Required
     shape = var.bds_instance_worker_node_shape
-    block_volume_size_in_gbs = var.bds_instance_nodes_block_volume_size_in_gbs
-    subnet_id                = var.subnet_id
-    number_of_nodes          = 3
-       shape_config {
-              memory_in_gbs = 120
-              ocpus         = 8
-         }
-      }
-
-      edge_node {
-        #Required
-        shape = var.bds_instance_edge_node_shape
-
-        subnet_id                = var.subnet_id
-        block_volume_size_in_gbs = var.bds_instance_worker_nodes_block_volume_size_in_gbs
-        number_of_nodes          = 1
-        shape_config {
-          memory_in_gbs = var.bds_instance_edge_memory_per_node
-          ocpus         = var.bds_instance_edge_ocpu_per_node
-        }
-      }
-
-      compute_only_worker_node {
-        #Required
-        shape = var.bds_instance_compute_only_worker_node_shape
-
-        subnet_id                = var.subnet_id
-        block_volume_size_in_gbs = var.bds_instance_worker_nodes_block_volume_size_in_gbs
-        number_of_nodes          = 1
-        shape_config {
-          memory_in_gbs = var.bds_instance_compute_only_worker_memory_per_node
-          ocpus         = var.bds_instance_compute_only_worker_ocpu_per_node
+    subnet_id       = var.subnet_id
+    number_of_nodes = 3
+    shape_config {
+      memory_in_gbs = 120
+      ocpus         = 8
+      nvmes         = 1
     }
   }
 
- ignore_existing_nodes_shape = ["worker", "master", "utility"]
+  edge_node {
+    #Required
+    shape = var.bds_instance_edge_node_shape
+
+    subnet_id                = var.subnet_id
+    block_volume_size_in_gbs = var.bds_instance_worker_nodes_block_volume_size_in_gbs
+    number_of_nodes          = 1
+    shape_config {
+      memory_in_gbs = var.bds_instance_edge_memory_per_node
+      ocpus         = var.bds_instance_edge_ocpu_per_node
+    }
+  }
+
+  compute_only_worker_node {
+    #Required
+    shape = var.bds_instance_compute_only_worker_node_shape
+
+    subnet_id                = var.subnet_id
+    block_volume_size_in_gbs = var.bds_instance_worker_nodes_block_volume_size_in_gbs
+    number_of_nodes          = 1
+    shape_config {
+      memory_in_gbs = var.bds_instance_compute_only_worker_memory_per_node
+      ocpus         = var.bds_instance_compute_only_worker_ocpu_per_node
+    }
+  }
+
+  ignore_existing_nodes_shape = ["worker", "master", "utility"]
 
   is_cloud_sql_configured = false
 
@@ -224,7 +224,7 @@ resource "oci_bds_bds_instance" "test_bds_instance" {
 
   #Uncomment kafka_broker_node block for use of Kafka cluster
   #kafka_broker_node {
-    #Required
+  #Required
   #  shape = var.bds_instance_compute_only_worker_node_shape
 
   #  subnet_id                = var.subnet_id
@@ -237,10 +237,10 @@ resource "oci_bds_bds_instance" "test_bds_instance" {
   #}
 
   #Optional
-#Uncomment this when running in home region (PHX)
-#  defined_tags = {
-#    "${oci_identity_tag_namespace.tag-namespace1.name}.${oci_identity_tag.tag1.name}" = var.bds_instance_defined_tags_value
-#  }
+  #Uncomment this when running in home region (PHX)
+  #  defined_tags = {
+  #    "${oci_identity_tag_namespace.tag-namespace1.name}.${oci_identity_tag.tag1.name}" = var.bds_instance_defined_tags_value
+  #  }
   freeform_tags = var.bds_instance_freeform_tags
   network_config {
     #Optional
