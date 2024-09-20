a935cefcc836d6fb27a9b2a022173832eb85b8f4
Girish Jambagi <girish.jambagi@oracle.com>
2024-09-20 14:23:23 -0500

    Added - Support for Zero Trust Packet Routing

---
+++ internal/service/zpr/register_resource.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package zpr
+
+import "github.com/oracle/terraform-provider-oci/internal/tfresource"
+
+func RegisterResource() {
+	tfresource.RegisterResource("oci_zpr_configuration", ZprConfigurationResource())
+	tfresource.RegisterResource("oci_zpr_zpr_policy", ZprZprPolicyResource())
+}
