cd7f78fd4032d27ac36bfc29d0896408fd745041
P V Krishna Chaitanya <p.v.krishna.chaitanya@oracle.com>
2024-11-20 14:46:54 +0530

    Revert "TERSI-4090: BaseDB CP - TestDatabaseDatabaseUpgradeResource_basic &"
    This reverts commit becb6fc3e1ea1d54ea48a232ee2ae7442ef5b90e.

--- internal/service/database/database_backups_data_source.go
+++ internal/service/database/database_backups_data_source.go
@@ -5,10 +5,17 @@ package database
 
 import (
 	"context"
+	"time"
+<<<<<<< ours
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	oci_common "github.com/oracle/oci-go-sdk/v65/common"
+	oci_database "github.com/oracle/oci-go-sdk/v65/database"
+=======
+>>>>>>> theirs
+
 	"github.com/oracle/terraform-provider-oci/internal/client"
 	"github.com/oracle/terraform-provider-oci/internal/tfresource"
-	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	oci_database "github.com/oracle/oci-go-sdk/v65/database"
@@ -86,7 +93,7 @@ func (s *DatabaseBackupsDataSourceCrud) Get() error {
 	request := oci_database.ListBackupsRequest{}
 
 	if backupDestinationType, ok := s.D.GetOkExists("backup_destination_type"); ok {
-		tmp := backupDestinationType.(string)
+		tmp := oci_database.ListBackupsBackupDestinationTypeEnum(backupDestinationType.(string))
 		request.BackupDestinationType = &tmp
 	}
 
@@ -125,7 +132,7 @@ func (s *DatabaseBackupsDataSourceCrud) Get() error {
 	}
 
 	if type_, ok := s.D.GetOkExists("type"); ok {
-		tmp := type_.(string)
+		tmp := oci_database.ListBackupsTypeEnum(type_.(string))
 		request.Type = &tmp
 	}
 
