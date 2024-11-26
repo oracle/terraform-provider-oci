93e9c7d241d3359ea5f4e115e9338805d1dbf09b
rgorle <ramesh.gorle@oracle.com>
2024-11-26 11:02:48 +0530

    Added - Support for StackMonitoring: Metric Extensions : Advanced Support and Integration

--- examples/stack_monitoring/metric_extensions/metric_extension.tf
+++ examples/stack_monitoring/metric_extensions/metric_extension.tf
@@ -56,3 +56,47 @@ data "oci_stack_monitoring_metric_extension" "test_metric_extension_example" {
   #Required
   metric_extension_id = oci_stack_monitoring_metric_extension.test_metric_extension_example.id
 }
+
+resource "oci_stack_monitoring_metric_extension" "test_metric_extension_example_http" {
+	#Required
+	compartment_id = var.compartment_ocid
+	name = "ME_MetricExtensionTerraformExampleHttp"
+	resource_type = "oracle_goldengate_admin_server"
+	display_name = "Golden Gate IO Read Performance"
+	collection_recurrences = "FREQ=MINUTELY;INTERVAL=10"
+	metric_list {
+		name = "IoReadBytes"
+		display_name = "IO Read Bytes"
+		is_dimension = false
+		data_type = "NUMBER"
+		is_hidden = true 
+		metric_category = "AVAILABILITY"
+	} 
+  metric_list {
+		name = "IOReadRate"
+		display_name = "IO Read Rate"
+		is_dimension = false
+		data_type = "NUMBER"
+		is_hidden = false 
+		metric_category = "AVAILABILITY"
+    compute_expression = "(IoReadBytes > 0 ? (__interval == 0 ? 0 : (IoReadBytes > _IoReadBytes ? (((IoReadBytes - _IoReadBytes) / __interval) / (1024 * 1024)) : 0))  : 0)"
+	}
+	query_properties {
+		collection_method = "HTTP"
+		url = "%pm_server_url%/services/v2/mpoints/%api_process_name%/processPerformance"
+		response_content_type = "APPLICATION_JSON"
+		protocol_type = "HTTPS"
+		script_details {
+			name = "ioRead_performance.js"
+			content = "ZnVuY3Rpb24gcnVuTWV0aG9kKG1ldHJpY09ic2VydmF0aW9uLCBzb3VyY2VQcm9wcykKewogICAgbGV0IHJlc3BvbnNlX3JhdyA9IEpTT04ucGFyc2UobWV0cmljT2JzZXJ2YXRpb24pOwogICAgbGV0IHJlc3BvbnNlID0gcmVzcG9uc2VfcmF3LnJlc3BvbnNlOwogICAgbGV0IGlvUmVhZEJ5dGVzID0gcmVzcG9uc2VbImlvUmVhZEJ5dGVzIl07CiAgICByZXR1cm4gW1tpb1JlYWRCeXRlc11dOwp9"
+		}
+	}
+	#Optional
+	description = "Collects count of instances in 'UP' status in staging compartments from monitoring table"
+	publish_trigger = var.publish_trigger
+}
+
+data "oci_stack_monitoring_metric_extension" "test_metric_extension_example_http" {
+  # Required
+  metric_extension_id = oci_stack_monitoring_metric_extension.test_metric_extension_example_http.id
+}
