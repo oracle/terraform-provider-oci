a3443b8ec1cf9185a16821766246b7d670a0943e
Giridharan Anbarasan <giridharan.anbarasan@oracle.com>
2025-01-14 19:31:08 -0500

    Finalize changelog and release for version v6.22.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,9 +7,9 @@ import (
 	"log"
 )
 
-const Version = "6.21.0"
+const Version = "6.22.0"
 
-const ReleaseDate = "2024-12-22"
+const ReleaseDate = "2025-01-19"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
