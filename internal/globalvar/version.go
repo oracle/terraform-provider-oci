88b4c0860e5ef6299c90a16e730109a1d6cf4b2d
Giridharan Anbarasan <giridharan.anbarasan@oracle.com>
2025-01-15 23:56:55 -0500

    Fix in version.go

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
+// Copyright (c) 2017, 2024 Oracle and/or its affiliates. All rights reserved.
 // Licensed under the Mozilla Public License v2.0
 
 package globalvar
@@ -7,15 +7,9 @@ import (
 	"log"
 )
 
-<<<<<<< HEAD
 const Version = "6.22.0"
 
 const ReleaseDate = "2025-01-19"
-=======
-const Version = "6.21.0"
-
-const ReleaseDate = "2024-12-22"
->>>>>>> 57b36061333 (Finalize changelog and release for version v6.21.0)
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
