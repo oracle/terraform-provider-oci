05ae29256c7d86099619fd9656384397f79c8184
Giridharan Anbarasan <giridharan.anbarasan@oracle.com>
2025-01-16 12:38:30 -0500

    Fix in versions.go

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,15 +7,9 @@ import (
 	"log"
 )
 
-<<<<<<< HEAD
 const Version = "6.22.0"
 
 const ReleaseDate = "2025-01-19"
-=======
-const Version = "6.21.0"
-
-const ReleaseDate = "2024-12-22"
->>>>>>> 57b36061333 (Finalize changelog and release for version v6.21.0)
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
