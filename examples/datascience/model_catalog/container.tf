4be7896d0d42459674ca270ceaedaacd24b8cfa1
Nitin More <nitin.m.more@oracle.com>
2025-01-29 00:02:04 +0530

    updating example file

--- examples/datascience/model_catalog/container.tf
+++ examples/datascience/model_catalog/container.tf
@@ -1,12 +1,6 @@
 // Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
 // Licensed under the Mozilla Public License v2.0
 
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
-
 variable "container_display_name" {
   default = "displayName"
 }
@@ -20,7 +14,7 @@ variable "container_is_latest" {
 }
 
 variable "container_state" {
-  default = "AVAILABLE"
+  default = "INACTIVE"
 }
 
 variable "container_tag_query_param" {
@@ -35,32 +29,9 @@ variable "container_usage_query_param" {
   default = "INFERENCE"
 }
 
-
-
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
-}
-
-resource "oci_datascience_containers" "test_container" {
-
+data "oci_datascience_containers" "test_containers" {
   #Optional
   container_name    = var.container_name
-  display_name      = var.container_display_name
-  is_latest         = var.container_is_latest
-  state             = var.container_state
-  tag_query_param   = var.container_tag_query_param
-  target_workload   = var.container_target_workload
-  usage_query_param = var.container_usage_query_param
-}
-
-data "oci_datascience_containers" "test_containers" {
-
-  #Optional
-  container_name    = oci_datascience_containers.test_container.display_name
   display_name      = var.container_display_name
   is_latest         = var.container_is_latest
   state             = var.container_state
