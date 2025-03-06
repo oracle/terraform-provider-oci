a3c28911b655e7ae873154c7d19d1cd2292c2074
Naga Raju Pasunuri <naga.raju.pasunuri@oracle.com>
2025-03-06 11:54:12 +0530

    Update version.go

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 const Version = "6.29.0"
-const ReleaseDate = "2025-03-10"
+const ReleaseDate = "2025-03-06"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
