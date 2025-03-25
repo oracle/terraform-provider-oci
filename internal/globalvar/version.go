534700b14006c71b05e959d05081488d654292dd
Giridharan Anbarasan <giridharan.anbarasan@oracle.com>
2025-03-24 22:55:25 -0400

    Exempted - Finalize oci.erb and release for version v6.32.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "6.31.0"
-const ReleaseDate = "2025-03-23"
+const Version = "6.32.0"
+const ReleaseDate = "2025-03-29"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
