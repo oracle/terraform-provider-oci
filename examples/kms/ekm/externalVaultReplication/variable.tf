ad08df7dd7e40c6cab93c2430083911b95006ba8
Akshay Mall <akshay.mall@oracle.com>
2025-04-07 20:50:19 +0530

    Added - Support for Ekm-Xrr

---
+++ examples/kms/ekm/externalVaultReplication/variable.tf
@@ -0,0 +1,33 @@
+variable "tenancy_ocid" {
+}
+
+variable "user_ocid" {
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key_path" {
+}
+
+variable "region" {
+}
+
+variable "compartment_ocid" {
+}
+
+variable "ext_vault" {
+}
+
+variable "ext_key_version_id" {
+}
+
+
+provider "oci" {
+  version          = "6.27.0"
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
