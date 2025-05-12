9a2b6acd51ee9c62cd2ce72e86de56ca07dfa853
P V Krishna Chaitanya <p.v.krishna.chaitanya@oracle.com>
2025-05-12 10:36:44 +0530

    Finalize changelog and release for version v7.1.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "7.0.0"
-const ReleaseDate = "2025-05-07"
+const Version = "7.1.0"
+const ReleaseDate = "2025-05-17"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
