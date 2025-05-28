09bb0e9fd9cef922a2737fb0605a836e6810a318
DevOps Pipeline <oci_dlcbld_in_grp@oracle.com>
2025-05-28 08:32:17 +0000

    Vendored - oci-go-sdk for release version v65

---
+++ vendor/github.com/oracle/oci-go-sdk/v65/opensearch/validate_opensearch_pipeline_details.go
@@ -0,0 +1,42 @@
+// Copyright (c) 2016, 2018, 2025, Oracle and/or its affiliates.  All rights reserved.
+// This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
+// Code generated. DO NOT EDIT.
+
+// OpenSearch Service API
+//
+// The OpenSearch service API provides access to OCI Search Service with OpenSearch.
+//
+
+package opensearch
+
+import (
+	"fmt"
+	"github.com/oracle/oci-go-sdk/v65/common"
+	"strings"
+)
+
+// ValidateOpensearchPipelineDetails The configuration details for validating pipeline configuration provided as input.
+type ValidateOpensearchPipelineDetails struct {
+
+	// The OCID of the compartment where the pipeline will be created.
+	CompartmentId *string `mandatory:"true" json:"compartmentId"`
+
+	// The pipeline configuration in YAML format. The command accepts the pipeline configuration as a string or within a .yaml file. If you provide the configuration as a string, each new line must be escaped with \.
+	PipelineConfigurationBody *string `mandatory:"true" json:"pipelineConfigurationBody"`
+}
+
+func (m ValidateOpensearchPipelineDetails) String() string {
+	return common.PointerString(m)
+}
+
+// ValidateEnumValue returns an error when providing an unsupported enum value
+// This function is being called during constructing API request process
+// Not recommended for calling this function directly
+func (m ValidateOpensearchPipelineDetails) ValidateEnumValue() (bool, error) {
+	errMessage := []string{}
+
+	if len(errMessage) > 0 {
+		return true, fmt.Errorf(strings.Join(errMessage, "\n"))
+	}
+	return false, nil
+}
