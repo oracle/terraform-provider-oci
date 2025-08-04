7138650bf3011f4490b9071fed6d996994342f75
Priyanshu Jaiwar <priyanshu.jaiwar@oracle.com>
2025-08-04 13:02:52 +0530

    Added - Support for scimQuery field for findingAnalytics API in data safe

--- internal/integrationtest/data_safe_security_assessment_finding_analytic_test.go
+++ internal/integrationtest/data_safe_security_assessment_finding_analytic_test.go
@@ -27,6 +27,18 @@ var (
 		"top_finding_status":        acctest.Representation{RepType: acctest.Optional, Create: `RISK`},
 	}
 
+	DataSafeSecurityAssessmentFindingAnalyticScimDataSourceRepresentation = map[string]interface{}{
+		"compartment_id":            acctest.Representation{RepType: acctest.Required, Create: `${var.compartment_id}`},
+		"access_level":              acctest.Representation{RepType: acctest.Optional, Create: `RESTRICTED`},
+		"compartment_id_in_subtree": acctest.Representation{RepType: acctest.Optional, Create: `false`},
+		"finding_key":               acctest.Representation{RepType: acctest.Optional, Create: `findingKey`},
+		"scim_query":                acctest.Representation{RepType: acctest.Optional, Create: `severity eq \"EVALUATE\"`},
+		"group_by":                  acctest.Representation{RepType: acctest.Optional, Create: `findingKeyAndTopFindingStatus`},
+		"is_top_finding":            acctest.Representation{RepType: acctest.Optional, Create: `false`},
+		"severity":                  acctest.Representation{RepType: acctest.Optional, Create: `HIGH`},
+		"top_finding_status":        acctest.Representation{RepType: acctest.Optional, Create: `RISK`},
+	}
+
 	DataSafeSecurityAssessmentFindingAnalyticResourceConfig = ""
 )
 
@@ -49,6 +61,17 @@ func TestDataSafeSecurityAssessmentFindingAnalyticResource_basic(t *testing.T) {
 		{
 			Config: config +
 				acctest.GenerateDataSourceFromRepresentationMap("oci_data_safe_security_assessment_finding_analytics", "test_security_assessment_finding_analytics", acctest.Required, acctest.Create, DataSafeSecurityAssessmentFindingAnalyticDataSourceRepresentation) +
+				compartmentIdVariableStr + DataSafeSecurityAssessmentFindingAnalyticResourceConfig,
+			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
+				resource.TestCheckResourceAttrSet(datasourceName, "finding_analytics_collection.#"),
+				resource.TestCheckResourceAttrSet(datasourceName, "finding_analytics_collection.0.items.#"),
+			),
+		},
+
+		// verify scim datasource
+		{
+			Config: config +
+				acctest.GenerateDataSourceFromRepresentationMap("oci_data_safe_security_assessment_finding_analytics", "test_security_assessment_finding_analytics", acctest.Required, acctest.Create, DataSafeSecurityAssessmentFindingAnalyticScimDataSourceRepresentation) +
 				compartmentIdVariableStr + DataSafeSecurityAssessmentFindingAnalyticResourceConfig,
 			Check: acctest.ComposeAggregateTestCheckFuncWrapper(
 				resource.TestCheckResourceAttrSet(datasourceName, "finding_analytics_collection.#"),
