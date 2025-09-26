50fcb7ae66b17d017ad3f51f9a1c5f71494fc950
Sriniakhil Gujulla Leel <sriniakhil.gujulla.leel@oracle.com>
2025-09-26 19:10:17 +0530

    Added - Support for sigint context to go sdk for GoldenGate

--- internal/service/golden_gate/golden_gate_trail_files_data_source.go
+++ internal/service/golden_gate/golden_gate_trail_files_data_source.go
@@ -6,6 +6,7 @@ package golden_gate
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	oci_golden_gate "github.com/oracle/oci-go-sdk/v65/goldengate"
 
@@ -15,7 +16,7 @@ import (
 
 func GoldenGateTrailFilesDataSource() *schema.Resource {
 	return &schema.Resource{
-		Read: readGoldenGateTrailFiles,
+		ReadContext: readGoldenGateTrailFilesWithContext,
 		Schema: map[string]*schema.Schema{
 			"filter": tfresource.DataSourceFiltersSchema(),
 			"deployment_id": {
@@ -98,12 +99,12 @@ func GoldenGateTrailFilesDataSource() *schema.Resource {
 	}
 }
 
-func readGoldenGateTrailFiles(d *schema.ResourceData, m interface{}) error {
+func readGoldenGateTrailFilesWithContext(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	sync := &GoldenGateTrailFilesDataSourceCrud{}
 	sync.D = d
 	sync.Client = m.(*client.OracleClients).GoldenGateClient()
 
-	return tfresource.ReadResource(sync)
+	return tfresource.HandleDiagError(m, tfresource.ReadResourceWithContext(ctx, sync))
 }
 
 type GoldenGateTrailFilesDataSourceCrud struct {
@@ -116,7 +117,7 @@ func (s *GoldenGateTrailFilesDataSourceCrud) VoidState() {
 	s.D.SetId("")
 }
 
-func (s *GoldenGateTrailFilesDataSourceCrud) Get() error {
+func (s *GoldenGateTrailFilesDataSourceCrud) GetWithContext(ctx context.Context) error {
 	request := oci_golden_gate.ListTrailFilesRequest{}
 
 	if deploymentId, ok := s.D.GetOkExists("deployment_id"); ok {
@@ -136,7 +137,7 @@ func (s *GoldenGateTrailFilesDataSourceCrud) Get() error {
 
 	request.RequestMetadata.RetryPolicy = tfresource.GetRetryPolicy(false, "golden_gate")
 
-	response, err := s.Client.ListTrailFiles(context.Background(), request)
+	response, err := s.Client.ListTrailFiles(ctx, request)
 	if err != nil {
 		return err
 	}
@@ -145,7 +146,7 @@ func (s *GoldenGateTrailFilesDataSourceCrud) Get() error {
 	request.Page = s.Res.OpcNextPage
 
 	for request.Page != nil {
-		listResponse, err := s.Client.ListTrailFiles(context.Background(), request)
+		listResponse, err := s.Client.ListTrailFiles(ctx, request)
 		if err != nil {
 			return err
 		}
