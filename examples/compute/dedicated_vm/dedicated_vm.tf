8d42c1581740cec6472fa0ccca237ea7e121b343
Rakshit Sinha <rakshit.sinha@oracle.com>
2025-09-30 21:38:30 +0000

    Added - Support for Multi-Shape Family Virtual Machine Instances on Dedicated Virtual Machine Hosts

--- examples/compute/dedicated_vm/dedicated_vm.tf
+++ examples/compute/dedicated_vm/dedicated_vm.tf
@@ -15,31 +15,25 @@ variable "compartment_ocid" {}
 
 variable subnet_id {}
 
-variable "instance_image_ocid" {
-  type = map(string)
-
-  default = {
-    # See https://docs.us-phoenix-1.oraclecloud.com/images/
-    # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
-  }
-}
+variable "instance_image_ocid" {}
 
 locals {
   # For resource "oci_core_dedicated_vm_host" "test_dedicated_vm_host"
-  dvh_shape        = "DVH.DenseIO.E4.128"
+  dvh_shape        = "DVH.Standard.E4.128"
   dvh_display_name = "TestDedicatedVmHost"
+  capacity_config  = "standard_e4_flex"
+  is_memory_encryption_enabled = "true"
 
   # For resource "oci_core_instance" "test_instance"
-  vmi_display_name                    = "TestInstance"
-  vmi_shape                           = "VM.DenseIO.E4.Flex"
-  instance_shape_config_memory_in_gbs = "128"
-  instance_shape_config_ocpus         = "8"
-  instance_shape_config_nvmes         = "1"
+  vmi_display_name = "TestInstance"
+  vmi_shape                           = "VM.Standard.E4.Flex"
+  instance_shape_config_memory_in_gbs = "16"
+  instance_shape_config_ocpus = "1"
 
   # For data "oci_core_dedicated_vm_hosts" "test_dedicated_vm_hosts"
   dvh_lifecycle_state                              = "ACTIVE"
-  remaining_memory_in_gbs_greater_than_or_equal_to = "512.0"
-  remaining_ocpus_greater_than_or_equal_to         = "32.0"
+  remaining_memory_in_gbs_greater_than_or_equal_to = "1.0"
+  remaining_ocpus_greater_than_or_equal_to = "1.0"
 
   # For data "oci_core_subnet" "test_subnet"
   subnet_id = var.subnet_id
@@ -53,12 +47,12 @@ provider "oci" {
   fingerprint         = var.fingerprint
   private_key_path    = var.private_key_path
   region              = var.region
-  # version             = "7.1.0"
+  # version             = "7.22.0"
 }
 
 data "oci_identity_availability_domain" "ad" {
   compartment_id = var.tenancy_ocid
-  ad_number      = 1
+  ad_number = 1
 }
 
 resource "oci_core_dedicated_vm_host" "test_dedicated_vm_host" {
@@ -69,6 +63,8 @@ resource "oci_core_dedicated_vm_host" "test_dedicated_vm_host" {
 
   #Optional
   display_name = local.dvh_display_name
+  capacity_config              = local.capacity_config
+  is_memory_encryption_enabled = local.is_memory_encryption_enabled
 
   timeouts {
     create = "60m"
@@ -90,7 +86,6 @@ resource "oci_core_instance" "test_instance" {
   shape_config {
     memory_in_gbs = local.instance_shape_config_memory_in_gbs
     ocpus         = local.instance_shape_config_ocpus
-    nvmes         = local.instance_shape_config_nvmes
   }
   instance_options {
     are_legacy_imds_endpoints_disabled = true
@@ -108,11 +103,16 @@ resource "oci_core_instance" "test_instance" {
 
   source_details {
     source_type = "image"
-    source_id   = var.instance_image_ocid[var.region]
+    source_id = var.instance_image_ocid
     # Apply this to set the size of the boot volume that's created for this instance.
     # Otherwise, the default boot volume size of the image is used.
     # This should only be specified when source_type is set to "image".
     # boot_volume_size_in_gbs = "60"
+  }
+
+  platform_config {
+    type                         = "AMD_VM"
+    is_memory_encryption_enabled = true
   }
 
   timeouts {
@@ -131,6 +131,8 @@ data "oci_core_dedicated_vm_hosts" "test_dedicated_vm_hosts" {
   #Optional
   availability_domain                              = data.oci_identity_availability_domain.ad.name
   display_name                                     = local.dvh_display_name
+  is_memory_encryption_enabled = local.is_memory_encryption_enabled
+
   instance_shape_name                              = local.vmi_shape
   remaining_memory_in_gbs_greater_than_or_equal_to = local.remaining_memory_in_gbs_greater_than_or_equal_to
   remaining_ocpus_greater_than_or_equal_to         = local.remaining_ocpus_greater_than_or_equal_to
@@ -159,20 +161,36 @@ data "oci_core_dedicated_vm_host" "test_oci_core_dedicated_vm_host" {
   dedicated_vm_host_id = oci_core_dedicated_vm_host.test_dedicated_vm_host.id
 }
 
-#output the dedicated vm host ids
+data "oci_core_dedicated_vm_hosts_instances" "test_dedicated_vm_hosts_instances" {
+  #Required
+  compartment_id = var.compartment_ocid
+  dedicated_vm_host_id = oci_core_dedicated_vm_host.test_dedicated_vm_host.id
+
+  #Optional
+  availability_domain = data.oci_identity_availability_domain.ad.name
+}
+
+# output of List DVHs
 output "dedicated_host_ids" {
-  value = [data.oci_core_dedicated_vm_hosts.test_dedicated_vm_hosts.id]
+  value = data.oci_core_dedicated_vm_hosts.test_dedicated_vm_hosts.id
 }
 
-#output the dedicated vm host ids
+# output of List DVH Shapes
 output "dedicated_host_shapes" {
-  value = [data.oci_core_dedicated_vm_host_shapes.test_dedicated_vm_host_shapes.dedicated_vm_host_shapes]
+  value = data.oci_core_dedicated_vm_host_shapes.test_dedicated_vm_host_shapes.dedicated_vm_host_shapes
 }
 
+# output of List DVH Instance Shapes
 output "dedicated_vm_host_instance_shapes" {
-  value = [data.oci_core_dedicated_vm_host_instance_shapes.test_dedicated_vm_host_instance_shapes.dedicated_vm_host_instance_shapes]
+  value = data.oci_core_dedicated_vm_host_instance_shapes.test_dedicated_vm_host_instance_shapes.dedicated_vm_host_instance_shapes
 }
 
+# output of List DVH Instances
+output "dedicated_vm_host_instances_data" {
+  value = data.oci_core_dedicated_vm_hosts_instances.test_dedicated_vm_hosts_instances.dedicated_vm_host_instances
+}
+
+# output of Get DVH
 output "dedicated_vm_host_data" {
-  value = [data.oci_core_dedicated_vm_host.test_oci_core_dedicated_vm_host.*]
+  value = data.oci_core_dedicated_vm_host.test_oci_core_dedicated_vm_host.*
 }
