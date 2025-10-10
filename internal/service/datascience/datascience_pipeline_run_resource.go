636c11ef21ccf1d5e6c0ad6afce7a0e07334862e
P V Krishna Chaitanya <p.v.krishna.chaitanya@oracle.com>
2025-10-10 11:31:42 +0530

    BugFix - goimports format for datascience_pipeline_run_resource

--- internal/service/datascience/datascience_pipeline_run_resource.go
+++ internal/service/datascience/datascience_pipeline_run_resource.go
@@ -6,10 +6,11 @@ package datascience
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"log"
 	"strconv"
 	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
