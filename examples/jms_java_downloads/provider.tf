3ff87b5fda0396c7d47f140eb6ace143d33b13bb
Phyo <phyo.khun@oracle.com>
2025-10-16 10:04:03 +0800

    Added - Support for JMS common variables

---
+++ examples/jms_java_downloads/provider.tf
@@ -0,0 +1,30 @@
+// Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+/*
+ * This example file shows how to configure the oci provider to target a single region.
+ */
+
+// These variables would commonly be defined as environment variables or sourced in a .env file
+variable "tenancy_ocid" {
+}
+
+variable "user_ocid" {
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key_path" {
+}
+
+variable "region" {
+}
+
+provider "oci" {
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
