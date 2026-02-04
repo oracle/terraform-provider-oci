6f4f12f58d0799c61b280d95879b3c8554938882
P V Krishna Chaitanya <p.v.krishna.chaitanya@oracle.com>
2026-02-04 20:42:49 +0530

    Finalize oci.erb and release for version v8.0.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "7.32.0"
-const ReleaseDate = "2026-01-27"
+const Version = "8.0.0"
+const ReleaseDate = "2026-02-04"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
