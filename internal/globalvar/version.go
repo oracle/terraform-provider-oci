55502f15ffd8c837b21341ba79c9c93218fa3c1a
Giridharan Anbarasan <giridharan.anbarasan@oracle.com>
2026-03-10 13:14:39 -0400

    Finalize oci.erb and release for version v8.5.0

--- internal/globalvar/version.go
+++ internal/globalvar/version.go
@@ -7,8 +7,8 @@ import (
 	"log"
 )
 
-const Version = "8.4.0"
-const ReleaseDate = "2026-03-04"
+const Version = "8.5.0"
+const ReleaseDate = "2026-03-10"
 
 func PrintVersion() {
 	log.Printf("[INFO] terraform-provider-oci %s\n", Version)
