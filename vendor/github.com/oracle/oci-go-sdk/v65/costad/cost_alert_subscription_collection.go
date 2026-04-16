e0bfb0dfb0ec8ea71a47460d5123569eaeab425d
Gaurav Saini <gaurav.g.saini@oracle.com>
2026-04-16 09:03:43 +0000

    Exempted - Preview SDK - DEXREQ-8582

---
+++ vendor/github.com/oracle/oci-go-sdk/v65/costad/cost_alert_subscription_collection.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2016, 2018, 2026, Oracle and/or its affiliates.  All rights reserved.
+// This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
+// Code generated. DO NOT EDIT.
+
+// CostAd API
+//
+// Use the CostAd API to manage cost monitor, cost anomaly events and subscription list. For more information, see Cost Anomaly Detection Overview (https://docs.oracle.com/iaas/Content/Billing/Concepts/costanomalydetectionoverview.htm).
+//
+
+package costad
+
+import (
+	"fmt"
+	"github.com/oracle/oci-go-sdk/v65/common"
+	"strings"
+)
+
+// CostAlertSubscriptionCollection Results of a costAlertSubscription search. Contains both CostAlertSubscriptionSummary items and other data.
+type CostAlertSubscriptionCollection struct {
+
+	// A page of CostAnomalyMonitorSummary objects.
+	Items []CostAlertSubscriptionSummary `mandatory:"true" json:"items"`
+}
+
+func (m CostAlertSubscriptionCollection) String() string {
+	return common.PointerString(m)
+}
+
+// ValidateEnumValue returns an error when providing an unsupported enum value
+// This function is being called during constructing API request process
+// Not recommended for calling this function directly
+func (m CostAlertSubscriptionCollection) ValidateEnumValue() (bool, error) {
+	errMessage := []string{}
+
+	if len(errMessage) > 0 {
+		return true, fmt.Errorf("%s", strings.Join(errMessage, "\n"))
+	}
+	return false, nil
+}
